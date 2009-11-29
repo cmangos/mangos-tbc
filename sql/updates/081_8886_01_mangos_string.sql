53fca5faf55eb1f0e4d3a5abc5c99c247d01cc3f
Nick Templeton <nick@nicktempleton.com>
2009-11-29 09:23:35 +0300

    Add username/password prompts to Remote Admin
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (backported from commit 67a9a2f)

---
+++ sql/updates/081_8886_01_mangos_string.sql
@@ -0,0 +1,5 @@
+DELETE FROM mangos_string WHERE entry IN(60,61,62);
+INSERT INTO mangos_string VALUES
+(60,'I\'m busy right now, come back later.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(61,'Username: ',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(62,'Password: ',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);

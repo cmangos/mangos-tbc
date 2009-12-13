898cc901cd5c055f69b05b51fd1ecb9b82924d7d
FragFrog <FragFrog@getmangos.com>
2009-12-13 22:12:06 +0300

    Spell 34123 aleatd casted from dummy effect.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5277,7 +5277,6 @@ void Aura::HandleShapeshiftBoosts(bool apply)
             break;
         case FORM_TREE:
             spellId1 = 5420;
-            spellId2 = 34123;
             break;
         case FORM_TRAVEL:
             spellId1 = 5419;

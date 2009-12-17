6b196cb82102736b92299088558e2912d2777067
VladimirMangos <vladimir@getmangos.com>
2009-12-17 16:55:28 +0300

    Fixed unexpected error output for "unused" `db_script_string`.
    When it reallt used in `gossip_scripts`.
    
    (backported from commit 44fbf7e)

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -7922,6 +7922,7 @@ void ObjectMgr::LoadDbScriptStrings()
     CheckScripts(sSpellScripts,ids);
     CheckScripts(sGameObjectScripts,ids);
     CheckScripts(sEventScripts,ids);
+    CheckScripts(sGossipScripts,ids);
 
     sWaypointMgr.CheckTextsExistance(ids);
 

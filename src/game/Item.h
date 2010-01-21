7c6986fe87aa224a38ac83f5a262b973710eb336
VladimirMangos <vladimir@getmangos.com>
2010-01-21 12:02:22 +0300

    More wide check item looting state at item operations.
    Move some checks from packet handlers to Player::Can functions
    
    Patch base at idea suggested originally by zhenya.
    
    (based on commit 5cf5f11)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Item.h
+++ src/game/Item.h
@@ -253,6 +253,7 @@ class MANGOS_DLL_SPEC Item : public Object
         void SetCount(uint32 value) { SetUInt32Value (ITEM_FIELD_STACK_COUNT, value); }
         uint32 GetMaxStackCount() const { return GetProto()->GetMaxStackSize(); }
         uint8 GetGemCountWithID(uint32 GemID) const;
+        uint8 CanBeMergedPartlyWith(ItemPrototype const* proto) const;
 
         uint8 GetSlot() const {return m_slot;}
         Bag *GetContainer() { return m_container; }

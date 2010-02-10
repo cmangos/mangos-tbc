6fe5880487da4d1bea7a110ddaad3aa583da14a0
VladimirMangos <vladimir@getmangos.com>
2010-02-10 03:48:44 +0300

    Show enchantment applied to item in trade slot to caster after apply.
    Also more strictly check cases when in spell targets presetn item in trade slot.
    
    (based on commit c12fd0e)

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3613,6 +3613,10 @@ void Spell::EffectEnchantItemPerm(uint32 i)
 
         // add new enchanting if equipped
         item_owner->ApplyEnchantment(itemTarget,PERM_ENCHANTMENT_SLOT,true);
+
+        // update trade window for show enchantment for caster in trade window
+        if (m_targets.m_targetMask & TARGET_FLAG_TRADE_ITEM)
+            p_caster->GetSession()->SendUpdateTrade();
     }
 }
 
@@ -3744,6 +3748,10 @@ void Spell::EffectEnchantItemTmp(uint32 i)
 
     // add new enchanting if equipped
     item_owner->ApplyEnchantment(itemTarget, TEMP_ENCHANTMENT_SLOT, true);
+
+    // update trade window for show enchantment for caster in trade window
+    if (m_targets.m_targetMask & TARGET_FLAG_TRADE_ITEM)
+        p_caster->GetSession()->SendUpdateTrade();
 }
 
 void Spell::EffectTameCreature(uint32 /*i*/)

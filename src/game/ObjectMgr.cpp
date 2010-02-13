1b0bb5a61521f4e4d4446ecac477d498b1cd5c2c
VladimirMangos <vladimir@getmangos.com>
2010-02-14 00:58:20 +0300

    Implement .reload all_gossips
    * For reload `gossip_menu`, `gossip_menu_option`, `gossip_scripts`,
      `npc_gossip`, `points_of_interest` by single command.
    * Also fixed reloading `points_of_interest`.
    
    (based on commit 858f364)

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -6282,6 +6282,8 @@ void ObjectMgr::LoadReputationOnKill()
 
 void ObjectMgr::LoadPointsOfInterest()
 {
+    mPointsOfInterest.clear();                              // need for reload case
+
     uint32 count = 0;
 
     //                                                0      1  2  3      4     5

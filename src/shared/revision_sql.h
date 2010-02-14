dc71c691145a3d26631e1016745c76a0af841cfc
Odyssey <Odyssey@getmangos.com>
2010-02-14 22:03:17 +0300

    Some missing .reload all_* commands descriptions in mangos.sql
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit e55133f)
    (based on commit cd613b0)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_091_9339_01_characters_group"
- #define REVISION_DB_MANGOS "required_092_9380_01_mangos_command"
+ #define REVISION_DB_MANGOS "required_093_9385_01_mangos_command"
  #define REVISION_DB_REALMD "required_9010_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__

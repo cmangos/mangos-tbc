698587ab2ce9deb9de95afc142df94e53ffe55ad
Shin_Darth <Shin_Darth@getmangos.com>
2010-02-14 20:43:05 +0300

    Some missing in mangos.sql command descriptions.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit ea8be87)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_091_9339_01_characters_group"
- #define REVISION_DB_MANGOS "required_090_7092_01_mangos_player_xp_for_level"
+ #define REVISION_DB_MANGOS "required_092_9380_01_mangos_command"
  #define REVISION_DB_REALMD "required_9010_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__

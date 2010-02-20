51435ef20367a536f5ba8cf13ea2addb93459fd2
VladimirMangos <vladimir@getmangos.com>
2010-02-20 22:14:15 +0300

    Return success result for Zone<->map coordinates convertions.
    This will prevent #INF-inity values in gps for zones where
    zone coordinates calculation impossible with current code.
    
    Also fix small typo in debug output format for honor
    
    (based on commit b87c746)

--- src/game/DBCStores.h
+++ src/game/DBCStores.h
@@ -55,8 +55,8 @@ ChatChannelsEntry const* GetChannelEntryFor(uint32 channel_id);
 
 bool IsTotemCategoryCompatiableWith(uint32 itemTotemCategoryId, uint32 requiredTotemCategoryId);
 
-void Zone2MapCoordinates(float& x,float& y,uint32 zone);
-void Map2ZoneCoordinates(float& x,float& y,uint32 zone);
+bool Zone2MapCoordinates(float& x,float& y,uint32 zone);
+bool Map2ZoneCoordinates(float& x,float& y,uint32 zone);
 
 uint32 GetTalentInspectBitPosInTab(uint32 talentId);
 uint32 GetTalentTabInspectBitSize(uint32 talentTabId);

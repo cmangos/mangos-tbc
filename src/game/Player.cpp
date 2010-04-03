c3d52638a778b3baa83aa79479c8738d8f300fdc
VladimirMangos <vladimir@getmangos.com>
2010-04-04 03:51:22 +0400

    Fixed bug with possible double remove spell.
    In case when saved in DB spell remove triggering by
    dependendences chain self remove attempt.
    
    (based on commit 5f16ce8)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -3066,7 +3066,7 @@ void Player::removeSpell(uint32 spell_id, bool disabled)
 
     // re-search, it can be corrupted in prev loop
     itr = m_spells.find(spell_id);
-    if (itr == m_spells.end())
+    if (itr == m_spells.end() || itr->second.state == PLAYERSPELL_REMOVED)
         return;                                             // already unleared
 
     // removing

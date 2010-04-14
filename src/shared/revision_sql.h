6686795044038836f4a8f7f37c1b8af299a00595
VladimirMangos <vladimir@getmangos.com>
2010-04-14 09:17:45 +0400

    Implement show realm version and build in realm list.
    * Rename `realmlist`.`color` field to `realmflags`
    
    * Client 2.x and later support show in realm list supported
      client version for specific realm.
    
      For client 1.x this implemented by adding version to name
      in similar way as it look in more recent clients.
    
      For enable version show each affected realm must have in `realmflags` set
      flag 4.
    
      For realm work with not officially supported builds (build > last suported)
      will show version 0.0.0 and correct build value.
    
    (based on commit 14e71d2)
    (based on commit 76d863a)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_102_9687_01_characters_character_queststatus_daily"
  #define REVISION_DB_MANGOS "required_101_9663_01_mangos_mangos_string"
- #define REVISION_DB_REALMD "required_9010_01_realmd_realmlist"
+ #define REVISION_DB_REALMD "required_9746_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__

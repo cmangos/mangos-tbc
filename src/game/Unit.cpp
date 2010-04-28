663f3d5616a84974356e9d5661b39301e2aaf755
tomrus88 <tomrus88@gmail.com>
2010-04-28 17:48:27 +0400

    Allow speed change for units
    (based on commit cb3432e)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9610,10 +9610,6 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
 
     propagateSpeedChange();
 
-    // Send speed change packet only for player
-    if (GetTypeId()!=TYPEID_PLAYER)
-        return;
-
     WorldPacket data;
     if(!forced)
     {
@@ -9662,9 +9658,13 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
     }
     else
     {
-        // register forced speed changes for WorldSession::HandleForceSpeedChangeAck
-        // and do it only for real sent packets and use run for run/mounted as client expected
-        ++((Player*)this)->m_forced_speed_changes[mtype];
+        if(GetTypeId() == TYPEID_PLAYER)
+        {
+            // register forced speed changes for WorldSession::HandleForceSpeedChangeAck
+            // and do it only for real sent packets and use run for run/mounted as client expected
+            ++((Player*)this)->m_forced_speed_changes[mtype];
+        }
+
         switch(mtype)
         {
             case MOVE_WALK:

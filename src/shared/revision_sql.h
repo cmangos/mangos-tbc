5da401d08dc02b4b4d70851602efeb18bbe6c654
VladimirMangos <vladimir@getmangos.com>
2010-05-23 23:58:29 +0400

    Implement .server log filter and .server log level comamnds.
    * .server log filter comamnd let temporary (until config reload or restart)
      set log filters state. Or look at filters state.
    * .server log level renamed from .server set loglevel but now let look at log level also.
    
    (based on commit 2a021a7)
    (based on commit 7dc5422)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_114_9849_01_characters_saved_variables"
- #define REVISION_DB_MANGOS "required_117_9886_02_mangos_command"
+ #define REVISION_DB_MANGOS "required_118_9924_02_mangos_command"
  #define REVISION_DB_REALMD "required_9748_01_realmd_realmlist"
 #endif // __REVISION_SQL_H__

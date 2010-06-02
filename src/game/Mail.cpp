90e1d2b94ea859808908c65897baababc786382c
VladimirMangos <vladimir@getmangos.com>
2010-06-02 06:10:58 +0400

    Mail price calculation cleanup backport.
    (based on commit e7d6fed)

--- src/game/Mail.cpp
+++ src/game/Mail.cpp
@@ -127,9 +127,9 @@ void WorldSession::HandleSendMail(WorldPacket & recv_data )
         return;
     }
 
-    uint32 reqmoney = money + 30;
-    if (items_count)
-        reqmoney = money + (30 * items_count);
+    uint32 cost = items_count ? 30 * items_count : 30;      // price hardcoded in client
+
+    uint32 reqmoney = cost + money;
 
     if (pl->GetMoney() < reqmoney)
     {

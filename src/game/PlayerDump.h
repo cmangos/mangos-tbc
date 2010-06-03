5eea26e7aa12599db1e888ca9444a10f9730e34b
VladimirMangos <vladimir@getmangos.com>
2010-06-03 17:12:34 +0400

    PDump code fixes and cleanups
    * At pdump creating will propertly saved NULL field values.
    * At pdump loading will skipped adding `character_declinedname`
      if name in some way will be changed (explicly or at loading)
    
    (based on commit a2f9788)

--- src/game/PlayerDump.h
+++ src/game/PlayerDump.h
@@ -25,13 +25,15 @@
 
 enum DumpTableType
 {
-    DTT_CHARACTER,      //                                  // characters
+    DTT_CHARACTER,      //    -> guid, name                 // characters
 
     DTT_CHAR_TABLE,     //                                  // character_action,
-                                                            // character_aura, character_declinedname,
+                                                            // character_aura
                                                             // character_homebind, character_queststatus,
                                                             // character_reputation, character_spell,
                                                             // character_spell_cooldown, character_ticket
+
+    DTT_CHAR_NAME_TABLE,// <- guid, name                    // character_declinedname
 
     DTT_INVENTORY,      //    -> item guids collection      // character_inventory
 

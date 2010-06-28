ed53abd4f2f71628102d801cecbf9350c6429ed3
schmoozerd <schmoozerd@getmangos.com>
2010-06-28 23:25:08 +0400

    Pool/event info in .npc info and .gobject target commands
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit 8944fdd)

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -3619,6 +3619,7 @@ bool ChatHandler::HandleNpcInfoCommand(const char* /*args*/)
         SendSysMessage(LANG_NPCINFO_TRAINER);
     }
 
+    ShowNpcOrGoSpawnInformation<Creature>(target->GetDBTableGUIDLow());
     return true;
 }
 

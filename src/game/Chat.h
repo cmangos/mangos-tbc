19c151367982eb5d254c0f3ae7f49122ad924607
schmoozerd <schmoozerd@getmangos.com>
2010-07-03 07:46:54 +0400

    Add pool/event ids to diff creature/gameobject list output commands.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit 797cc2b)

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -541,6 +541,8 @@ class ChatHandler
         bool HandleGoHelper(Player* _player, uint32 mapid, float x, float y, float const* zPtr = NULL, float const* ortPtr = NULL);
         template<typename T>
         void ShowNpcOrGoSpawnInformation(uint32 guid);
+        template <typename T>
+        std::string PrepareStringNpcOrGoSpawnInformation(uint32 guid);
 
         /**
          * Stores informations about a deleted character
@@ -593,5 +595,8 @@ class CliHandler : public ChatHandler
         void* m_callbackArg;
         Print* m_print;
 };
+
+
+
 
 #endif
--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_114_9849_01_characters_saved_variables"
- #define REVISION_DB_MANGOS "required_134_10109_01_mangos_creature_model_info"
+ #define REVISION_DB_MANGOS "required_135_10125_01_mangos_mangos_string"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

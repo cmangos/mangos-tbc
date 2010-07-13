362839c29bbbe9195f6075004412755ea02fa6cc
SilverIce <slifeleaf@gmail.com>
2010-07-13 05:01:18 +0400

    Fixed visibility constant refresh problem with pet direct control spells.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit 0ca1a15)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3340,6 +3340,8 @@ void Aura::HandleModPossessPet(bool apply, bool Real)
 
     if (apply)
     {
+        pet->addUnitState(UNIT_STAT_CONTROLLED);
+
         camera.SetView(pet);
         p_caster->SetCharm(pet);
         p_caster->SetClientControl(pet, 1);
@@ -3361,6 +3363,8 @@ void Aura::HandleModPossessPet(bool apply, bool Real)
         // on delete only do caster related effects
         if(m_removeMode == AURA_REMOVE_BY_DELETE)
             return;
+
+        pet->clearUnitState(UNIT_STAT_CONTROLLED);
 
         pet->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);
 

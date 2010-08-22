5f3c5fef6bb404ec5658ebc8d3b684d21d51d141
NoFantasy <nofantasy@nf.no>
2010-08-22 12:48:06 +0400

    [s0116] Implement creature_movement_template
    Template can be used for several cases:
    * Unique creature that are already spawned in database (requires creature.MovementType=2 like guid based creature_movement)
    * Summoned creature that has a pre-defined path (requires creature_template.MovementType=2)
    
    Note that creature_template.MovementType=2 should be used with care, and must not be used for creatures that may be summoned in random locations in world.
    
    Added additional startup checks for existing creature_movement-table
    
    Also a small spoon of changes and cleanup for WaypointMovementGenerator
    * Correctly clearUnitState at Interrupt of WaypointMovementGenerator for creature
    * Initialization to ensure destination is always set (prevent evade problem)
    * Simplify how behavior for node is processed, incl fix MovementInform script call (based on idea/past code from Quriq14)
    * Make sure last node is also processed correct (DB script for last node will now work as expected)
    
    Signed-off-by: NoFantasy <nofantasy@nf.no>
    
    (based on commit 4fe568a)
    (based on commit 258a11e)
    (based on commit 6a4ea78)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_s0114_09136_01_characters_character_ticket"
- #define REVISION_DB_MANGOS "required_s0114_10353_03_mangos_mangos_string"
+ #define REVISION_DB_MANGOS "required_s0116_10362_01_mangos_creature_movement_template"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

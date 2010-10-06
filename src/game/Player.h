e6fa1bd025686478f11c519a5c5b4434400692c2
VladimirMangos <vladimir@getmangos.com>
2010-10-06 17:33:14 +0400

    [s0286] Remove reundenant spell store for temporary unsummoned  pet.
    We not use this data and now always have proeprtly save current pet
    when it temporary unsummoned and avoid it overwrite by another cases.
    
    (based on commit 936208e)

--- src/game/Player.h
+++ src/game/Player.h
@@ -2415,7 +2415,6 @@ class MANGOS_DLL_SPEC Player : public Unit
 
         // Temporary removed pet cache
         uint32 m_temporaryUnsummonedPetNumber;
-        uint32 m_oldpetspell;
 
         ReputationMgr  m_reputationMgr;
 

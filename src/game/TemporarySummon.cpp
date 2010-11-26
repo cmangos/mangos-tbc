cde63e04bd378d8405c23a48f66ee9559336e503
VladimirMangos <vladimir@getmangos.com>
2010-11-26 19:41:07 +0300

    [s0395] Cleanup weather update code and some random code cleanups form applied/reverted/applied/reverted timer changes.
    (based on commit 967877f)
    (based on commit 06e2d68)
    (based on commit 395f8f1)
    (based on commit 8398a55)
    (based on commit 925318e)
    (based on commit f57ec56)
    (based on commit b95e89e)

--- src/game/TemporarySummon.cpp
+++ src/game/TemporarySummon.cpp
@@ -63,7 +63,7 @@ void TemporarySummon::Update( uint32 diff )
 
         case TEMPSUMMON_CORPSE_TIMED_DESPAWN:
         {
-            if ( m_deathState == CORPSE)
+            if (m_deathState == CORPSE)
             {
                 if (m_timer <= diff)
                 {
@@ -78,7 +78,7 @@ void TemporarySummon::Update( uint32 diff )
         case TEMPSUMMON_CORPSE_DESPAWN:
         {
             // if m_deathState is DEAD, CORPSE was skipped
-            if ( m_deathState == CORPSE || m_deathState == DEAD)
+            if (m_deathState == CORPSE || m_deathState == DEAD)
             {
                 UnSummon();
                 return;
@@ -88,7 +88,7 @@ void TemporarySummon::Update( uint32 diff )
         }
         case TEMPSUMMON_DEAD_DESPAWN:
         {
-            if ( m_deathState == DEAD )
+            if (m_deathState == DEAD)
             {
                 UnSummon();
                 return;
@@ -98,7 +98,7 @@ void TemporarySummon::Update( uint32 diff )
         case TEMPSUMMON_TIMED_OR_CORPSE_DESPAWN:
         {
             // if m_deathState is DEAD, CORPSE was skipped
-            if ( m_deathState == CORPSE || m_deathState == DEAD)
+            if (m_deathState == CORPSE || m_deathState == DEAD)
             {
                 UnSummon();
                 return;

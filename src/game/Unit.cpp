b503de79abb21b405e3d698d3cabdf844923c00b
Tassader2 <Tassader2@getmangos.com>
2010-12-27 03:13:12 +0300

    [s0581] Show spell miss as miss, not resist
    Signed-off-by: ApoC <apoc@nymfe.net>
    
    (based on commit 09c03b8)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -2736,7 +2736,8 @@ SpellMissInfo Unit::MagicSpellHitResult(Unit *pVictim, SpellEntry const *spell)
 
     int32 rand = irand(0,10000);
     if (rand > HitChance)
-        return SPELL_MISS_RESIST;
+        return SPELL_MISS_MISS;
+
     return SPELL_MISS_NONE;
 }
 

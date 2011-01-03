ccc80e9fbdc8da19e232419215145a1f6f9bb4bc
NoFantasy <nofantasy@nf.no>
2011-01-03 18:50:10 +0300

    [s0623] Add unknown case enum UNIT_BYTE1_FLAG_UNK_2
    Signed-off-by: NoFantasy <nofantasy@nf.no>
    
    (based on commit 4051363)

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -158,6 +158,7 @@ enum UnitStandFlags
 enum UnitBytes1_Flags
 {
     UNIT_BYTE1_FLAG_ALWAYS_STAND = 0x01,
+    UNIT_BYTE1_FLAG_UNK_2        = 0x02,
     UNIT_BYTE1_FLAG_UNTRACKABLE  = 0x04,
     UNIT_BYTE1_FLAG_ALL          = 0xFF
 };

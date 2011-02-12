6e9e1915e58ab2ba9b23d73d676ce095ec40007f
SilverIce <slifeleaf@gmail.com>
2011-02-12 23:56:03 +0300

    [s0802] Reset camera view at removing from world
    this helps us to clean reference between viewpoint and camera in some cases
    
    (based on commit ef10da0)

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1952,6 +1952,9 @@ void Player::RemoveFromWorld()
     ///- Do not add/remove the player from the object storage
     ///- It will crash when updating the ObjectAccessor
     ///- The player should only be removed when logging out
+    if (IsInWorld())
+        GetCamera().ResetView();
+
     Unit::RemoveFromWorld();
 }
 

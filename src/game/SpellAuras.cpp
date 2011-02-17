72e6a00ed3c3d19e2b8f7f879a2f274ce7ae548b
VladimirMangos <vladimir@getmangos.com>
2011-02-17 09:44:47 +0300

    [s0818] Prevent crash at player far teleport landing to nonexisten battleground instance.
    Teleport back in like case or to homebind in bad case.
    
    (based on commit 47e5f97)
    (based on commit ea62f21)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2609,7 +2609,7 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                     if (totem && apply)
                         ((Player*)target)->GetCamera().SetView(totem);
                     else
-                        ((Player*)target)->GetCamera().ResetView(totem);
+                        ((Player*)target)->GetCamera().ResetView();
 
                     return;
                 }

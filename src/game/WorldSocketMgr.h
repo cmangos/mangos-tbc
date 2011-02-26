4361e44a68c8f1d0c37bb7c41b2a5d9a2146ac0f
XTZGZoReX <xtzgzorex@gmail.com>
2011-02-26 07:14:21 +0300

    [s0878] Small code cleanups in world sockets.
    (based on commit cc082e6)
    
    Partly backport for better API compatibility in branches.

--- src/game/WorldSocketMgr.h
+++ src/game/WorldSocketMgr.h
@@ -29,6 +29,8 @@
 #include <ace/Singleton.h>
 #include <ace/Thread_Mutex.h>
 
+#include <string>
+
 class WorldSocket;
 class ReactorRunnable;
 class ACE_Event_Handler;
@@ -41,7 +43,7 @@ public:
   friend class ACE_Singleton<WorldSocketMgr,ACE_Thread_Mutex>;
 
   /// Start network, listen at address:port .
-  int StartNetwork (ACE_UINT16 port, const char* address);
+  int StartNetwork (ACE_UINT16 port, std::string& address);
 
   /// Stops all network threads, It will wait for all running threads .
   void StopNetwork ();

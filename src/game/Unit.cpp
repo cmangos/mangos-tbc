f618d8f0874d5f08bcba401d65736486a918b03b
stfx <stfx@github.com>
2011-03-09 17:14:20 +0300

    [s0922] Use expected packet size appropriate to real used fields for SMSG_SPELLDAMAGESHIELD
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -1722,7 +1722,7 @@ void Unit::DealMeleeDamage(CalcDamageInfo *damageInfo, bool durabilityLoss)
 
                pVictim->DealDamageMods(this,damage,NULL);
 
-               WorldPacket data(SMSG_SPELLDAMAGESHIELD,(8+8+4+4));
+               WorldPacket data(SMSG_SPELLDAMAGESHIELD,(8+8+4+4+4));
                data << pVictim->GetObjectGuid();
                data << GetObjectGuid();
                data << uint32(i_spellProto->Id);

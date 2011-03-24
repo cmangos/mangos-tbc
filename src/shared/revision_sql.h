49312c673f406b973fc54697075c1ade0340b453
Laise <fenrisse@gmail.com>
2011-03-24 07:50:24 +0300

    [s0967] Add shared object for auras of same spell and move spell proc code to its own file, also spread procs by auras and effect indexes.
    (based on commit b2260d2)
    
    Per-effect spell family masks. In current table its just duplicates but know cases in
    not yet backported patches that need diff values for diff effects.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_s0963_10156_02_characters_pet_aura"
- #define REVISION_DB_MANGOS "required_s0955_xxxxx_01_mangos_creature_equip_template"
+ #define REVISION_DB_MANGOS "required_s0967_10156_01_mangos_spell_proc_event"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

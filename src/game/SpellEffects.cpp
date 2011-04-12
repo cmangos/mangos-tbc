ea8868678e13e2e677ef396c18e079d1cd6d20e5
DaC <DaC@getmangos.com>
2011-04-13 01:51:34 +0400

    [s1116] Move dummy effect code for spell 38194 to proper spell family case.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit bf1eb8e)

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1097,14 +1097,6 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                     m_caster->CastCustomSpell(unitTarget, 37675, &basepoints0, NULL, NULL, true);
                     return;
                 }
-                case 38194:                                 // Blink
-                {
-                    // Blink
-                    if (unitTarget)
-                        m_caster->CastSpell(unitTarget, 38203, true);
-
-                    return;
-                }
                 case 39189:                                 // Sha'tari Torch
                 {
                     if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT || m_caster->GetTypeId() != TYPEID_PLAYER)
@@ -1372,6 +1364,14 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                         };
                         unitTarget->CastSpell( unitTarget, spell_list[urand(0, 5)], true);
                     }
+                    return;
+                }
+                case 38194:                                 // Blink
+                {
+                    // Blink
+                    if (unitTarget)
+                        m_caster->CastSpell(unitTarget, 38203, true);
+
                     return;
                 }
             }

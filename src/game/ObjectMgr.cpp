c03bfbd40addd11742b09e48fdf2fb4fd6602a0e
NoFantasy <nofantasy@nf.no>
2011-04-17 08:50:51 +0400

    [s1150] Drop not needed MAX_CONDITION define
    Signed-off-by: NoFantasy <nofantasy@nf.no>
    
    (based on commit f49436d)

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -7338,13 +7338,7 @@ bool PlayerCondition::Meets(Player const * player) const
 // Verification of condition values validity
 bool PlayerCondition::IsValid(ConditionType condition, uint32 value1, uint32 value2)
 {
-    if (condition >= MAX_CONDITION)                         // Wrong condition type
-    {
-        sLog.outErrorDb("Condition has bad type of %u, skipped ", condition );
-        return false;
-    }
-
-    switch (condition)
+    switch(condition)
     {
         case CONDITION_AURA:
         {
@@ -7595,6 +7589,9 @@ bool PlayerCondition::IsValid(ConditionType condition, uint32 value1, uint32 val
         }
         case CONDITION_NONE:
             break;
+        default:
+            sLog.outErrorDb("Condition has bad type of %u, skipped ", condition);
+            return false;
     }
     return true;
 }

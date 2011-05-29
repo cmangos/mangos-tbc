70c1f9c7d0c95008362f7695144bb703744c94b9
Iriskin <Iriskin@github.com>
2011-05-29 10:04:20 +0400

    [s1246] Fixed typo in chat command help.
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit 549a96b)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_s1099_11299_02_characters_pet_aura"
- #define REVISION_DB_MANGOS "required_s1244_11513_01_mangos_mangos_string"
+ #define REVISION_DB_MANGOS "required_s1246_11521_01_mangos_command"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

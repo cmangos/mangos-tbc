e4341389f6211ddbcd1fdef43707fbf903916faf
VladimirMangos <vladimir@getmangos.com>
2011-09-09 19:51:34 +0200

    [s1354] Fixed auction crash in case missing localization for only some items
    Source crash in missing locale strings array size check before access
    to it in locale structure.
    
    Also move repeating code for access to wide used localization string arrays to ObjectMgr functions.
    
    (based on commit d2abd41)

--- src/game/Chat.cpp
+++ src/game/Chat.cpp
@@ -1748,7 +1748,7 @@ valid examples:
                                 return false;
                             }
 
-                            for(uint8 i=0; i<MAX_LOCALE; ++i)
+                            for(uint8 i=0; i < MAX_LOCALE; ++i)
                             {
                                 uint32 skillLineNameLength = strlen(skillLine->name[i]);
                                 if (skillLineNameLength > 0 && strncmp(skillLine->name[i], buffer, skillLineNameLength) == 0)
@@ -1785,7 +1785,7 @@ valid examples:
                             }
 
                             bool foundName = false;
-                            for(uint8 i=0; i<ql->Title.size(); i++)
+                            for(uint8 i=0; i < ql->Title.size(); ++i)
                             {
                                 if (ql->Title[i] == buffer)
                                 {

261b783eef6f295c3292e02c1d032b9d99e68aef
VladimirMangos <vladimir@getmangos.com>
2011-09-09 23:35:01 +0200

    [s1356] AHBot chat/console commands.
    Originaly provided in ahbot patch but havy rewrited.
    
    (based on commit 5820242)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_s1350_11716_09_characters_mail"
- #define REVISION_DB_MANGOS "required_s1339_11701_01_mangos_command"
+ #define REVISION_DB_MANGOS "required_s1356_11731_02_mangos_mangos_string"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

c575f32f9e2b5aaf5029c9427582e4d91aef1f76
stfx <stfx@hotmail.de>
2011-09-16 08:44:07 +0200

    [s1367] Add missing characters.sql version bump of s1350

--- sql/characters.sql
+++ sql/characters.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_s1342_11704_01_characters_auction` bit(1) default NULL
+  `required_s1350_11716_09_characters_mail` bit(1) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
 
 --

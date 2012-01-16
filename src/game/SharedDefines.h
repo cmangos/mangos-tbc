12ed315572171c6c64c3541f8636765893b26ec2
virusav <virusav@getmangos.com>
2012-01-16 18:26:42 +0100

    [s1457] Remove check for quest_template if QuestSort and SkillId match
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- src/game/SharedDefines.h
+++ src/game/SharedDefines.h
@@ -2082,23 +2082,6 @@ inline SkillType SkillByLockType(LockType locktype)
     return SKILL_NONE;
 }
 
-inline uint32 SkillByQuestSort(int32 QuestSort)
-{
-    switch(QuestSort)
-    {
-        case QUEST_SORT_HERBALISM:      return SKILL_HERBALISM;
-        case QUEST_SORT_FISHING:        return SKILL_FISHING;
-        case QUEST_SORT_BLACKSMITHING:  return SKILL_BLACKSMITHING;
-        case QUEST_SORT_ALCHEMY:        return SKILL_ALCHEMY;
-        case QUEST_SORT_LEATHERWORKING: return SKILL_LEATHERWORKING;
-        case QUEST_SORT_ENGINEERING:    return SKILL_ENGINEERING;
-        case QUEST_SORT_TAILORING:      return SKILL_TAILORING;
-        case QUEST_SORT_COOKING:        return SKILL_COOKING;
-        case QUEST_SORT_FIRST_AID:      return SKILL_FIRST_AID;
-    }
-    return 0;
-}
-
 enum SkillCategory
 {
     SKILL_CATEGORY_ATTRIBUTES    = 5,

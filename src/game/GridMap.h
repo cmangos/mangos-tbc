7c841c9c0837c49f18a8c2e95e81be781fa42adf
faramir118 <faramir118@gmail.com>
2012-01-29 22:08:43 +0100

    [s1482] Fix Liquid Extraction and better hole support

--- src/game/GridMap.h
+++ src/game/GridMap.h
@@ -47,6 +47,8 @@ struct GridMapFileHeader
     uint32 heightMapSize;
     uint32 liquidMapOffset;
     uint32 liquidMapSize;
+    uint32 holesOffset;
+    uint32 holesSize;
 };
 
 #define MAP_AREA_NO_AREA      0x0001

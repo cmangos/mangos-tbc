6701840c6b91febf115a22fb0c2d8685f65e4b8c
Schmoozerd <schmoozerd@scriptdev2.com>
2012-04-21 16:13:28 +0200

    [s1528] Rewrite and cleanup DB Script Engine
    * Moved actual script processing code to ScriptMgr
    * Unifed and improved log output. Now table-name is passed to each script execution
    * Added the "buddy concept" to all commands (except the outdated direct field change commands):
    Now all commands support to search a creature (or go for some commands) in a radius, with which to do some stuff.
    See doc/script_commands.txt for more details and information
    
    Attention DB Devs:
    Current DB-Scripts are converted automatically as far as possible, but some old target selecting mechanics were not reasonable to do automated.
    For such cases the command is marked with data_flags & 0x10 (==16), which will throw DB-Errors to track them down faster
    
    Thanks to NeatElves and especially Grz3s for testing!
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>
    Signed-off-by: Nighoo <nighoo@gmail.com>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_s1350_11716_09_characters_mail"
- #define REVISION_DB_MANGOS "required_s1504_11926_01_mangos_creature_template"
+ #define REVISION_DB_MANGOS "required_s1528_11940_07_mangos_spell_scripts.sql"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

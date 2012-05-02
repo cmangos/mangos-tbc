8edcf87fb664cc871f1e8807b44c972dee9e0bd0
Nighoo <nighoo@gmail.com>
2012-05-02 20:54:03 +0200

    [s1568] Revert "Uncomment a needed change of 2654c01."
    commit 3c45bd5 breaks action bars completely on switching stances
    
    This reverts commit 3c45bd510a2d73a2aa295bca5d7b3bc773a76dd1.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2673,7 +2673,7 @@ void Aura::HandleAuraModShapeshift(bool apply, bool Real)
         target->RemoveSpellsCausingAura(SPELL_AURA_MOD_SHAPESHIFT, GetHolder());
 
         // need send to client not form active state, or at re-apply form client go crazy
-        target->SendForcedObjectUpdate();
+        // target->SendForcedObjectUpdate();                -- not need in pre-3.x
 
         if (modelid > 0)
             target->SetDisplayId(modelid);

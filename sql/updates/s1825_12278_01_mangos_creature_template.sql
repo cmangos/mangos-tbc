6b25651bf9ae5d3a1d4abc2e3b3d6ea8be611fa0
Xfurry <xfurry@scriptdev2.com>
2012-12-21 01:51:24 +0200

    Fix the broken SQL file

--- sql/updates/s1825_12278_01_mangos_creature_template.sql
+++ sql/updates/s1825_12278_01_mangos_creature_template.sql
@@ -1,3 +1,3 @@
-ALTER TABLE db_version CHANGE COLUMN required_ required_s1825_12278_01_mangos_creature_template bit;
+ALTER TABLE db_version CHANGE COLUMN required_s1820_12269_12_mangos_gossip_menu_option required_s1825_12278_01_mangos_creature_template bit;
 
 ALTER TABLE creature_template DROP COLUMN spell1, DROP COLUMN spell2, DROP COLUMN spell3, DROP COLUMN spell4;

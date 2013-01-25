8efa44df3b1105da4e413f7d0b795969d655a08c
Cyberium <cyberium@techemail.com>
2013-01-25 08:55:54 +0200

    [s1878] More correct help string for .wp show command.
    Thanks @alexluana for pointing.
    
    (based on commit [12344] - 63f4f24)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_s1350_11716_09_characters_mail"
- #define REVISION_DB_MANGOS "required_s1826_12279_01_mangos_creature_template"
+ #define REVISION_DB_MANGOS "required_s1878_12344_01_mangos_command"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

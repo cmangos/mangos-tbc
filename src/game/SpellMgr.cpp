6030c4308ec1d6344dc846d9e9d7f4e3173a1a49
Xfurry <xfurry@scriptdev2.com>
2013-04-12 22:07:57 +0300

    [s1959] Implement spells 19869 and 20037
    Also add stacking exception for spells 19832 and 23014
    
    (based on commit [12439] - 7d53fcd)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1865,6 +1865,11 @@ bool SpellMgr::IsNoStackSpellDueToSpell(uint32 spellId_1, uint32 spellId_2) cons
                     if (spellInfo_1->SpellIconID == 1662 && spellInfo_2->SpellIconID == 1662)
                         return false;
 
+                    // Possess visual and Possess
+                    if ((spellInfo_1->Id == 23014 && spellInfo_2->Id == 19832) ||
+                            (spellInfo_2->Id == 23014 && spellInfo_1->Id == 19832))
+                        return false;
+
                     break;
                 }
                 case SPELLFAMILY_MAGE:

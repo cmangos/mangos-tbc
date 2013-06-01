dc946fe2c9da2b637e52d60481d42c054bbcea36
Schmoozerd <schmoozerd@scriptdev2.com>
2013-06-01 19:42:22 +0300

    [s2025] Fixup commit s2023 Thanks to Zakamurite for pointing
    Also thanks to him for giving a helping hand in correcting the commit
    
    (based on commit [12513] - 3348191)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4571,6 +4571,7 @@ void Player::RepopAtGraveyard()
     // and don't show spirit healer location
     if (ClosestGrave)
     {
+        bool updateVisibility = IsInWorld() && GetMapId() == ClosestGrave->map_id;
         TeleportTo(ClosestGrave->map_id, ClosestGrave->x, ClosestGrave->y, ClosestGrave->z, GetOrientation());
         if (isDead())                                       // not send if alive, because it used in TeleportTo()
         {
@@ -4581,7 +4582,8 @@ void Player::RepopAtGraveyard()
             data << ClosestGrave->z;
             GetSession()->SendPacket(&data);
         }
-        UpdateVisibilityAndView();
+        if (updateVisibility && IsInWorld())
+            UpdateVisibilityAndView();
     }
 }
 

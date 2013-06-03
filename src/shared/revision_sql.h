16840253bdb6b082eb0c2aa0859af5660417dddb
Xfurry <xfurry@scriptdev2.com>
2013-06-03 20:31:40 +0200

    [s2034] Add database changes to support more data for DB Script texts
    (based on commit [12522] - 2728e04)
    
    Signed-off-by: Schmoozerd <schmoozerd@cmangos.net>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_s1350_11716_09_characters_mail"
- #define REVISION_DB_MANGOS "required_s2010_xxxxx_01_mangos_spell_proc_event"
+ #define REVISION_DB_MANGOS "required_s2034_12522_01_mangos_db_script_string"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__

e4dffaafd32d92bec6e1c89cabdda9053f05b412
Schmoozerd <schmoozerd@cmangos.net>
2014-11-15 01:53:18 +0100

    [s2223] Create function to load active entities.
    Note this function is considered WIP, and will require further work pending more features
    
    Ideas and loop to load all grids on a map by Neo2003.
    
    (based on commit [12783] - aaf514c)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- src/game/Map.h
+++ src/game/Map.h
@@ -188,7 +188,6 @@ class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>
         uint32 GetMaxResetDelay() const;
 
         bool Instanceable() const { return i_mapEntry && i_mapEntry->Instanceable(); }
-        // NOTE: this duplicate of Instanceable(), but Instanceable() can be changed when BG also will be instanceable
         bool IsDungeon() const { return i_mapEntry && i_mapEntry->IsDungeon(); }
         bool IsRaid() const { return i_mapEntry && i_mapEntry->IsRaid(); }
         bool IsRaidOrHeroicDungeon() const { return IsRaid() || GetDifficulty() > DUNGEON_DIFFICULTY_NORMAL; }
@@ -196,6 +195,7 @@ class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>
         bool IsBattleGround() const { return i_mapEntry && i_mapEntry->IsBattleGround(); }
         bool IsBattleArena() const { return i_mapEntry && i_mapEntry->IsBattleArena(); }
         bool IsBattleGroundOrArena() const { return i_mapEntry && i_mapEntry->IsBattleGroundOrArena(); }
+        bool IsContinent() const { return i_mapEntry && i_mapEntry->IsContinent(); }
 
         // can't be NULL for loaded map
         MapPersistentState* GetPersistentState() const { return m_persistentState; }

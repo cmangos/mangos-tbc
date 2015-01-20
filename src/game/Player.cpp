4c691be8fdd3bf6d1911396f2192c53ce9f36399
Salja <salja8840@gmail.com>
2015-01-20 18:09:25 +0100

    Eluna Core Patch and Update
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -9942,6 +9942,10 @@ InventoryResult Player::CanUseItem(ItemPrototype const* pProto) const
         if (getLevel() < pProto->RequiredLevel)
             return EQUIP_ERR_CANT_EQUIP_LEVEL_I;
 
+        InventoryResult eres = sEluna->OnCanUseItem(this, pProto->ItemId);
+        if (eres != EQUIP_ERR_OK)
+            return eres;
+
         return EQUIP_ERR_OK;
     }
     return EQUIP_ERR_ITEM_NOT_FOUND;

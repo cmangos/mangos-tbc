c46c8b856818be31f41d2d2c3a96b58a792520bf
evil-at-wow <evil.at.wow@gmail.com>
2015-03-07 14:00:59 +0100

    [s2255] Fix NPCs with waypoints running too much because of a backport mistake in [s2241].
    Fixes cmangos/issues#619.

--- src/game/movement/MoveSplineInit.cpp
+++ src/game/movement/MoveSplineInit.cpp
@@ -159,7 +159,7 @@ namespace Movement
     MoveSplineInit::MoveSplineInit(Unit& m) : unit(m)
     {
         // mix existing state into new
-        args.flags.runmode = unit.m_movementInfo.HasMovementFlag(MOVEFLAG_WALK_MODE);
+        args.flags.runmode = !unit.m_movementInfo.HasMovementFlag(MOVEFLAG_WALK_MODE);
         args.flags.flying = unit.m_movementInfo.HasMovementFlag((MovementFlags)(MOVEFLAG_CAN_FLY | MOVEFLAG_FLYING | MOVEFLAG_LEVITATING));
     }
 

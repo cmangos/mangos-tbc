81bbfe784afbdd0c9360123fe71606065fe8f001
Salja <salja8840@gmail.com>
2015-03-14 22:33:53 +0100

    Eluna Weather Changes
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/Weather.cpp
+++ src/game/Weather.cpp
@@ -219,6 +219,8 @@ bool Weather::SendWeatherForPlayersInZone(Map const* _map)
 
     ///- Log the event
     LogWeatherState(state);
+
+    sEluna->OnChange(this, m_zone, state, m_grade);
     return true;
 }
 

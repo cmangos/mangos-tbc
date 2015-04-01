f6174f415813d7a203d248ca0a555d409f1fa5a9
evil-at-wow <evil.at.wow@gmail.com>
2015-04-01 20:39:10 +0200

    [s2295] Remove a useless statement from [s2294].
    Thank you for pointing this out, @fgreinus.

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4201,7 +4201,6 @@ SpellCastResult Spell::CheckCast(bool strict)
                         if ((*itr)->GetModifier()->m_miscvalue == 4327)
                         {
                             return SPELL_FAILED_FIZZLE;
-                            itr = auraClassScripts.begin();
                         }
                         else
                             ++itr;

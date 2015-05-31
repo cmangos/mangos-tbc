07f8e761acade6d135baac42ddf70d59d2c0d84c
Xfurry <xfurry@scriptdev2.com>
2015-05-31 18:28:17 +0200

    [s2303] Update spell effect for aura 30019.
    This should also remove aura 30532 when conditions are met.
    
    (based on commit [12874] - 1eb4008)
    
    Signed-off-by: evil-at-wow <evil.at.wow@gmail.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6453,6 +6453,7 @@ void Aura::PeriodicDummyTick()
                     {
                         target->CastSpell(target, 30529, true);
                         target->RemoveAurasDueToSpell(30019);
+                        target->RemoveAurasDueToSpell(30532);
                     }
                     return;
                 }

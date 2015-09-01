d34678b5172f157631b4aaddcfc616ea662edc0b
evil-at-wow <evil.at.wow@gmail.com>
2015-09-02 00:01:02 +0200

    Some minor code changes in preparation of C++11.
    With the introduction of user-defined literals in C++11, code that uses
    format macro constants with no space after the preceding string literal
    is invalid.
    
    Also avoid using code that will be seen by a C++11 compiler as an
    invalid use of a variably-modified type (a variable-length array or
    type derived from it).
    
    (based on cmangos/mangos-wotlk@4011efb)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7442,7 +7442,8 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
         m_speed_rate[mtype] = rate;
         propagateSpeedChange();
 
-        const uint16 SetSpeed2Opc_table[MAX_MOVE_TYPE][2] =
+        typedef const uint16 SpeedOpcodePair[2];
+        SpeedOpcodePair SetSpeed2Opc_table[MAX_MOVE_TYPE] =
         {
             {SMSG_FORCE_WALK_SPEED_CHANGE,        SMSG_SPLINE_SET_WALK_SPEED},
             {SMSG_FORCE_RUN_SPEED_CHANGE,         SMSG_SPLINE_SET_RUN_SPEED},
@@ -7454,13 +7455,15 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
             {SMSG_FORCE_FLIGHT_BACK_SPEED_CHANGE, SMSG_SPLINE_SET_FLIGHT_BACK_SPEED},
         };
 
+        const SpeedOpcodePair& speedOpcodes = SetSpeed2Opc_table[mtype];
+        
         if (forced && GetTypeId() == TYPEID_PLAYER)
         {
             // register forced speed changes for WorldSession::HandleForceSpeedChangeAck
             // and do it only for real sent packets and use run for run/mounted as client expected
             ++((Player*)this)->m_forced_speed_changes[mtype];
 
-            WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][0]), 18);
+            WorldPacket data(Opcodes(speedOpcodes[0]), 18);
             data << GetPackGUID();
             data << (uint32)0;                              // moveEvent, NUM_PMOVE_EVTS = 0x39
             if (mtype == MOVE_RUN)
@@ -7468,7 +7471,7 @@ void Unit::SetSpeedRate(UnitMoveType mtype, float rate, bool forced)
             data << float(GetSpeed(mtype));
             ((Player*)this)->GetSession()->SendPacket(&data);
         }
-        WorldPacket data(Opcodes(SetSpeed2Opc_table[mtype][1]), 12);
+        WorldPacket data(Opcodes(speedOpcodes[1]), 12);
         data << GetPackGUID();
         data << float(GetSpeed(mtype));
         SendMessageToSet(&data, false);

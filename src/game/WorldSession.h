8bb4cda66e14178d93a363716ecd7a0aeaaabb4f
Schluepper <theschluepper@gmail.com>
2016-01-14 20:14:46 +0100

    properly implement logging out player bots

--- src/game/WorldSession.h
+++ src/game/WorldSession.h
@@ -314,8 +314,12 @@ class MANGOS_DLL_SPEC WorldSession
         void SendPlaySpellVisual(ObjectGuid guid, uint32 spellArtKit);
         void SendItemPageInfo(ItemPrototype* itemProto);
 
-        void SetIsBot(bool value) { m_bIsBot = value; }
-        bool GetIsBot() { return m_bIsBot; }
+        // PlayerBot-System
+        void SetPlayerBot(bool value) { m_bPlayerBot = value; }
+        bool GetPlayerBot() { return m_bPlayerBot; }
+        bool IsActivePlayerBot() { return m_bPlayerBot && m_bPlayerBotActive; }
+        bool GetPlayerBotActive() { return m_bPlayerBotActive; }
+        void SetPlayerBotActive(bool value) { m_bPlayerBotActive = value; }
 
         // opcodes handlers
         void Handle_NULL(WorldPacket& recvPacket);          // not used
@@ -779,7 +783,9 @@ class MANGOS_DLL_SPEC WorldSession
         TutorialDataState m_tutorialState;
         ACE_Based::LockedQueue<WorldPacket*> _recvQueue;
 
-        bool m_bIsBot;
+        // PlayerBot-System
+        bool m_bPlayerBot;
+        bool m_bPlayerBotActive;
 };
 #endif
 /// @}

e6718f85691474e0403c6eddfbf37c3feef2f2e6
LilleCarl <lillecarl@gmail.com>
2016-01-28 19:59:16 +0100

    Rewrite TP anticheat, because stupidity is real.

--- src/game/AntiCheat_teleport.cpp
+++ src/game/AntiCheat_teleport.cpp
@@ -1,7 +1,7 @@
 #include "AntiCheat_teleport.h"
 #include "CPlayer.h"
 
-AntiCheat_teleport::AntiCheat_teleport(Player* player) : AntiCheat(player)
+AntiCheat_teleport::AntiCheat_teleport(CPlayer* player) : AntiCheat(player)
 {
 }
 
@@ -15,97 +15,16 @@ bool AntiCheat_teleport::HandleMovement(MovementInfo& moveInfo, Opcodes opcode)
         return false;
     }
 
-    bool startMove = false;
-    bool stopMove = false;
-
-    switch (opcode) // Determine if they're going forwards or backwards
-    {
-    case MSG_MOVE_START_FORWARD:
-    case MSG_MOVE_START_BACKWARD:
-    case MSG_MOVE_START_STRAFE_LEFT:
-    case MSG_MOVE_START_STRAFE_RIGHT:
-    case MSG_MOVE_START_SWIM:
-    case MSG_MOVE_START_ASCEND:
-    case MSG_MOVE_START_DESCEND:
-    case MSG_MOVE_JUMP:
-        startMove = true;
-        break;
-
-    case MSG_MOVE_STOP:
-    case MSG_MOVE_STOP_STRAFE:
-    case MSG_MOVE_STOP_TURN:
-    case MSG_MOVE_STOP_SWIM:
-    case MSG_MOVE_STOP_ASCEND:
-    case MSG_MOVE_FALL_LAND:
-        stopMove = true;
-        break;
-    default:
-        break;
-    }
-
-    if (!startMove && !stopMove)
-        return false;
-
-    MoveType moveType = NONE;
-
-    switch (opcode) // Determine which type of movement they're doing
-    {
-    case MSG_MOVE_START_FORWARD:
-    case MSG_MOVE_START_BACKWARD:
-    case MSG_MOVE_STOP:
-        moveType = MOVEY;
-        break;
-    case MSG_MOVE_START_STRAFE_LEFT:
-    case MSG_MOVE_START_STRAFE_RIGHT:
-    case MSG_MOVE_STOP_STRAFE:
-        moveType = MOVEX;
-        break;
-    case MSG_MOVE_START_SWIM:
-    case MSG_MOVE_STOP_SWIM:
-        moveType = SWIM;
-        break;
-    case MSG_MOVE_START_ASCEND:
-    case MSG_MOVE_START_DESCEND:
-    case MSG_MOVE_STOP_ASCEND:
-        moveType = MOVEZ;
-        break;
-    case MSG_MOVE_JUMP:
-    case MSG_MOVE_FALL_LAND:
-        moveType = JUMP;
-        break;
-    default:
-        break;
-    }
-
-    if (moveType == NONE) // Shouldn't happen, but we don't want this in the container.
-        return false;
-
-    bool moving = false;
-
-    for (auto& i : m_MoveMap) // Check if we're moving in any direction (before this packet was recieved)
-        if (i.second)
-            moving = true;
-
-    if (m_MoveInfo[0].HasMovementFlag(MOVEFLAG_ONTRANSPORT) && m_MoveInfo[1].HasMovementFlag(MOVEFLAG_ONTRANSPORT))
-        moving = true; // Temporarily disable tele anticheat on transport
-
-    if (startMove && !moving && GetDistance3D() > 0.f) // We started moving, wasn't moving before & distance is more then 0, damn cheaters.
+    if (!IsMoving(m_MoveInfo[1]) && IsMoving(m_MoveInfo[0]) && GetDistOrTransportDist() > 0.f)
     {
         const Position* pos = m_MoveInfo[1].GetPos();
 
         m_Player->TeleportTo(m_Player->GetMapId(), pos->x, pos->y, pos->z, pos->o, TELE_TO_NOT_LEAVE_TRANSPORT & TELE_TO_NOT_LEAVE_COMBAT);
-        m_Player->ToCPlayer()->BoxChat << "TELE CHEAT!" << "\n";
 
-        return true;
+        m_Player->BoxChat << "TELE CHEAT" << "\n";
     }
 
-    if (startMove) // Update data if we're moving or not.
-        m_MoveMap[moveType] = true;
-    else if (stopMove)
-        m_MoveMap[moveType] = false;
-
-    if (stopMove) // Only update old movementinfo if we're stopping
-        m_MoveInfo[1] = m_MoveInfo[0];
+    m_MoveInfo[1] = m_MoveInfo[0];
 
     return false;
 }
--- src/game/AntiCheat_teleport.h
+++ src/game/AntiCheat_teleport.h
@@ -2,42 +2,12 @@
 
 #include "AntiCheat.h"
 
-enum MoveType
-{
-    NONE,
-    MOVEY,  // MSG_MOVE_START_FORWARD, MSG_MOVE_START_BACKWARD, MSG_MOVE_STOP
-    MOVEX,  // MSG_MOVE_START_STRAFE_LEFT, MSG_MOVE_START_STRAFE_RIGHT, MSG_MOVE_STOP_STRAFE
-    SWIM,   // MSG_MOVE_START_SWIM, MSG_MOVE_STOP_SWIM
-    MOVEZ,  // MSG_MOVE_START_ASCEND, MSG_MOVE_START_DESCEND, MSG_MOVE_STOP_ASCEND
-    JUMP,   // MSG_MOVE_JUMP, MSG_MOVE_FALL_LAND
-};
-
-/*
-MSG_MOVE_START_FORWARD
-MSG_MOVE_START_BACKWARD
-MSG_MOVE_START_STRAFE_LEFT
-MSG_MOVE_START_STRAFE_RIGHT
-MSG_MOVE_START_SWIM
-MSG_MOVE_START_ASCEND
-MSG_MOVE_START_DESCEND
-MSG_MOVE_JUMP
-
-MSG_MOVE_STOP
-MSG_MOVE_STOP_STRAFE
-MSG_MOVE_STOP_TURN
-MSG_MOVE_STOP_SWIM
-MSG_MOVE_STOP_ASCEND
-*/
-
 class AntiCheat_teleport : public AntiCheat
 {
 public:
-    typedef std::unordered_map<MoveType, bool> MoveMap;
-    AntiCheat_teleport(Player* player);
+    AntiCheat_teleport(CPlayer* player);
 
     bool HandleMovement(MovementInfo& moveInfo, Opcodes opcode);
     bool IsMoving(MovementInfo& moveInfo);
     void HandleTeleport();
-private:
-    MoveMap m_MoveMap;
 };

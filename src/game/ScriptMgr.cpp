ff64e9c6da5d829457e73c0e6658677f4a2a8bec
Xfurry <xfurry@scriptdev2.com>
2016-04-24 23:16:59 +0200

    Update script commands doc for the recent commit
    (based on commit https://github.com/cmangos/mangos-wotlk/commit/83ff2277c3061c445ade30c122849b4d787211dc)
    
    Signed-off-by: Cala <calaftp@free.fr>

--- src/game/ScriptMgr.cpp
+++ src/game/ScriptMgr.cpp
@@ -2051,7 +2051,7 @@ bool ScriptAction::HandleScriptStep()
             }
             break;
         }
-        case SCRIPT_COMMAND_UPDATE_TEMPLATE:
+        case SCRIPT_COMMAND_UPDATE_TEMPLATE:                // 44
         {
             if (LogIfNotCreature(pSource))
                 return false;

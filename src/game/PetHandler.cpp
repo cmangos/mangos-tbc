dc1d6d66689ca7e309a12a7080c07d010025b518
Phatcat <holsthein@live.dk>
2016-07-16 01:35:18 +0200

    Handle owner entering combat properly
    (And set guardian in combat with whatever the unit attacks (if they for
    some reason don't have an AI or their AI differs, otherwise should be
    handled by PetAI)

--- src/game/PetHandler.cpp
+++ src/game/PetHandler.cpp
@@ -125,6 +125,8 @@ void WorldSession::HandlePetAction(WorldPacket& recv_data)
 
                     pet->GetMotionMaster()->Clear();
 
+                    _player->SetInCombatState(true, targetUnit);
+
                     if (((Creature*)pet)->AI())
                     {
                         ((Creature*)pet)->AI()->AttackStart(targetUnit);
@@ -281,6 +283,8 @@ void WorldSession::HandlePetAction(WorldPacket& recv_data)
                     {
                         pet->AttackStop();
                         pet->GetMotionMaster()->Clear();
+
+                        _player->SetInCombatState(true, unit_target);
 
                         if (((Creature*)pet)->AI())
                         {

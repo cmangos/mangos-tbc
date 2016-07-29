2236ddbdb860d1ce8240b6c828232e43cb56e266
darkstalker <slayerbeast@gmail.com>
2016-07-29 23:08:43 +0300

    Backport SMSG_INSTANCE_DIFFICULTY update

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18312,6 +18312,11 @@ void Player::SendInitialPacketsBeforeAddToMap()
     // tutorial stuff
     GetSession()->SendTutorialsData();
 
+    data.Initialize(SMSG_INSTANCE_DIFFICULTY, 4 + 4);
+    data << uint32(GetMap()->GetDifficulty());
+    data << uint32(0);
+    GetSession()->SendPacket(&data);
+
     SendInitialSpells();
 
     data.Initialize(SMSG_SEND_UNLEARN_SPELLS, 4);

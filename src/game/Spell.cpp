bd1769d60a67bb82aa7ad99e68bb7cbacbedbbf9
killerwife <killerwife@gmail.com>
2016-08-31 23:15:30 +0200

    Fix SPELL_ATTR_CASTABLE_WHILE_DEAD impl
    AI hooks cast after creature dies, i.e. need to only check this for
    player, since creature does only what we want it to do.

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -4231,7 +4231,7 @@ SpellCastResult Spell::CheckCast(bool strict)
             return SPELL_FAILED_NOT_READY;
     }
 
-    if (!m_caster->isAlive() && !m_spellInfo->HasAttribute(SPELL_ATTR_CASTABLE_WHILE_DEAD))
+    if (!m_caster->isAlive() && m_caster->GetTypeId() == TYPEID_PLAYER && !m_spellInfo->HasAttribute(SPELL_ATTR_CASTABLE_WHILE_DEAD))
         return SPELL_FAILED_CASTER_DEAD;
 
     // check global cooldown
@@ -6446,7 +6446,7 @@ bool Spell::CheckTarget(Unit* target, SpellEffectIndex eff)
             && m_spellInfo->EffectImplicitTargetA[eff] != TARGET_SCRIPT && m_spellInfo->EffectImplicitTargetA[eff] != TARGET_SELF)
         return false;
 
-    if (!IsAllowingDeadTarget(m_spellInfo) && !target->isAlive() && !(target == m_caster && m_spellInfo->HasAttribute(SPELL_ATTR_CASTABLE_WHILE_DEAD)))
+    if (!IsAllowingDeadTarget(m_spellInfo) && !target->isAlive() && !(target == m_caster && m_spellInfo->HasAttribute(SPELL_ATTR_CASTABLE_WHILE_DEAD)) && m_caster->GetTypeId() == TYPEID_PLAYER)
         return false;
 
     switch (m_spellInfo->Id)

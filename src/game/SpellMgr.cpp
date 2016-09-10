36d368150deabafb505c8b4b6341bf1e53cf9843
Ono <warlockbugs@users.noreply.github.com>
2016-09-10 07:33:46 +0300

    Fix Blessing of Protection: do not stack with other blessings
    Close https://github.com/cmangos/mangos-tbc/pull/117

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -491,7 +491,7 @@ SpellSpecific GetSpellSpecific(uint32 spellId)
             if (IsSealSpell(spellInfo))
                 return SPELL_SEAL;
 
-            if (spellInfo->IsFitToFamilyMask(uint64(0x0000000010000100)))
+            if (spellInfo->IsFitToFamilyMask(uint64(0x0000000010000180)))
                 return SPELL_BLESSING;
 
             if (spellInfo->IsFitToFamilyMask(uint64(0x00000820180400)) && spellInfo->HasAttribute(SPELL_ATTR_EX3_UNK9))

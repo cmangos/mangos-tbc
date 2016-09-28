2a16767e8e27b7bb4a1eaf3f3994b4fadb3c3564
Ono <warlockbugs@users.noreply.github.com>
2016-09-28 16:54:11 +0300

    [s2333] Fix Primal Fury and similar talent issues
    * Fixes issues with spells learned by talents persisting talent reset
    * Talent reset will now always try to reset talents (even if used talent
    count is 0) in attempt to clean up orphaned spells and auras left by
    these spells

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2325_01_realmd"
- #define REVISION_DB_CHARACTERS "required_s2327_01_characters_pvpstats"
+ #define REVISION_DB_CHARACTERS "required_s2333_01_characters_reset_talents"
  #define REVISION_DB_MANGOS "required_s2332_01_mangos_more_spell_data"
 #endif // __REVISION_SQL_H__

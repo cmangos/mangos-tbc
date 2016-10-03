5d7394a9fc8af8b766e8fbd3ccf154b4b014ee64
killerwife <killerwife@gmail.com>
2016-10-03 15:51:44 +0200

    Implement proc system flags
    PROC_FLAG_DONE_SPELL_NONE_DMG_CLASS_POS
    PROC_FLAG_TAKEN_SPELL_NONE_DMG_CLASS_POS
    PROC_FLAG_DONE_SPELL_NONE_DMG_CLASS_NEG
    PROC_FLAG_TAKEN_SPELL_NONE_DMG_CLASS_NEG
    Rename
    PROC_FLAG_DONE_SPELL_MAGIC_DMG_CLASS_POS
    PROC_FLAG_TAKEN_SPELL_MAGIC_DMG_CLASS_POS
    PROC_FLAG_DONE_SPELL_MAGIC_DMG_CLASS_NEG
    PROC_FLAG_TAKEN_SPELL_MAGIC_DMG_CLASS_NEG
    
    Can be expanded upon in the future. Enables correct handling of things
    like Sap interrupting Stealth

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5916,7 +5916,7 @@ void Aura::PeriodicTick()
                 // Set trigger flag
                 uint32 procAttacker = PROC_FLAG_ON_DO_PERIODIC;
                 uint32 procVictim = PROC_FLAG_ON_TAKE_PERIODIC;
-                uint32 procEx = PROC_EX_NORMAL_HIT | PROC_EX_PERIODIC_POSITIVE;
+                uint32 procEx = PROC_EX_NORMAL_HIT | PROC_EX_INTERNAL_HOT;
                 pCaster->ProcDamageAndSpell(target, procAttacker, procVictim, procEx, gain, BASE_ATTACK, spellProto);
 
                 // add HoTs to amount healed in bgs

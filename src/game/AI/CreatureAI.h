8333f477e38f7a04ef4ac0714385f8ea6842c446
killerwife <killerwife@gmail.com>
2016-10-09 19:41:34 +0200

    Implement sending spellInfo in DamageTaken/Done
    Careful, can be nullptr.

--- src/game/AI/CreatureAI.h
+++ src/game/AI/CreatureAI.h
@@ -153,6 +153,7 @@ class MANGOS_DLL_SPEC CreatureAI
          * @param pDoneTo Unit* to whom Damage of amount uiDamage will be dealt
          * @param uiDamage Amount of Damage that will be dealt, can be changed here
          */
+        virtual void DamageDeal(Unit* pDoneTo, uint32& uiDamage, DamageEffectType damagetype, SpellEntry const* spellInfo) { DamageDeal(pDoneTo, uiDamage, damagetype); }
         virtual void DamageDeal(Unit* /*pDoneTo*/, uint32& /*uiDamage*/, DamageEffectType damagetype) {}
 
         /**
@@ -162,6 +163,7 @@ class MANGOS_DLL_SPEC CreatureAI
          * @param pDealer Unit* who will deal Damage to the creature
          * @param uiDamage Amount of Damage that will be dealt, can be changed here
          */
+        virtual void DamageTaken(Unit* pDealer, uint32& uiDamage, DamageEffectType damagetype, SpellEntry const* spellInfo) { DamageTaken(pDealer, uiDamage, damagetype); }
         virtual void DamageTaken(Unit* /*pDealer*/, uint32& /*uiDamage*/, DamageEffectType damagetype) {}
 
         /**

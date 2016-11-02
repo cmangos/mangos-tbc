7f5e352c09a898ed761346cbeb2d2d7c2acb508b
Ono <warlockbugs@users.noreply.github.com>
2016-11-03 01:46:23 +0300

    Implement group member status display
    Allows using following API on group unitIDs which aren't in client's visibility range:
    * UnitIsDead()
    * UnitIsGhost()
    * UnitIsAFK()
    * UnitIsDND()
    * UnitIsPVP()
    * UnitIsPVPFreeForAll()
    
    Also fires proper UI events for these unitids.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1417,6 +1417,9 @@ void Player::SetDeathState(DeathState s)
         if (getClass() == CLASS_WARRIOR)
             CastSpell(this, SPELL_ID_PASSIVE_BATTLE_STANCE, true);
     }
+
+    if (GetGroup())
+        SetGroupUpdateFlag(GROUP_UPDATE_FLAG_STATUS);
 }
 
 bool Player::BuildEnumData(QueryResult* result, WorldPacket* p_data)
@@ -1559,6 +1562,9 @@ void Player::ToggleAFK()
 {
     ToggleFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK);
 
+    if (GetGroup())
+        SetGroupUpdateFlag(GROUP_UPDATE_FLAG_STATUS);
+
     // afk player not allowed in battleground
     if (isAFK() && InBattleGround() && !InArena())
         LeaveBattleground();
@@ -1567,6 +1573,9 @@ void Player::ToggleAFK()
 void Player::ToggleDND()
 {
     ToggleFlag(PLAYER_FLAGS, PLAYER_FLAGS_DND);
+
+    if (GetGroup())
+        SetGroupUpdateFlag(GROUP_UPDATE_FLAG_STATUS);
 }
 
 ChatTagFlags Player::GetChatTag() const
@@ -16799,6 +16808,9 @@ void Player::SetFFAPvP(bool state)
         SetFlag(PLAYER_FLAGS, PLAYER_FLAGS_FFA_PVP);
     else
         RemoveFlag(PLAYER_FLAGS, PLAYER_FLAGS_FFA_PVP);
+
+    if (GetGroup())
+        SetGroupUpdateFlag(GROUP_UPDATE_FLAG_STATUS);
 }
 
 void Player::UpdateDuelFlag(time_t currTime)

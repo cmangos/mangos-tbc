bb1098d03f2659e7f83b2ba3a16dfc6c8d5c013e
Cyberium <cybermomo@techemail.com>
2016-11-03 20:41:09 +0100

    More cleaning

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -8747,8 +8747,7 @@ void Unit::CleanupsBeforeDelete()
 
 CharmInfo* Unit::InitCharmInfo(Unit* charm)
 {
-    if (m_charmInfo)
-        delete m_charmInfo;
+    delete m_charmInfo;
     m_charmInfo = new CharmInfo(charm);
 
     return m_charmInfo;

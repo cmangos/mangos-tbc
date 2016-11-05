67011f71b6bc3c8e5e6f99bfcebcad8cc0d5f1df
killerwife <killerwife@gmail.com>
2016-11-05 14:01:24 +0100

    Implement Brittle bones
    Every 10 seconds 33% chance to cause rattled

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -1991,6 +1991,13 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                         GetHolder()->SetAuraDuration(urand(1, 30)*IN_MILLISECONDS);
                         return;
                     }
+                    case 32441:                             // Brittle Bones
+                    {
+                        m_isPeriodic = true;
+                        m_modifier.periodictime = 10 * IN_MILLISECONDS; // randomly applies Rattled 32437
+                        m_periodicTimer = 0;
+                        return;
+                    }
                     case 33326:                             // Stolen Soul Dispel
                     {
                         target->RemoveAurasDueToSpell(32346);
@@ -6358,6 +6365,10 @@ void Aura::PeriodicDummyTick()
                     return;
                 }
 //              // Gossip NPC Periodic - Talk
+                case 32441:                                 // Brittle Bones
+                    if (roll_chance_i(33))
+                        target->CastSpell(target, 32437, true, nullptr, this);  // Rattled
+                    return;
 //              case 33208: break;
 //              // Gossip NPC Periodic - Despawn
 //              case 33209: break;

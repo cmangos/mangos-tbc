3402380923a49bc9e93d3f534a0cc3e177cf4cb7
namreeb <github@namreeb.org>
2016-11-19 19:34:45 -1000

    [s2338] Give world database control over spell charges on items.  A negative value signifies that the item should be deleted once all charges are consumed.  This rendered the ITEM_EXTRA_NON_CONSUMABLE flag obsolete, therefore it was also removed.
    WARNING: Future readers may feel that this is bad design.  You're right.  It is.  Unfortunately, it is the behavior the client expects.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2325_01_realmd"
  #define REVISION_DB_CHARACTERS "required_s2337_01_characters_weekly_quests"
- #define REVISION_DB_MANGOS "required_s2336_01_mangos_mangle"
+ #define REVISION_DB_MANGOS "required_s2338_01_mangos_item_extraflags"
 #endif // __REVISION_SQL_H__

ce2e3aa1086c7345340fe4bb52e6dac621d9c9b0
Cala <calaftp@free.fr>
2016-12-13 23:49:10 +0100

    Update revision_sql.h
    Forgot this one. Sorry.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2325_01_realmd"
  #define REVISION_DB_CHARACTERS "required_s2337_01_characters_weekly_quests"
- #define REVISION_DB_MANGOS "required_s2340_01_mangos_spell_template"
+ #define REVISION_DB_MANGOS "required_s2341_01_mangos_game_event"
 #endif // __REVISION_SQL_H__

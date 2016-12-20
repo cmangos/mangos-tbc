7ea154991ee456c78a8bd375d02e629d6e8cb812
Cyberium <cyberium@users.noreply.github.com>
2016-12-20 18:47:25 +0100

    Fix sql revision order after last commit

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_s2342_01_mangos_game_event_creature_data` bit(1) DEFAULT NULL
+  `required_s2343_01_mangos_quest_template` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --

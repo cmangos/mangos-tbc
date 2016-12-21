e13c80a9d35794ef1c8c6b2a85575e60cfc51a00
LilleCarl <lillecarl@gmail.com>
2016-12-21 14:09:13 +0100

    Updates to nofall, don't remember. This should be reworked.

--- src/game/AntiCheat_nofall.cpp
+++ src/game/AntiCheat_nofall.cpp
@@ -25,15 +25,18 @@ bool AntiCheat_nofall::HandleMovement(MovementInfo& moveInfo, Opcodes opcode)
     if (isTransport())
         return false;
 
-    if (isFalling())
-        return false;
-
     if (isSwimming())
         return false;
 
+    if (GetDistance2D() < 1.f)
+        return false;
+
+    if (std::abs(GetDistanceZ()) > 0.f)
+        return SetOldMoveInfo(false);
+
     float floorz = pMap->GetHeight(m_Player->GetPositionX(), m_Player->GetPositionY(), m_Player->GetPositionZ());
 
-    if (m_MoveInfo[0].GetPos()->z == m_MoveInfo[1].GetPos()->z && floorz < m_MoveInfo[0].GetPos()->z - JUMPHEIGHT_LAND)
+    if (m_MoveInfo[0].GetPos()->z - floorz > JUMPHEIGHT_LAND)
     {
         const Position* p = m_MoveInfo[1].GetPos();
 
@@ -42,9 +45,7 @@ bool AntiCheat_nofall::HandleMovement(MovementInfo& moveInfo, Opcodes opcode)
         m_Player->BoxChat << "NOFALLCHEAT" << "\n";
     }
 
-    m_MoveInfo[1] = m_MoveInfo[0];
-
-    return false;
+    return SetOldMoveInfo(false);
 }
 
 void AntiCheat_nofall::HandleRelocate(float x, float y, float z, float o)

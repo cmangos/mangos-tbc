c516ccfc03307e6fd1f625821aaa2b894424e0ab
LilleCarl <lillecarl@gmail.com>
2017-01-29 00:41:36 +0100

    Optimize out coping of MovementInfo for fly and waterwalking anticheat. [skip ci]

--- src/game/AntiCheat_fly.cpp
+++ src/game/AntiCheat_fly.cpp
@@ -7,26 +7,24 @@ AntiCheat_fly::AntiCheat_fly(CPlayer* player) : AntiCheat(player)
 
 bool AntiCheat_fly::HandleMovement(MovementInfo& MoveInfo, Opcodes opcode, bool cheat)
 {
-    AntiCheat::HandleMovement(MoveInfo, opcode, cheat);
-
     if (!Initialized())
     {
-        m_LastCanFlyTime = newMoveInfo.GetTime();
-        return SetOldMoveInfo(false);
+        m_LastCanFlyTime = MoveInfo.GetTime();
+        return false;
     }
 
     if (CanFly())
-        m_LastCanFlyTime = newMoveInfo.GetTime();
+        m_LastCanFlyTime = MoveInfo.GetTime();
 
-    if (isFlying(newMoveInfo) && !CanFly() && m_LastCanFlyTime + 500 < newMoveInfo.GetTime())
+    if (isFlying(MoveInfo) && !CanFly() && m_LastCanFlyTime + 500 < MoveInfo.GetTime())
     {
         if (m_Player->GetSession()->GetSecurity() > SEC_PLAYER)
             m_Player->BoxChat << "FLY CHEAT" << "\n";
 
         m_Player->SetCanFly(false);
 
-        return SetOldMoveInfo(true);
+        return true;
     }
 
-    return SetOldMoveInfo(false);
+    return false;
 }
--- src/game/AntiCheat_waterwalking.cpp
+++ src/game/AntiCheat_waterwalking.cpp
@@ -10,20 +10,18 @@ AntiCheat_waterwalking::AntiCheat_waterwalking(CPlayer* player) : AntiCheat(play
 
 bool AntiCheat_waterwalking::HandleMovement(MovementInfo& MoveInfo, Opcodes opcode, bool cheat)
 {
-    AntiCheat::HandleMovement(MoveInfo, opcode, cheat);
-
     if (!Initialized())
-        return SetOldMoveInfo(false);
+        return false;
 
-    if (!cheat && !m_Player->HasAuraType(SPELL_AURA_WATER_WALK) && !m_Player->HasAuraType(SPELL_AURA_GHOST) && newMoveInfo.HasMovementFlag(MOVEFLAG_WATERWALKING))
+    if (!cheat && !m_Player->HasAuraType(SPELL_AURA_WATER_WALK) && !m_Player->HasAuraType(SPELL_AURA_GHOST) && MoveInfo.HasMovementFlag(MOVEFLAG_WATERWALKING))
     {
         m_Player->SetWaterWalk(false);
 
         if (m_Player->GetSession()->GetSecurity() > SEC_PLAYER)
             m_Player->BoxChat << "WATERWALK CHEAT" << "\n";
 
-        return SetOldMoveInfo(true);
+        return true;
     }
 
-    return SetOldMoveInfo(false);
+    return false;
 }

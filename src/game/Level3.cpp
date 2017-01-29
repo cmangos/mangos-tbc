6e9120940df7f1c373d59c147dbb3ce9e74e6cda
LilleCarl <lillecarl@gmail.com>
2017-01-29 22:31:05 +0100

    Implement SMSG_COOLDOWN_CHEAT, so i remember how to use it if i ever find reason to.

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -1315,6 +1315,11 @@ bool ChatHandler::HandleCooldownCommand(char* args)
     if (!*args)
     {
         target->RemoveAllSpellCooldown();
+
+        WorldPacket data(SMSG_COOLDOWN_CHEAT, 8);
+        data << target->GetObjectGuid();
+        m_session->SendPacket(data);
+
         PSendSysMessage(LANG_REMOVEALL_COOLDOWN, tNameLink.c_str());
     }
     else

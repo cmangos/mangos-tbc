da8e8e6d7c4939ae426782e9374f52e57a1fddf1
killerwife <killerwife@gmail.com>
2017-02-14 17:55:36 +0100

    [s2358] Implement event group

--- sql/base/characters.sql
+++ sql/base/characters.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_s2357_01_characters_spell_cooldown` bit(1) DEFAULT NULL
+  `required_s2358_01_characters_event_group_chosen` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Last applied sql update to DB';
 
 --
--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_s2356_01_mangos_dbscript_string_rename` bit(1) DEFAULT NULL
+  `required_s2358_01_mangos_game_event_group` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --

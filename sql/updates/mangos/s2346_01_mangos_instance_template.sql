3f1be51ae3aac641824fd08411f94a0625e3c80b
boxa <boxa@mail.ru>
2017-02-16 22:37:37 +0300

    Add deadmines map id into update

--- sql/updates/mangos/s2346_01_mangos_instance_template.sql
+++ sql/updates/mangos/s2346_01_mangos_instance_template.sql
@@ -5,5 +5,5 @@ ADD COLUMN mountAllowed tinyint(3) UNSIGNED NOT NULL DEFAULT 0 AFTER ScriptName;
 
 UPDATE instance_template SET mountAllowed = 1 WHERE map IN
 (
-    209, 269, 309, 509, 534, 560, 564, 568, 580
+    36, 209, 269, 309, 509, 534, 560, 564, 568, 580
 );

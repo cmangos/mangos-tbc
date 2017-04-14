8cde5229e98c3371785842bf04cb6ad35c50125b
Cyberium <cyberium@users.noreply.github.com>
2017-04-14 09:32:23 +0200

    Add check for spawntimesecsmax at db load
    Set it to spawntimesecsmin if its loawer than that value.

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -1326,6 +1326,13 @@ void ObjectMgr::LoadCreatures()
             continue;
         }
 
+        if (data.spawntimesecsmax < data.spawntimesecsmin)
+        {
+            sLog.outErrorDb("Table `creature` have creature (GUID: %u Entry: %u) with `spawntimesecsmax` (%u) value lower than `spawntimesecsmin` (%u), it will be adjusted to %u.",
+                guid, data.id, uint32(data.spawntimesecsmax), uint32(data.spawntimesecsmin), uint32(data.spawntimesecsmin));
+            data.spawntimesecsmax = data.spawntimesecsmin;
+        }
+
         if (mapEntry->IsDungeon())
         {
             if (data.spawnMask & ~SPAWNMASK_DUNGEON_ALL)
@@ -1545,6 +1552,13 @@ void ObjectMgr::LoadGameObjects()
         if (data.spawntimesecsmin == 0 && gInfo->IsDespawnAtAction())
         {
             sLog.outErrorDb("Table `gameobject` have gameobject (GUID: %u Entry: %u) with `spawntimesecs` (0) value, but gameobejct marked as despawnable at action.", guid, data.id);
+        }
+
+        if (data.spawntimesecsmax < data.spawntimesecsmin)
+        {
+            sLog.outErrorDb("Table `gameobject` have gameobject (GUID: %u Entry: %u) with `spawntimesecsmax` (%u) value lower than `spawntimesecsmin` (%u), it will be adjusted to %u.",
+                guid, data.id, uint32(data.spawntimesecsmax), uint32(data.spawntimesecsmin), uint32(data.spawntimesecsmin));
+            data.spawntimesecsmax = data.spawntimesecsmin;
         }
 
         if (go_state >= MAX_GO_STATE)

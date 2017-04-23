90ab945ac2ef8260896c5c7f1bb7c117629a8162
killerwife <killerwife@gmail.com>
2017-04-23 18:24:45 +0200

    [s2354] Implement dbscripts_on_relay

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2325_01_realmd"
  #define REVISION_DB_CHARACTERS "required_s2352_01_characters_fix_revision"
- #define REVISION_DB_MANGOS "required_s2353_01_mangos_spell_affect"
+ #define REVISION_DB_MANGOS "required_s2354_01_mangos_dbscripts_on_relay"
 #endif // __REVISION_SQL_H__

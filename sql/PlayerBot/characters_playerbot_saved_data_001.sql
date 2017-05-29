dd8ede50cb42dc38727ca78773450825c649f4a3
Cala <calaftp@free.fr>
2017-05-29 23:08:48 +0200

    Playerbot: code cleanup

--- sql/PlayerBot/characters_playerbot_saved_data_001.sql
+++ sql/PlayerBot/characters_playerbot_saved_data_001.sql
@@ -9,18 +9,18 @@
 
 -- When updating, don't forget to update revision_sql.h
 
-DROP TABLE IF EXISTS `playerbotai_db_version`;
-CREATE TABLE `playerbotai_db_version` (
+DROP TABLE IF EXISTS `playerbot_db_version`;
+CREATE TABLE `playerbot_db_version` (
   `version` varchar(120) default NULL,
-  `required_5_playerbotai_auto_follow` bit(1) default NULL
+  `characters_playerbot_saved_data_001` bit(1) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 
 --
 -- Dumping data for table `playerbotai_db_version`
 --
 
-LOCK TABLES `playerbotai_db_version` WRITE;
-INSERT INTO `playerbotai_db_version` VALUES
+LOCK TABLES `playerbot_db_version` WRITE;
+INSERT INTO `playerbot_db_version` VALUES
 ('PlayerbotAI default database.',NULL);
 UNLOCK TABLES;
 

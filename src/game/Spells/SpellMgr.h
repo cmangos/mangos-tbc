3a674991e7bd4e8e79db9b2f55b7e17ad11fe2d7
Cyberium <cyberium@users.noreply.github.com>
2017-06-30 19:29:39 +0200

    TARGET_SINGLE_ENEMY seem badly named
    For spell 47750 it can be both enemy or ally.
    We may have to rename that target.
    
    For now removing it from implicitly negative target is a good idea to fix some issue.
    
    close https://github.com/cmangos/issues/issues/78

--- src/game/Spells/SpellMgr.h
+++ src/game/Spells/SpellMgr.h
@@ -742,7 +742,6 @@ inline bool IsHostileTarget(uint32 target)
         case TARGET_ALL_HOSTILE_UNITS_AROUND_CASTER:
         case TARGET_CURRENT_ENEMY_COORDINATES:
         case TARGET_LARGE_FRONTAL_CONE:
-        case TARGET_SINGLE_ENEMY:
             return true;
         default:
             break;

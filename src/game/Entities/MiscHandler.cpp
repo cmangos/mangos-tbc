da7ad42862eb69592c0117b8e990e77b904f8d24
killerwife <killerwife@gmail.com>
2017-08-28 12:32:37 +0200

    Fix inspect logic
    CanInteract is only usable for NPCs. For players it seems to require
    CanAttack because thats when players turn yellow/red.

--- src/game/Entities/MiscHandler.cpp
+++ src/game/Entities/MiscHandler.cpp
@@ -998,7 +998,7 @@ void WorldSession::HandleInspectOpcode(WorldPacket& recv_data)
     if (!_player->IsWithinDistInMap(plr, INSPECT_DISTANCE, false))
         return;
 
-    if (!_player->CanInteract(plr))
+    if (_player->CanAttack(plr))
         return;
 
     uint32 talent_points = 0x3D;
@@ -1092,7 +1092,7 @@ void WorldSession::HandleInspectHonorStatsOpcode(WorldPacket& recv_data)
     if (!_player->IsWithinDistInMap(player, INSPECT_DISTANCE, false))
         return;
 
-    if (!_player->CanInteract(player))
+    if (_player->CanAttack(player))
         return;
 
     WorldPacket data(MSG_INSPECT_HONOR_STATS, 8 + 1 + 4 * 4);

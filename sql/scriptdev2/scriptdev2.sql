30702f490f61e446534b4b776be24002ca2607db
Saltgurka <saltgurkasalt@gmail.com>
2017-09-05 23:09:41 +0200

    Remove SD2 Script for quest 10852 (Now done in DB)
    * New script: https://github.com/cmangos/tbc-db/commit/56a759ab23fda52b3bd4b6d34b8d81d11ab8c8c9

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -76,7 +76,6 @@ UPDATE gameobject_template SET ScriptName='go_ethereum_stasis' WHERE entry=18459
 UPDATE gameobject_template SET ScriptName='go_ethereum_stasis' WHERE entry BETWEEN 185461 AND 185464;
 UPDATE gameobject_template SET ScriptName='go_jump_a_tron' WHERE entry=183146;
 UPDATE gameobject_template SET ScriptName='go_andorhal_tower' WHERE entry IN (176094,176095,176096,176097);
-UPDATE gameobject_template SET ScriptName='go_veil_skith_cage' WHERE entry IN (185202,185203,185204,185205);
 
 /* GUARD */
 UPDATE creature_template SET ScriptName='guard_azuremyst' WHERE entry=18038;
@@ -972,7 +971,6 @@ UPDATE creature_template SET ScriptName='mob_unkor_the_ruthless' WHERE entry=182
 UPDATE creature_template SET ScriptName='npc_akuno' WHERE entry=22377;
 UPDATE creature_template SET ScriptName='npc_hungry_nether_ray' WHERE entry=23439;
 UPDATE creature_template SET ScriptName='npc_letoll' WHERE entry=22458;
-UPDATE creature_template SET ScriptName='npc_captive_child' WHERE entry=22314;
 UPDATE creature_template SET ScriptName='npc_isla_starmane' WHERE entry=18760;
 UPDATE creature_template SET ScriptName="npc_skywing" WHERE entry=22424;
 UPDATE creature_template SET ScriptName="npc_cenarion_sparrowhawk" WHERE entry=22972;
@@ -1684,10 +1682,10 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,commen
 
 (-1000589,'Kroshius live? Kroshius crush!',0,1,0,0,'SAY_KROSHIUS_REVIVE'),
 
-(-1000590,'Woot!',0,0,0,0,'Captive Child SAY_THANKS_1'),
-(-1000591,'I think those weird bird guys were going to eat us. Gross!',0,0,0,0,'Captive Child SAY_THANKS_2'),
-(-1000592,'Yay! We\'re free!',0,0,0,0,'Captive Child SAY_THANKS_3'),
-(-1000593,'Gross!',0,0,0,0,'Captive Child SAY_THANKS_4'),
+(-1000590,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000591,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000592,'REUSE_ME',0,0,0,0,'REUSE_ME'),
+(-1000593,'REUSE_ME',0,0,0,0,'REUSE_ME'),
 
 (-1000603,'Do not test me, scurvy dog! I\'m trained in the way of the Blood Knights!',0,0,0,0,'silvermoon harry SAY_AGGRO'),
 (-1000604,'I\'ll pay! I\'ll pay! Eeeek! Please don\'t hurt me!',0,0,0,0,'silvermoon harry SAY_BEATEN'),

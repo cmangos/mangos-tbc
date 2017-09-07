3cdf3f48333dffff7226f5f9bbf94420a5b9b973
killerwife <killerwife@gmail.com>
2017-09-07 18:06:43 +0200

    [s2361] Add questgiver_greeting and locales to properly handle SMSG_QUESTGIVER_QUEST_LIST
    TODO: Remove npc_gossip

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2325_01_realmd"
  #define REVISION_DB_CHARACTERS "required_s2359_01_characters_account_instances_entered"
- #define REVISION_DB_MANGOS "required_s2360_01_mangos_comments"
+ #define REVISION_DB_MANGOS "required_s2361_01_mangos_questgiver_greeting"
 #endif // __REVISION_SQL_H__

2d602c3ed9d0549c1f5dfaadc1441923a8f669bc
killerwife <killerwife@gmail.com>
2017-10-18 16:59:34 +0200

    Implement GetDistanceNoBoundingRadius and fix spell travelling distance calc
    Spells use raw distance without bounding radius or combat reach for
    calculating animation travel distance. This bug was most evident on Void
    Reaver (who has bounding radius 10, combat reach 15) and uses a delayed
    spell called Arcane Orb which is a delayed slow missile. It had up to 2
    seconds delay in comparison to client.

--- src/game/Entities/Object.cpp
+++ src/game/Entities/Object.cpp
@@ -1052,6 +1052,15 @@ float WorldObject::GetDistance(float x, float y, float z) const
     return (dist > 0 ? dist : 0);
 }
 
+float WorldObject::GetDistanceNoBoundingRadius(float x, float y, float z) const
+{
+    float dx = GetPositionX() - x;
+    float dy = GetPositionY() - y;
+    float dz = GetPositionZ() - z;
+    float dist = sqrt((dx * dx) + (dy * dy) + (dz * dz));
+    return (dist > 0 ? dist : 0);
+}
+
 float WorldObject::GetCombatDistance(const WorldObject * obj, bool forMeleeRange) const
 {
     float radius = GetCombinedCombatReach(obj, forMeleeRange);

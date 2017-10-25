98d59aa1e740a8a242f311db0840ee782a1312dd
killerwife <killerwife@gmail.com>
2017-10-26 00:14:59 +0200

    Fix initial taxi not applying discount and add TODO

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -17705,7 +17705,11 @@ bool Player::ActivateTaxiPathTo(std::vector<uint32> const& nodes, Creature* npc 
     uint32 money = GetMoney();
 
     if (npc)
-        totalcost = (uint32)ceil(totalcost * GetReputationPriceDiscount(npc));
+    {
+        float discount = GetReputationPriceDiscount(npc);
+        totalcost = (uint32)ceil(totalcost * discount);
+        firstcost = (uint32)ceil(firstcost * discount);
+    }
 
     if (money < totalcost)
     {

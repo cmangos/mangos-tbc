28a3e8efa46c6cb6271ffaad2fca88146754f51c
killerwife <killerwife@gmail.com>
2017-11-05 21:23:02 +0100

    Add assistability check to unittarget of chain heal
    This is checked clientside but it need to be rechecked anyway.

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -2508,6 +2508,9 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
             if (!pUnitTarget)
                 break;
 
+            if (!m_caster->CanAssistSpell(pUnitTarget, m_spellInfo))
+                break;
+
             if (EffectChainTarget <= 1)
                 targetUnitMap.push_back(pUnitTarget);
             else

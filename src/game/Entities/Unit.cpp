bc02c0bb7fa8faa846b8a31445e3f0589463b543
Killerwife <killerwife@gmail.com>
2018-01-29 00:25:16 +0100

    Improve handling of magnet target
    Magnet target does not have to fulfill all spell rules if its valid for
    a given spell. For example grounding totem is not a valid target for
    polymorph.

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -6617,7 +6617,7 @@ Unit* Unit::SelectMagnetTarget(Unit* victim, Spell* spell, SpellEffectIndex eff)
         {
             if (Unit* magnet = (*itr)->GetCaster())
             {
-                if (magnet->isAlive() && magnet->IsWithinLOSInMap(this) && spell->CheckTarget(magnet, eff))
+                if (magnet->isAlive() && magnet->IsWithinLOSInMap(this) && spell->CheckTarget(magnet, eff, EXCEPTION_MAGNET))
                 {
                     if (SpellAuraHolder* holder = (*itr)->GetHolder())
                         if (holder->DropAuraCharge())
@@ -6638,7 +6638,7 @@ Unit* Unit::SelectMagnetTarget(Unit* victim, Spell* spell, SpellEffectIndex eff)
         {
             if (Unit* magnet = (*i)->GetCaster())
             {
-                if (magnet->isAlive() && magnet->IsWithinLOSInMap(this) && (!spell || spell->CheckTarget(magnet, eff)))
+                if (magnet->isAlive() && magnet->IsWithinLOSInMap(this) && (!spell || spell->CheckTarget(magnet, eff, EXCEPTION_MAGNET)))
                 {
                     if (roll_chance_i((*i)->GetModifier()->m_amount))
                     {

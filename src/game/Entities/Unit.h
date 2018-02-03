e9797ed15113bacc136806cc7c098a83845d3e02
Killerwife <killerwife@gmail.com>
2018-02-03 15:32:59 +0100

    Implement Pet Dismiss sounds

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -2212,6 +2212,7 @@ class Unit : public WorldObject
         void SendPetActionFeedback(uint8 msg) const;
         void SendPetTalk(uint32 pettalk) const;
         void SendPetAIReaction() const;
+        void SendPetDismiss(uint32 soundId) const;
         ///----------End of Pet responses methods----------
 
         void propagateSpeedChange() { GetMotionMaster()->propagateSpeedChange(); }

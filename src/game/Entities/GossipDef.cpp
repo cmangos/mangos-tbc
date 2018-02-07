fec69e791650f2de8b1cc05a3981cd3f60715e76
killerwife <killerwife@gmail.com>
2018-02-07 10:06:02 +0100

    Simply logic for close on cancel for quests

--- src/game/Entities/GossipDef.cpp
+++ src/game/Entities/GossipDef.cpp
@@ -766,10 +766,7 @@ void PlayerMenu::SendQuestGiverRequestItems(Quest const* pQuest, ObjectGuid npcG
         data << pQuest->GetIncompleteEmote();
 
     // Close Window after cancel
-    if (CloseOnCancel)
-        data << uint32(0x01);                               // auto finish
-    else
-        data << uint32(0x00);
+    data << uint32(CloseOnCancel);                          // auto finish
 
     data << uint32(pQuest->GetSuggestedPlayers());          // SuggestedGroupNum
 

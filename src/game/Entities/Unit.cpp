4526cce35de316f39ac183a35227eb71bb19c0fd
Killerwife <killerwife@gmail.com>
2018-02-22 12:16:22 +0100

    Fix crash due to AttackedBy being called when dead
    PetAI is critical in this because charminfo is already deleted.
    Logically this should never be executed when mob is dead.

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -5914,6 +5914,9 @@ bool Unit::IsNeutralToAll() const
 
 void Unit::AttackedBy(Unit* attacker)
 {
+    if (!isAlive())
+        return;
+
     // trigger AI reaction
     if (AI())
         AI()->AttackedBy(attacker);

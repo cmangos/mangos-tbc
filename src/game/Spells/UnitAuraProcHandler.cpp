b51c33a11631d068c0cf03dd4bd2b144dfb5c7f3
Killerwife <killerwife@gmail.com>
2018-03-01 16:25:42 +0100

    Fix Master of Elements proccing more that once per spell

--- src/game/Spells/UnitAuraProcHandler.cpp
+++ src/game/Spells/UnitAuraProcHandler.cpp
@@ -1114,7 +1114,7 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(ProcExecutionData& data)
             // Master of Elements
             if (dummySpell->SpellIconID == 1920)
             {
-                if (!procSpell)
+                if (!procSpell || data.spell->GetScriptValue() == 1)
                     return SPELL_AURA_PROC_FAILED;
 
                 // mana cost save
@@ -1125,6 +1125,7 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(ProcExecutionData& data)
 
                 target = this;
                 triggered_spell_id = 29077;
+                data.spell->SetScriptValue(1); // can only happen once per spell
                 break;
             }
 

9b0c47df7f914e32d1452000dfe6bd6b71a26374
Killerwife <killerwife@gmail.com>
2018-03-01 10:57:39 +0100

    Relations Stage 2 Rollout: NPC can hit neutrals with AOE
    Confirmed on Azuregos and plenty more creatures.
    Closes https://github.com/cmangos/issues/issues/1572

--- src/game/Entities/Relations.cpp
+++ src/game/Entities/Relations.cpp
@@ -1040,8 +1040,6 @@ bool Unit::CanAttackSpell(Unit* target, SpellEntry const* spellInfo, bool isAOE)
                     return false;
                 }
             }
-            else
-                return IsEnemy(target); // non player controlled can only hit hostiles with AOE
         }
 
         return true;

7bb21796e6a440c6bae1f7c2e8a09de750cff4c4
MantisLord <sabinprosper@gmail.com>
2018-03-11 21:46:23 -0400

    Implement Scarlet Monastery Corrupted Ashbringer event
    TODO:
    - Sounds are supposed to be heard by other players?
    - Chapel door should open automatically

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -1366,6 +1366,19 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                     }
                     return;
                 }
+                case 28414:                                 // Call of the Ashbringer
+                {
+                    if (!m_caster || m_caster->GetTypeId() != TYPEID_PLAYER)
+                        return;
+                    static constexpr uint32 AshbringerSounds[12] = { 8906,8907,8908,8920,8921,8922,8923,8924,8925,8926,8927,8928 };
+                    m_caster->PlayDirectSound(AshbringerSounds[urand(0, 11)]);
+                    return;
+                }
+                case 28697:                                 // Forgiveness
+                {
+                    m_caster->DealDamage(unitTarget, unitTarget->GetHealth(), nullptr, DIRECT_DAMAGE, SPELL_SCHOOL_MASK_NORMAL, nullptr, false, m_damageInfo);
+                    return;
+                }
                 case 28730:                                 // Arcane Torrent (Mana)
                 {
                     Aura* dummy = m_caster->GetDummyAura(28734);

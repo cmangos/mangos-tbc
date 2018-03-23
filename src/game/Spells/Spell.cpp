2d0d03fc7d9a535018bdbf91afb25f3f6b8f562e
keyshuwen <285196897@qq.com>
2018-03-23 22:04:12 +0800

    Core/Spell: possible crash fix.

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -6024,6 +6024,9 @@ SpellCastResult Spell::CheckCasterAuras() const
 
 bool Spell::CanAutoCast(Unit* target)
 {
+    if (!target)
+        return false;
+
     ObjectGuid targetguid = target->GetObjectGuid();
 
     for (int j = 0; j < MAX_EFFECT_INDEX; ++j)

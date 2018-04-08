182e4462f70038c3cece52383629ef9a7d215d86
Killerwife <killerwife@gmail.com>
2018-04-08 19:15:29 +0200

    Implement awarding of script titles
    Titles implemented:
    Conqueror, Justicar, Hand of Adal, Champion of the Naaru

--- src/game/Entities/Player.h
+++ src/game/Entities/Player.h
@@ -2125,6 +2125,7 @@ class Player : public Unit
         DeclinedName const* GetDeclinedNames() const { return m_declinedname; }
         bool HasTitle(uint32 bitIndex) const;
         bool HasTitle(CharTitlesEntry const* title) const { return HasTitle(title->bit_index); }
+        void SetTitle(uint32 titleId, bool lost = false);
         void SetTitle(CharTitlesEntry const* title, bool lost = false);
 
 #ifdef BUILD_PLAYERBOT

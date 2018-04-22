e94b019e24d767fed84576644af2f7bfe009f5af
Killerwife <killerwife@gmail.com>
2018-04-22 09:38:03 +0200

    Implement Terokk script

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -1067,6 +1067,9 @@ UPDATE creature_template SET ScriptName='npc_avatar_of_terokk' WHERE entry=22375
 UPDATE creature_template SET ScriptName='npc_minion_of_terokk' WHERE entry=22376;
 UPDATE creature_template SET ScriptName='npc_draenei_tomb_guardian' WHERE entry=22285;
 UPDATE creature_template SET ScriptName='npc_vengeful_harbinger' WHERE entry=21638;
+UPDATE creature_template SET ScriptName='boss_terokk' WHERE entry IN(21838);
+INSERT INTO scripted_event_id VALUES
+(15014, 'event_summon_terokk');
 
 /* THOUSAND NEEDLES */
 UPDATE creature_template SET ScriptName='npc_kanati' WHERE entry=10638;
@@ -2330,7 +2333,14 @@ INSERT INTO script_texts (entry,content_default,sound,type,language,emote,commen
 (-1001274,'Now you''re gonna get it good, $n!', '0', '0', '0', '0','dashel stonefist SAY_STONEFIST_1'),
 (-1001275,'Okay, okay! Enough fighting. No one else needs to get hurt.', '0', '0', '0', '0','dashel stonefist SAY_STONEFIST_2'),
 (-1001276,'It''s okay, boys. Back off. You''ve done enough. I''ll meet up with you later.', '0', '0', '0', '0','dashel stonefist SAY_STONEFIST_3'),
-(-1001277,'That could have gone better. $n, come here, please. I have something I need you to take care of for me.','0','4','0','0','Ambassador Sunsorrow - During Lament of the Highborne script');
+(-1001277,'That could have gone better. $n, come here, please. I have something I need you to take care of for me.','0','4','0','0','Ambassador Sunsorrow - During Lament of the Highborne script'),
+(-1001278,'Who calls me to this world?  The stars are not yet aligned... my powers fail me!  You will pay for this!',0,0,0,1,'Terokk SAY_SPAWN'),
+(-1001279,'Show me what you''re made of, $n!',0,6,0,1,'Terokk SAY_CHOSEN_ONE'),
+(-1001280,'Kwa! You cannot kill me, I am immortal!',0,6,0,1,'Terokk SAY_DIVINE_SHIELD'),
+(-1001281,'%s becomes enraged as his shield shatters.',0,3,0,1,'Terokk SAY_ENRAGE'),
+(-1001282,'Enemy sighted!  Fall into formation and prepare for bombing maneuvers!',0,6,0,1,'Skyguard Ace SAY_SPAWN'),
+(-1001283,'Quickly! Use the flames and support the ground troops. Its ancient magic should cleanse Terokk''s shield.',0,6,0,1,'Skyguard Ace SAY_FLAMES'),
+(-1001284,'They did it!  Enemy down!  Return to base!',0,6,0,1,'Skyguard Ace SAY_TEROKK_DOWN');
 
 -- -1 010 000 Classic texts
 

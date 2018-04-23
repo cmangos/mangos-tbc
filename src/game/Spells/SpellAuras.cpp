33a99ec762c54dd110429ab0e94c54125d65b116
Warlockbugs <warlockbugs@outlook.com>
2018-04-23 08:15:56 +0300

    Aura Flags: Fix bnot and parenthesis typo
    Well... this is embarassing.

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -7729,7 +7729,7 @@ void SpellAuraHolder::SetAuraFlag(uint32 slot, bool add)
     uint32 index    = slot / 4;
     uint32 byte     = (slot % 4) * 8;
     uint32 val      = m_target->GetUInt32Value(UNIT_FIELD_AURAFLAGS + index);
-    val &= ~(uint32(AFLAG_MASK_ALL) << byte);
+    val &= (~uint32(AFLAG_MASK_ALL) << byte);
     if (add)
     {
         const Unit* caster = GetCaster();

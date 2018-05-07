da24323a830c685c51c8a4b0adf47ac7f12251bc
dev.maximus <dev.maximus@gmail.com>
2018-05-07 12:55:26 -0700

    move threatmanager support to unit level in preparation for charmai we need both player and creature threatmanager support

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -2086,6 +2086,7 @@ class Unit : public WorldObject
         void DeleteThreatList();
         bool IsSecondChoiceTarget(Unit* pTarget, bool checkThreatArea);
         bool SelectHostileTarget();
+        bool IsOutOfThreatArea(Unit* victim) const;
         void TauntUpdate();
         void FixateTarget(Unit* taunter);
         ObjectGuid GetFixateTargetGuid() const { return m_fixateTargetGuid; }

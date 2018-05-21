50f01a45bb1a02acbbbfa3f7c4b66d5791a69c11
Warlockbugs <warlockbugs@outlook.com>
2018-05-21 20:21:47 +0300

    Implement Taxi Time Test benchmark serverside support
    * Client command /timetest now toggles flag PLAYER_FLAGS_TAXI_BENCHMARK
    * Add PLAYER_FLAGS_TAXI_BENCHMARK flag to cleanup list

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -2622,7 +2622,7 @@ void Player::InitStatsForLevel(bool reapplyMods)
     SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);    // must be set
 
     // cleanup player flags (will be re-applied if need at aura load), to avoid have ghost flag without ghost aura, for example.
-    RemoveFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK | PLAYER_FLAGS_DND | PLAYER_FLAGS_GM | PLAYER_FLAGS_GHOST | PLAYER_FLAGS_PVP_DESIRED | PLAYER_FLAGS_FFA_PVP);
+    RemoveFlag(PLAYER_FLAGS, PLAYER_FLAGS_AFK | PLAYER_FLAGS_DND | PLAYER_FLAGS_GM | PLAYER_FLAGS_GHOST | PLAYER_FLAGS_PVP_DESIRED | PLAYER_FLAGS_FFA_PVP | PLAYER_FLAGS_TAXI_BENCHMARK);
 
     RemoveStandFlags(UNIT_STAND_FLAGS_ALL);                 // one form stealth modified bytes
 

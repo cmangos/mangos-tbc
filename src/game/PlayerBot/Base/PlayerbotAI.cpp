db0191b8353f27c94b3b339bcbe9fcaa1dbdeca4
cala <cala@users.noreply.github.com>
2018-06-21 16:56:08 +0200

    More consistency in the naming of GameObject methods
    * A handful of methods from GameObject where starting by a lower case while we use upper case.
    An exception is made for isVisibleForInState() because a similar method with the same spelling is used by other objects, so this spelling is kept for consistency.

--- src/game/PlayerBot/Base/PlayerbotAI.cpp
+++ src/game/PlayerBot/Base/PlayerbotAI.cpp
@@ -3707,7 +3707,7 @@ void PlayerbotAI::DoLoot()
     GameObject* go = m_bot->GetMap()->GetGameObject(m_lootCurrent);
 
     // clear creature or object that is not spawned or if not creature or object
-    if ((c && c->IsDespawned()) || (go && !go->isSpawned()) || (!c && !go))
+    if ((c && c->IsDespawned()) || (go && !go->IsSpawned()) || (!c && !go))
     {
         m_lootCurrent = ObjectGuid();
         return;
@@ -6422,7 +6422,7 @@ void PlayerbotAI::findNearbyGO()
 
             float ground_z = map->GetHeightStatic(go->GetPositionX(), go->GetPositionY(), go->GetPositionZ());
             // DEBUG_LOG("ground_z (%f) > INVALID_HEIGHT (%f)",ground_z,INVALID_HEIGHT);
-            if ((ground_z > INVALID_HEIGHT) && go->isSpawned())
+            if ((ground_z > INVALID_HEIGHT) && go->IsSpawned())
                 m_lootTargets.push_back(go->GetObjectGuid());
         }
     }
@@ -10353,7 +10353,7 @@ void PlayerbotAI::_HandleCommandSurvey(std::string& /*text*/, Player& fromPlayer
             if (!go)
                 continue;
 
-            if (!go->isSpawned())
+            if (!go->IsSpawned())
                 continue;
 
             detectout << "|cFFFFFF00|Hfound:" << guid << ":" << entry  << ":" <<  "|h[" << go->GetGOInfo()->name << "]|h|r";

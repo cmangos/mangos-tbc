b96dc9b472453123206d7df3576daed6a9551663
Warlockbugs <warlockbugs@outlook.com>
2018-07-13 21:05:32 +0300

    [s2376] Next Generation Taxi System for CMaNGOS: DB storage format update
    Finally, adds proper support for saving and loading flight paths outside of the commercial taxi network: quest flights, script flights, etc.
    These flights usually are not associated with any taxi nodes and could not be properly saved in previous renditions of the taxi system.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2367_01_realmd_totp"
- #define REVISION_DB_CHARACTERS "required_s2375_01_characters_pet_loyalty"
+ #define REVISION_DB_CHARACTERS "required_s2376_01_characters_taxi_system_format_update"
  #define REVISION_DB_MANGOS "required_s2374_01_mangos_quest_template_rewmaxrep"
 #endif // __REVISION_SQL_H__

9421fdba8da5d709c32ca9ea352a95b644cdf1c7
Killerwife <killerwife@gmail.com>
2018-07-23 00:29:46 +0200

    Do a more thorough job of cleanup up after temporary spawns and totems
    Fixes a crash where unsummon did not interrupt spells for various creature subtypes.

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -2934,3 +2934,15 @@ void Creature::LockOutSpells(SpellSchoolMask schoolMask, uint32 duration)
 
     WorldObject::LockOutSpells(schoolMask, duration);
 }
+
+void Creature::UnsummonCleanup()
+{
+    CombatStop();
+    RemoveAllAurasOnDeath();
+    BreakCharmOutgoing();
+    BreakCharmIncoming();
+    RemoveGuardians();
+    RemoveMiniPet();
+    UnsummonAllTotems();
+    InterruptNonMeleeSpells(false);
+}

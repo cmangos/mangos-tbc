3051efe5d7468619e59619db254b550fbf3236df
Killerwife <killerwife@gmail.com>
2018-08-04 13:25:29 +0200

    Fully reimplement Moroes using Timer AI
    Completely rewamped vanish. Should never bug out no matter the core changes.

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -3990,6 +3990,9 @@ void Aura::HandleModStealth(bool apply, bool Real)
                 }
             }
         }
+
+        if (GetId() == 29448) // Moroes Vanish
+            target->AI()->SendAIEvent(AI_EVENT_CUSTOM_A, target, target);
     }
 }
 

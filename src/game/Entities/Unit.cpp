e5b1e4163efd4bc913c7d930db00ea510039fca9
Killerwife <killerwife@gmail.com>
2018-08-20 21:35:43 +0200

    Correctly clear fixate on evade

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -8742,7 +8742,7 @@ bool Unit::SelectHostileTarget()
     }
 
     // enter in evade mode in other case
-    m_fixateTargetGuid.Clear();
+    FixateTarget(nullptr);
     AI()->EnterEvadeMode();
 
     return false;

6142abd213542ca42409769c53b6e6df6a08cf91
killerwife <killerwife@gmail.com>
2018-08-23 15:25:42 +0200

    Implement Anzu Intro, substantially improve combat AI and implement birds

--- src/game/Spells/UnitAuraProcHandler.cpp
+++ src/game/Spells/UnitAuraProcHandler.cpp
@@ -2069,7 +2069,6 @@ SpellAuraProcResult Unit::HandleProcTriggerSpellAuraProc(ProcExecutionData& data
                     break;
                 // case 38363: break;                   // Gushing Wound
                 // case 39215: break;                   // Gushing Wound
-                // case 40250: break;                   // Improved Duration
                 // case 40329: break;                   // Demo Shout Sensor
                 // case 40364: break;                   // Entangling Roots Sensor
                 // case 41054: break;                   // Copy Weapon

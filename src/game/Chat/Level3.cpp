1b7212289dcdaa26c175b28ff2362b6642168753
stfx <stfx@hotmail.de>
2018-08-26 13:08:12 +0200

    ﻿Static member accessed through instance [readability-static-accessed-through-instance]

--- src/game/Chat/Level3.cpp
+++ src/game/Chat/Level3.cpp
@@ -5728,7 +5728,7 @@ bool ChatHandler::HandlePDumpLoadCommand(char* args)
         }
     }
 
-    switch (PlayerDumpReader().LoadDump(file, account_id, name, lowguid))
+    switch (PlayerDumpReader::LoadDump(file, account_id, name, lowguid))
     {
         case DUMP_SUCCESS:
             PSendSysMessage(LANG_COMMAND_IMPORT_SUCCESS);

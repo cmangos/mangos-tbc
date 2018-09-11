b6d8d26b51dbf2aa69a7d40c5a2ed12efeaaea8e
killerwife <killerwife@gmail.com>
2018-09-11 13:04:43 +0200

    Remove redundant casting to creature in cast spell handler

--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -2548,7 +2548,7 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                 case 5229:                                  // Enrage
                 {
                     int32 reductionMod = -27;
-                    if (m_caster->HasAura(9634)) // If in Dire Bear form only 17%
+                    if (m_caster->HasAura(9634)) // If in Dire Bear form only 16%
                         reductionMod = -16;
                     m_caster->CastCustomSpell(nullptr, 25503, &reductionMod, nullptr, nullptr, TRIGGERED_OLD_TRIGGERED);
                     return;

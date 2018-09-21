9c2cd8e0067105bd03a9cb6887ca5852760a4e6e
killerwife <killerwife@gmail.com>
2018-09-21 14:11:39 +0200

    Implement deaths door warp gate counter sending

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -392,6 +392,8 @@ UPDATE creature_template SET ScriptName='npc_soulgrinder' WHERE entry=23019;
 UPDATE creature_template SET ScriptName='npc_mogdorg_the_wizened' WHERE entry=22941;
 UPDATE creature_template SET ScriptName='npc_supplicant' WHERE entry IN(23052,23053);
 INSERT INTO scripted_event_id VALUES(14739,'event_into_the_soulgrinder');
+UPDATE creature_template SET ScriptName='npc_warp_gate' WHERE entry IN(22471,22472);
+UPDATE creature_template SET ScriptName='npc_fel_cannon' WHERE entry=22443;
 
 /* BLASTED LANDS */
 UPDATE creature_template SET ScriptName='npc_fallen_hero_of_horde' WHERE entry=7572;

59f017c7135b85ada87a7f9644ef92d54e6a6a03
João Fidalgo <joao.fidalgo@airc.pt>
2018-09-22 18:21:24 +0100

    fix drop db script when executed in multiple transactions

--- sql/create/db_drop_mysql.sql
+++ sql/create/db_drop_mysql.sql
@@ -1,21 +1,13 @@
-REVOKE ALL PRIVILEGES ON * . * FROM 'mangos'@'localhost';
+REVOKE ALL ON `tbcmangos`.* FROM 'mangos'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `tbcmangos` . * FROM 'mangos'@'localhost';
+REVOKE ALL ON `tbccharacters`.* FROM 'mangos'@'localhost';
 
-REVOKE GRANT OPTION ON `tbcmangos` . * FROM 'mangos'@'localhost';
+REVOKE ALL ON `tbcrealmd`.* FROM 'mangos'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `tbccharacters` . * FROM 'mangos'@'localhost';
+DROP USER 'mangos'@'localhost';
 
-REVOKE GRANT OPTION ON `tbccharacters` . * FROM 'mangos'@'localhost';
+DROP DATABASE IF EXISTS `tbcmangos`;
 
-REVOKE ALL PRIVILEGES ON `tbcrealmd` . * FROM 'mangos'@'localhost';
+DROP DATABASE IF EXISTS `tbccharacters`;
 
-REVOKE GRANT OPTION ON `tbcrealmd` . * FROM 'mangos'@'localhost';
-
-DELETE FROM `user` WHERE CONVERT( User USING utf8 ) = CONVERT( 'mangos' USING utf8 ) AND CONVERT( Host USING utf8 ) = CONVERT( 'localhost' USING utf8 ) ;
-
-DROP DATABASE IF EXISTS `tbcmangos` ;
-
-DROP DATABASE IF EXISTS `tbccharacters` ;
-
-DROP DATABASE IF EXISTS `tbcrealmd` ;
+DROP DATABASE IF EXISTS `tbcrealmd`;

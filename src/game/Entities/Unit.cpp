425efb55247197292679f99e30c34aa5e5c9c45d
killerwife <killerwife@gmail.com>
2018-12-12 21:00:48 +0100

    Make spellstealer caster of new buff
    Confirmed via wotlk sniffs. Caster is not used for calculations since we directly use base points.

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -5022,7 +5022,7 @@ void Unit::RemoveAurasDueToSpellBySteal(uint32 spellId, ObjectGuid casterGuid, U
         int32 basePoints = aur->GetBasePoints();
         // construct the new aura for the attacker - will never return nullptr, it's just a wrapper for
         // some different constructors
-        Aura* new_aur = CreateAura(aur->GetSpellProto(), aur->GetEffIndex(), &basePoints, new_holder, stealer, this);
+        Aura* new_aur = CreateAura(aur->GetSpellProto(), aur->GetEffIndex(), &basePoints, new_holder, stealer, stealer);
 
         // set periodic to do at least one tick (for case when original aura has been at last tick preparing)
         int32 periodic = aur->GetModifier()->periodictime;

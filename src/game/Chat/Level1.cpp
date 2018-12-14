e975818883c0c7e15c20894a94b73292f92c80dc
henhouse <henry925@gmail.com>
2018-12-14 18:02:32 +0100

    Raise mod speed ceiling to 50
    Not an issue on modern hardware for many years now.

--- src/game/Chat/Level1.cpp
+++ src/game/Chat/Level1.cpp
@@ -949,7 +949,7 @@ bool ChatHandler::HandleModifyASpeedCommand(char* args)
 
     float modSpeed = (float)atof(args);
 
-    if (modSpeed > 10 || modSpeed < 0.1)
+    if (modSpeed > 50 || modSpeed < 0.1)
     {
         SendSysMessage(LANG_BAD_VALUE);
         SetSentErrorMessage(true);
@@ -997,7 +997,7 @@ bool ChatHandler::HandleModifySpeedCommand(char* args)
 
     float modSpeed = (float)atof(args);
 
-    if (modSpeed > 10 || modSpeed < 0.1)
+    if (modSpeed > 50 || modSpeed < 0.1)
     {
         SendSysMessage(LANG_BAD_VALUE);
         SetSentErrorMessage(true);
@@ -1042,7 +1042,7 @@ bool ChatHandler::HandleModifySwimCommand(char* args)
 
     float modSpeed = (float)atof(args);
 
-    if (modSpeed > 10.0f || modSpeed < 0.01f)
+    if (modSpeed > 50.0f || modSpeed < 0.01f)
     {
         SendSysMessage(LANG_BAD_VALUE);
         SetSentErrorMessage(true);
@@ -1132,7 +1132,7 @@ bool ChatHandler::HandleModifyFlyCommand(char* args)
 
     float modSpeed = (float)atof(args);
 
-    if (modSpeed > 10.0f || modSpeed < 0.1f)
+    if (modSpeed > 50.0f || modSpeed < 0.1f)
     {
         SendSysMessage(LANG_BAD_VALUE);
         SetSentErrorMessage(true);

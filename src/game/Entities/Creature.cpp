fc6ad96d4297fe6f81c6eb22c91d6fe2ff6b2afa
Killerwife <killerwife@gmail.com>
2018-12-22 22:09:43 +0100

    Add checkAttackability to SetInCombatWithZone
    Sometimes we want to put players in combat with an unattackable trigger. Sometimes we only want attackable players to be added.

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -2102,7 +2102,7 @@ void Creature::SendZoneUnderAttackMessage(Player* attacker) const
     sWorld.SendZoneUnderAttackMessage(GetZoneId(), attacker->GetTeam() == ALLIANCE ? HORDE : ALLIANCE);
 }
 
-void Creature::SetInCombatWithZone()
+void Creature::SetInCombatWithZone(bool checkAttackability)
 {
     if (!CanHaveThreatList())
     {
@@ -2132,6 +2132,9 @@ void Creature::SetInCombatWithZone()
 
             if (pPlayer->isAlive())
             {
+                if (checkAttackability && !CanAttack(pPlayer))
+                    continue;
+
                 AddThreat(pPlayer);
                 SetInCombatWith(pPlayer);
                 pPlayer->SetInCombatWith(this);

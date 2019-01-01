d71271fd31325e5781a655ba7916d73d67335fe6
Warlockbugs <warlockbugs@outlook.com>
2019-01-02 02:38:01 +0300

    [s2383] Restore proc of starter version of Seal of Righteousness w/o Judgement
    Pre-wrath only.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2367_01_realmd_totp"
  #define REVISION_DB_CHARACTERS "required_s2379_01_characters_playerbot_saved_data"
- #define REVISION_DB_MANGOS "required_s2382_01_mangos_seal_of_righteousness_restored"
+ #define REVISION_DB_MANGOS "required_s2383_01_mangos_seal_of_righteousness_proc_restored"
 #endif // __REVISION_SQL_H__

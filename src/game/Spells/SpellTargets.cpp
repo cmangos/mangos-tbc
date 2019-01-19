94c83a9c305ba82f9333e17b0d304cffef12dc1a
Killerwife <killerwife@gmail.com>
2019-01-19 17:18:16 +0100

    Targeting Rework: SPELL_EFFECT_KNOCK_BACK is a TARGET_TYPE_UNIT

--- src/game/Spells/SpellTargets.cpp
+++ src/game/Spells/SpellTargets.cpp
@@ -225,7 +225,7 @@ SpellEffectInfo SpellEffectInfoTable[MAX_SPELL_EFFECTS] =
     /*[95]*/     { "SPELL_EFFECT_SKINNING",                     TARGET_TYPE_UNIT,           TARGET_NONE },
     /*[96]*/     { "SPELL_EFFECT_CHARGE",                       TARGET_TYPE_UNIT,           TARGET_NONE },
     /*[97]*/     { "SPELL_EFFECT_97",                           TARGET_TYPE_UNIT_DEST,      TARGET_NONE },
-    /*[98]*/     { "SPELL_EFFECT_KNOCK_BACK",                   TARGET_TYPE_LOCATION_DEST,  TARGET_NONE },
+    /*[98]*/     { "SPELL_EFFECT_KNOCK_BACK",                   TARGET_TYPE_UNIT,           TARGET_NONE },
     /*[99]*/     { "SPELL_EFFECT_DISENCHANT",                   TARGET_TYPE_ITEM,           TARGET_NONE },
     /*[100]*/    { "SPELL_EFFECT_INEBRIATE",                    TARGET_TYPE_UNIT,           TARGET_NONE },
     /*[101]*/    { "SPELL_EFFECT_FEED_PET",                     TARGET_TYPE_ITEM,           TARGET_NONE },

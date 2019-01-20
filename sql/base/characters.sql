b3edf5af7df5058aed388a887cc9e0723e8b357c
Warlockbugs <warlockbugs@outlook.com>
2019-01-20 04:24:37 +0300

    [s2388] Prevent max value overflow when learning skill steps

--- sql/base/characters.sql
+++ sql/base/characters.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `character_db_version`;
 CREATE TABLE `character_db_version` (
-  `required_s2387_01_characters_weapon_skills_fix_tbc` bit(1) DEFAULT NULL
+  `required_s2388_01_characters_fix_skills_overflow` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Last applied sql update to DB';
 
 --

5d473f76158b4a5a8aa6729d38bab000e467ddfb
Carl Hjerpe <github@lillecarl.com>
2019-02-17 20:48:44 +1000

    Typo from last commit

--- src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/scholomance/instance_scholomance.cpp
+++ src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/scholomance/instance_scholomance.cpp
@@ -141,7 +141,7 @@ void instance_scholomance::DoRespawnEntranceRoom(Player* pSummoner)
         }
 
         uiMobList.push_back(uiMobEntry);
-        std::shuffle(uiMobEntry.begin(), uiMobEntry.end(), std::mt19937(std::random_device()()));
+        std::shuffle(uiMobList.begin(), uiMobList.end(), std::mt19937(std::random_device()()));
 
         for (uint8 j = 0; j < MAX_NPC_PER_GROUP; ++j)
             pSummoner->SummonCreature(uiMobList[j], aEntranceRoomSpawnLocs[4 * i + j].m_fX, aEntranceRoomSpawnLocs[4 * i + j].m_fY, aEntranceRoomSpawnLocs[4 * i + j].m_fZ, aEntranceRoomSpawnLocs[4 * i + j].m_fO, TEMPSPAWN_DEAD_DESPAWN, 0);

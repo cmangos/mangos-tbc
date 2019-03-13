6ef9998481ba3963516255783c5fcffdc84e5685
BThallid <bthallid@yahoo.com>
2019-03-13 11:44:33 +0100

    [PlayerBot] Remove a few warnings

--- src/game/PlayerBot/Base/PlayerbotAI.cpp
+++ src/game/PlayerBot/Base/PlayerbotAI.cpp
@@ -6810,7 +6810,7 @@ bool PlayerbotAI::CanStore()
                 totalfree =  totalfree + pBag->GetFreeSlots();
         }
     }
-    return totalfree;
+    return totalfree > 0;
 }
 
 // use item on self

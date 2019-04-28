79210bc3ff3796ab39eb2beb0b1a01833869dfb8
Carl Hjerpe <github@lillecarl.com>
2019-04-28 20:01:24 +0200

    Learn weapon skills automagically as well

--- src/game/Custom/AutoLearnSpells.hpp
+++ src/game/Custom/AutoLearnSpells.hpp
@@ -20,7 +20,6 @@ public:
             return;
 
             auto result = WorldDatabase.PQuery("SELECT DISTINCT trainertemplateid, trainerclass FROM creature_template WHERE trainertemplateid != 0");
-
             if (!result)
                 return;
 
@@ -29,7 +28,11 @@ public:
                 auto trainerid = result->Fetch()[0].GetUInt32();
                 auto classid   = result->Fetch()[1].GetUInt32();
 
-                TrainerSpellData const* tSpells = sObjectMgr.GetNpcTrainerTemplateSpells(trainerid);
+                auto* clsEntry = sChrClassesStore.LookupEntry(classid);
+                if (!clsEntry)
+                    continue;
+
+                auto* tSpells = sObjectMgr.GetNpcTrainerTemplateSpells(trainerid);
                 if (!tSpells)
                     continue;
 
@@ -38,6 +41,31 @@ public:
             }
             while (result->NextRow());
             delete result;
+            result = nullptr;
+
+            // Add Weapon skills as well
+            result = WorldDatabase.PQuery("SELECT trainertemplateid FROM creature_template WHERE trainertype = 2");
+            if (!result)
+                return;
+
+            do
+            {
+                auto trainerid = result->Fetch()[0].GetUInt32();
+
+                auto* tSpells = sObjectMgr.GetNpcTrainerTemplateSpells(trainerid);
+                if (!tSpells)
+                    continue;
+
+                for (auto& j : tSpells->spellList)
+                    if (auto* spellproto = sSpellTemplate.LookupEntry<SpellEntry>(j.second.learnedSpell))
+                        if (spellproto->Effect[0] == SPELL_EFFECT_WEAPON)
+                            for (auto classid = 0; classid < MAX_CLASSES; ++classid)
+                                if (sChrClassesStore.LookupEntry(classid))
+                                    m_trainerspells.insert(std::make_pair(classid, j.second));
+             }
+            while (result->NextRow());
+            delete result;
+            result = nullptr;
     }
 
     TrainerMap::const_iterator Begin(uint8 classid) { return m_trainerspells.lower_bound(classid); }

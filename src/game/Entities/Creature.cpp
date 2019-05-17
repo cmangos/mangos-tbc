944c6dcabe85e0d0c3a37c38f1926c01cf18a98c
killerwife <killerwife@gmail.com>
2019-05-17 16:38:27 +0200

    Adjust creature/pet regen based on their timers

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -717,12 +717,12 @@ void Creature::RegenerateAll(uint32 update_diff)
     if (!isInCombat() || IsEvadeRegen())
         RegenerateHealth();
 
-    RegeneratePower();
+    RegeneratePower(2.f);
 
     m_regenTimer = REGEN_TIME_FULL;
 }
 
-void Creature::RegeneratePower()
+void Creature::RegeneratePower(float timerMultiplier)
 {
     if (!IsRegeneratingPower())
         return;
@@ -746,7 +746,7 @@ void Creature::RegeneratePower()
                 {
                     float ManaIncreaseRate = sWorld.getConfig(CONFIG_FLOAT_RATE_POWER_MANA);
                     float intellect = GetStat(STAT_INTELLECT);
-                    addValue = sqrt(intellect) * OCTRegenMPPerSpirit() * ManaIncreaseRate;
+                    addValue = sqrt(intellect) * OCTRegenMPPerSpirit() * ManaIncreaseRate / 5.f * timerMultiplier;
                 }
             }
             else

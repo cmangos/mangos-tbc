821cd92535d52fe1f773f1fcb5e7618865f503d1
Ben Dol <dolb90@gmail.com>
2019-05-30 04:37:51 +1200

    Simplify the the main method.

--- src/testing/Core/TestMain.cpp
+++ src/testing/Core/TestMain.cpp
@@ -24,25 +24,12 @@ int main(int argc, char *argv[])
 {
     //sTestEngine.run();
 
-    Catch::Session session; // There must be exactly one instance
-
-  // writing to session.configData() here sets defaults
-  // this is the preferred way to set them
-
-    int returnCode = session.applyCommandLine(argc, argv);
-    if (returnCode != 0) // Indicates a command line error
-        return returnCode;
-
-    // writing to session.configData() or session.Config() here 
-    // overrides command line args
-    // only do this if you know you need to
-
-    int numFailed = session.run();
+    int result = Catch::Session().run(argc, argv);
 
     //sTestEngine.kill();
 
     // numFailed is clamped to 255 as some unices only use the lower 8 bits.
     // This clamping has already been applied, so just return it here
     // You can also do any post run clean-up here
-    return numFailed;
+    return result;
 }

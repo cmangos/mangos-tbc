c7df2d9cd7516f2f915886891f3afeea355a9eef
Killerwife <killerwife@gmail.com>
2019-08-09 22:01:00 +0200

    Minor refactor of sending chat message to players
    For future debugging use

--- src/game/Entities/Player.h
+++ src/game/Entities/Player.h
@@ -2176,6 +2176,8 @@ class Player : public Unit
         void SetTitle(uint32 titleId, bool lost = false);
         void SetTitle(CharTitlesEntry const* title, bool lost = false);
 
+        void SendMessageToPlayer(std::string const& const message) const; // debugging purposes
+
 #ifdef BUILD_PLAYERBOT
         // A Player can either have a playerbotMgr (to manage its bots), or have playerbotAI (if it is a bot), or
         // neither. Code that enables bots must create the playerbotMgr and set it using SetPlayerbotMgr.

8fd1742bc7c0543668cbe9942409f4159faa14af
Warlockbugs <warlockbugs@outlook.com>
2019-10-13 12:56:57 +0300

    New random movement generator hierarchy for CMaNGOS (transitional)
    Replaces existing family of random movement generators with a new one, which features a number of improvements.
    
    This is an adapted excerpt from my motionmaster rewrite that got stuck in development hell.
    
    The list of changes includes, but not limited to:
    * Fleeing, Timed Fleeing, Confused and Random movegens are now based on unified codebase
    * Timed Fleeing movegen renamed to Panic movegen
    * Random movegen renamed to Wander movegen
    * Improved performance for random hierarchy movegens
    * Unified and improved point picking for pathfinding for random hierarchy movegens
    * Rewrote Unit crowd control APIs:
    - IncapacitatedState concept retired: setters are now thematically split to take advantage of new movegens
    - Unit::IsIncapacitated() is renamed to Unit::IsCrowdControlled()
    * Add Unit::SendMeleeAttackStop() to random hierarchy movegens
    * Add UNIT_FLAG_UNK_29 management to Unit::SetStunned() and Unit::SetFeignDeath()

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -4147,7 +4147,7 @@ void Aura::HandleModConfuse(bool apply, bool Real)
     if (!apply && GetTarget()->HasAuraType(SPELL_AURA_MOD_CONFUSE))
         return;
 
-    GetTarget()->SetConfused(apply, GetCasterGuid(), GetId(), m_removeMode);
+    GetTarget()->SetConfused(apply, GetCasterGuid(), GetId());
 
     GetTarget()->getHostileRefManager().HandleSuppressed(apply);
 }
@@ -4161,7 +4161,7 @@ void Aura::HandleModFear(bool apply, bool Real)
     if (!apply && GetTarget()->HasAuraType(SPELL_AURA_MOD_FEAR))
         return;
 
-    GetTarget()->SetFeared(apply, GetCasterGuid(), GetId());
+    GetTarget()->SetFleeing(apply, GetCasterGuid(), GetId());
 
     // 2.3.0 - fear no longer applies suppression - in case of uncomment, need to adjust IsSuppressedTarget
     // GetTarget()->getHostileRefManager().HandleSuppressed(apply);
@@ -7096,7 +7096,7 @@ void Aura::PeriodicTick()
                 if (BattleGround* bg = ((Player*)pCaster)->GetBattleGround())
                     bg->UpdatePlayerScore(((Player*)pCaster), SCORE_HEALING_DONE, gain);
 
-            if (pCaster->isInCombat() && !pCaster->IsIncapacitated())
+            if (pCaster->isInCombat() && !pCaster->IsCrowdControlled())
                 target->getHostileRefManager().threatAssist(pCaster, float(gain) * 0.5f * sSpellMgr.GetSpellThreatMultiplier(spellProto), spellProto);
 
             pCaster->ProcDamageAndSpell(ProcSystemArguments(target, procAttacker, procVictim, procEx, gain, BASE_ATTACK, spellProto, nullptr, gain));
@@ -7730,9 +7730,9 @@ void Aura::HandlePreventFleeing(bool apply, bool Real)
     {
         const Aura* first = fearAuras.front();
         if (apply)
-            GetTarget()->SetFeared(false, first->GetCasterGuid());
+            GetTarget()->SetFleeing(false);
         else
-            GetTarget()->SetFeared(true, first->GetCasterGuid(), first->GetId());
+            GetTarget()->SetFleeing(true, first->GetCasterGuid(), first->GetId());
     }
 }
 

8ffdeae2f49557798a7d4bea9e6c01abb1f5df2a
bladez <bladez.lightshope@gmail.com>
2019-10-19 10:46:53 +0300

    Prevent chat freeze due to malformed chat links
    Don't skip whitespace when reading chat link command characters
    
    Adaptation of: https://github.com/lh-server/core/commit/7ab13bb4fe0c42177c2744b02130dcbaf64afcf7

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -265,6 +265,7 @@ class ChatHandler
         bool HandleDebugMoveflags(char* args);
         bool HandleDebugLootDropStats(char* args);
         bool HandleDebugOverflowCommand(char* args);
+        bool HandleDebugChatFreezeCommand(char* args);
 
         bool HandleDebugHaveAtClientCommand(char* args);
         bool HandleDebugIsVisibleCommand(char* args);

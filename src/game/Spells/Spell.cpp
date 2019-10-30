f9f1ea4084da8442c20f31594a7974e77de17997
killerwife <killerwife@gmail.com>
2019-10-30 23:25:35 +0100

    Targeting Rework: Fix dest effects not being queued due to wrong line order

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -927,7 +927,6 @@ void Spell::AddItemTarget(Item* item, uint8 effectMask)
 
 void Spell::AddDestExecution(SpellEffectIndex effIndex)
 {
-    m_destTargetInfo.effectMask |= (1 << effIndex);
     if (m_destTargetInfo.effectMask == 0)
     {
         // spell fly from visual cast object
@@ -949,6 +948,7 @@ void Spell::AddDestExecution(SpellEffectIndex effIndex)
         else
             m_destTargetInfo.timeDelay = uint64(0);
     }
+    m_destTargetInfo.effectMask |= (1 << effIndex);
 }
 
 void Spell::DoAllEffectOnTarget(TargetInfo* target)

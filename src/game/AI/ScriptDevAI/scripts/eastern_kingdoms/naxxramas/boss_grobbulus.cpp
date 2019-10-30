06f1e374262c936262fa3d36d78d95d840982d9f
cala <cala@users.noreply.github.com>
2019-10-30 10:31:43 +0100

    Improve Four Horsemen encounter
    * Improve Four Horsemen encounter in Naxxramas:
    - Improve timers
    - Add missing say when using special ability for each horsemn
    - Refactor script code with a super class for the common horsemen behaviours, using children classes only for the specific ones (different yell/say or targeting logic)
    - Fix text type for Lady Blaumeux

--- src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/naxxramas/boss_grobbulus.cpp
+++ src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/naxxramas/boss_grobbulus.cpp
@@ -87,7 +87,7 @@ struct boss_grobbulusAI : public ScriptedAI
 
         // Clean-up stage
         DoCastSpellIfCan(m_creature, SPELL_DESPAWN_SUMMONS, CAST_TRIGGERED);
-        
+
         ScriptedAI::EnterEvadeMode();
     }
 

4d26a7e1982e9b50a7fa2314069d0a16d844a53a
killerwife <killerwife@gmail.com>
2019-12-22 13:18:34 +0100

    TargetingRework: Fix SPELL_EFFECT_TELEPORT_UNITS and fill TARGET_TYPE_UNIT_DEST properly
    SPELL_EFFECT_TELEPORT_UNITS  was wrong type
    TARGET_TYPE_UNIT_DEST was missing fill in case part of fill was in target flags
    Also refactors teleport effect to fully use m_targets

--- src/game/Spells/Spell.h
+++ src/game/Spells/Spell.h
@@ -129,6 +129,9 @@ class SpellCastTargets
 
             m_targetMask = target.m_targetMask;
 
+            m_destOri = 0.f;
+            m_mapId = target.m_mapId;
+
             return *this;
         }
 
@@ -175,6 +178,9 @@ class SpellCastTargets
         std::string m_strTarget;
 
         uint32 m_targetMask;
+
+        float m_destOri;
+        uint32 m_mapId; // not to be written to packet in vanilla/tbc
 
     private:
         // objects (can be used at spell creating and after Update at casting
@@ -664,6 +670,7 @@ class Spell
         bool CheckAndAddMagnetTarget(Unit* unitTarget, SpellEffectIndex effIndex, bool targetB, TempTargetingData& data);
         static void CheckSpellScriptTargets(SQLMultiStorage::SQLMSIteratorBounds<SpellTargetEntry>& bounds, UnitList& tempTargetUnitMap, UnitList& targetUnitMap, SpellEffectIndex effIndex);
         void FilterTargetMap(UnitList& filterUnitList, SpellEffectIndex effIndex);
+        void FillFromTargetFlags(TempTargetingData& targetingData, SpellEffectIndex effIndex);
 
         void FillAreaTargets(UnitList& targetUnitMap, float radius, float cone, SpellNotifyPushType pushType, SpellTargets spellTargets, WorldObject* originalCaster = nullptr);
         void FillRaidOrPartyTargets(UnitList& targetUnitMap, Unit* member, float radius, bool raid, bool withPets, bool withcaster) const;

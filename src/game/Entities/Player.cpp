6ce4168c23c032dc60f833d69e38c38a331f1e92
killerwife <killerwife@gmail.com>
2020-02-13 13:33:39 +0100

    WorldState: Add full API for LoveIsInTheAir event leader counters

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -8108,6 +8108,8 @@ void Player::SendInitWorldStates(uint32 zoneid, uint32 areaid) const
     if (InstanceData* instanceData = GetMap()->GetInstanceData())
         instanceData->FillInitialWorldStates(data, count, zoneid, areaid);
 
+    sWorldState.FillInitialWorldStates(data, count, zoneid, areaid);
+
     data.put<uint16>(count_pos, count);                     // set actual world state amount
 
     GetSession()->SendPacket(data);

a88c9d5fd3e1b99a77184e989497e90d03aec313
killerwife <killerwife@gmail.com>
2020-02-19 13:29:51 +0100

    [s2417] Implement IP logging for accounts

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_REALMD "required_s2398_01_realmd_banning"
+ #define REVISION_DB_REALMD "required_s2417_01_realmd_account_logons"
  #define REVISION_DB_CHARACTERS "required_s2415_01_characters_ahbot_items"
  #define REVISION_DB_MANGOS "required_s2416_01_mangos_command_ahbot"
 #endif // __REVISION_SQL_H__

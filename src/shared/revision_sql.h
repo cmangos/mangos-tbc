7bde00f7dae4568a13de38861693845e35e3113b
norcimo <norcimo@protonmail.com>
2020-02-28 14:25:55 +0100

    [s2400] Allow linking graveyards by area id
    This will permit some fix with incorrect graveyard selected for some area.
    
    PR -> https://github.com/cmangos/mangos-classic/pull/369
    How to use it -> https://github.com/cmangos/issues/wiki/Game_graveyard_zone

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2398_01_realmd_banning"
  #define REVISION_DB_CHARACTERS "required_s2395_01_characters_WorldState"
- #define REVISION_DB_MANGOS "required_s2399_01_mangos_dbscript_priority_miliseconds"
+ #define REVISION_DB_MANGOS "required_s2400_01_mangos_graveyard_map_links"
 #endif // __REVISION_SQL_H__

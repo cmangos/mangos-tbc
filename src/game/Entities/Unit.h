28bbd053d05bf70878192570f026bebca7b632cf
killerwife <killerwife@gmail.com>
2020-03-04 16:19:13 +0100

    Do not remove combo points on pvp CombatStop

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -1781,7 +1781,7 @@ class Unit : public WorldObject
         void EngageInCombatWith(Unit* enemy);
         void EngageInCombatWithAggressor(Unit* aggressor);
         void ClearInCombat();
-        void HandleExitCombat();
+        void HandleExitCombat(bool pvpCombat = false);
 
         SpellAuraHolderBounds GetSpellAuraHolderBounds(uint32 spell_id)
         {

b462b1e06ce9f622f72f1ed240d04547f11be89d
Warlockbugs <warlockbugs@outlook.com>
2020-03-06 20:20:56 +0300

    Spell DR update: Detach levels from duration diminishing selectively
    Changes:
    * Allows configuring if DR group's duration can be diminished or not
    * Allows DR hit counter to go above 4 levels if duration is not diminished for other uses
    * Sets DR reset time to last spell's duration instead of default 15 seconds for DR groups with non-diminished duration
    * Move DIMINISHING_CHARM to DRTYPE_ALL group
    * Configure DIMINISHING_CHARM duration to be diminished against players only

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -771,8 +771,8 @@ enum DiminishingLevels
  */
 struct DiminishingReturn
 {
-    DiminishingReturn(DiminishingGroup group, uint32 t, uint32 count)
-        : DRGroup(group), stack(0), hitTime(t), hitCount(count)
+    DiminishingReturn(DiminishingGroup group, uint32 t, uint32 count, uint32 duration)
+        : DRGroup(group), stack(0), hitTime(t), hitCount(count), lastDuration(duration)
     {}
 
     /**
@@ -796,6 +796,10 @@ struct DiminishingReturn
      * decides how how long the duration of the stun etc is.
      */
     uint32                  hitCount;
+    /**
+     * Records nominal duration of the last applied spell entry of this DiminishingGroup
+     */
+    uint32                  lastDuration;
 };
 
 // At least some values expected fixed and used in auras field, other custom
@@ -1238,7 +1242,7 @@ class Unit : public WorldObject
          * that DiminishingGroup
          * @param group The group to increase the level for by one
          */
-        void IncrDiminishing(DiminishingGroup group);
+        void IncrDiminishing(DiminishingGroup group, uint32 duration, bool pvp);
         /**
          * Calculates how long the duration of a spell should be considering
          * diminishing returns, ie, if the Level passed in is DIMINISHING_LEVEL_IMMUNE

af9884cddc8a10670c0973f39c3816643e30013d
killerwife <killerwife@gmail.com>
2020-03-10 23:37:08 +0100

    Assign proper gender to broadcast_text queries

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -12111,6 +12111,8 @@ void Player::PrepareGossipMenu(WorldObject* pSource, uint32 menuId)
 
     GossipMenuItemsMapBounds pMenuItemBounds = sObjectMgr.GetGossipMenuItemsMapBounds(menuId);
 
+    Gender gender = GENDER_MALE;
+
     // prepares quest menu when true
     bool canSeeQuests = menuId == GetDefaultGossipMenuForSource(pSource);
 
@@ -12139,6 +12141,8 @@ void Player::PrepareGossipMenu(WorldObject* pSource, uint32 menuId)
         if (pSource->GetTypeId() == TYPEID_UNIT)
         {
             Creature* pCreature = (Creature*)pSource;
+
+            gender = Gender(pCreature->getGender());
 
             uint32 npcflags = pCreature->GetUInt32Value(UNIT_NPC_FLAGS);
 
@@ -12265,12 +12269,12 @@ void Player::PrepareGossipMenu(WorldObject* pSource, uint32 menuId)
                 if (gossipMenu.option_broadcast_text)
                 {
                     BroadcastText const* bct = sObjectMgr.GetBroadcastText(gossipMenu.option_broadcast_text);
-                    strOptionText = bct->GetText(loc_idx);
+                    strOptionText = bct->GetText(loc_idx, gender);
                 }
                 if (gossipMenu.box_broadcast_text)
                 {
                     BroadcastText const* bct = sObjectMgr.GetBroadcastText(gossipMenu.box_broadcast_text);
-                    strBoxText = bct->GetText(loc_idx);
+                    strBoxText = bct->GetText(loc_idx, gender);
                 }
             }
             else

c923fa0e6d9fd8c31e52246ff80251a42a8eab0e
Warlockbugs <warlockbugs@outlook.com>
2020-03-11 23:43:00 +0300

    [s2404] New Tickets system: Further fixes
    Changes:
    * Yet another attempt to fix MySQL 8 timestamp storage issues by abandoning timestamp type altogether
    * Replace MySQL-only REPLACE INTO statements to generic SQL DELETE and INSERT for compliance
    * Remade survey saving (Thanks @Killerwife)
    * Self-resolution case without assignment should no longer attach conclusion with empty name to mail

--- sql/updates/characters/s2402_01_characters_new_ticket_system_sql_std.sql
+++ sql/updates/characters/s2402_01_characters_new_ticket_system_sql_std.sql
@@ -4,7 +4,7 @@ ALTER TABLE character_db_version CHANGE COLUMN required_s2401_01_characters_new_
 -- Apply standard compliant structure for table `gm_tickets`
 --
 
-ALTER TABLE `gm_tickets` MODIFY COLUMN `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `gm_tickets` MODIFY COLUMN `created` bigint(40) unsigned NOT NULL;
 ALTER TABLE `gm_tickets` MODIFY COLUMN `updated` bigint(40) unsigned NOT NULL DEFAULT 0;
 ALTER TABLE `gm_tickets` MODIFY COLUMN `seen` bigint(40) unsigned NOT NULL DEFAULT 0;
 ALTER TABLE `gm_tickets` MODIFY COLUMN `answered` bigint(40) unsigned NOT NULL DEFAULT 0;

f78ba546b10e2275b49baba165a0d398a53d7a20
Warlockbugs <warlockbugs@outlook.com>
2020-03-14 18:07:15 +0300

    [s2405] Chat: New feature - Static custom channels
    This feature is introduced to accommodate blossomed "world chat" culture.
    
    Static custom channels are custom channels with global-like properties.
    
    By default they have:
    * No owner
    * No moderators
    * No password
    * No premoderation
    
    GMs have power to execute any moderator commands or override any of default settings above on demand.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2398_01_realmd_banning"
  #define REVISION_DB_CHARACTERS "required_s2404_01_characters_new_ticket_system_sql_created"
- #define REVISION_DB_MANGOS "required_s2401_01_mangos_new_ticket_system"
+ #define REVISION_DB_MANGOS "required_s2405_01_mangos_static_custom_chat"
 #endif // __REVISION_SQL_H__

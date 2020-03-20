6c9d04c426d88f8a35d44ee26c12cdd531055a53
killerwife <killerwife@gmail.com>
2020-03-20 19:57:24 +0100

    Add missing virtual calls for Aura derivatives

--- src/game/Spells/SpellAuras.h
+++ src/game/Spells/SpellAuras.h
@@ -558,7 +558,7 @@ class AreaAura : public Aura
 {
     public:
         AreaAura(SpellEntry const* spellproto, SpellEffectIndex eff, int32 const* currentDamage, int32 const* currentBasePoints, SpellAuraHolder* holder, Unit* target, Unit* caster = nullptr, Item* castItem = nullptr, uint32 originalRankSpellId = 0);
-        ~AreaAura();
+        virtual ~AreaAura();
     protected:
         void Update(uint32 diff) override;
     private:
@@ -571,7 +571,7 @@ class PersistentAreaAura : public Aura
 {
     public:
         PersistentAreaAura(SpellEntry const* spellproto, SpellEffectIndex eff, int32 const* currentDamage, int32 const* currentBasePoints, SpellAuraHolder* holder, Unit* target, Unit* caster = nullptr, Item* castItem = nullptr);
-        ~PersistentAreaAura();
+        virtual ~PersistentAreaAura();
     protected:
         void Update(uint32 diff) override;
 };
@@ -581,7 +581,7 @@ class SingleEnemyTargetAura : public Aura
         friend Aura* CreateAura(SpellEntry const* spellproto, SpellEffectIndex eff, int32 const* currentDamage, int32 const* currentBasePoints, SpellAuraHolder* holder, Unit* target, Unit* caster, Item* castItem);
 
     public:
-        ~SingleEnemyTargetAura();
+        virtual ~SingleEnemyTargetAura();
         Unit* GetTriggerTarget() const override;
 
     protected:
@@ -594,7 +594,7 @@ class GameObjectAura : public Aura
 {
     public:
         GameObjectAura(SpellEntry const* spellproto, SpellEffectIndex eff, int32 const* currentDamage, int32 const* currentBasePoints, SpellAuraHolder* holder, Unit* target, GameObject* caster);
-        ~GameObjectAura();
+        virtual ~GameObjectAura();
 
     protected:
         void Update(uint32 diff) override;

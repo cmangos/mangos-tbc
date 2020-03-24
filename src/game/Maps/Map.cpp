42580458d74f6b6cda73fe4cccaad00d62f04906
killerwife <killerwife@gmail.com>
2020-03-24 22:50:42 +0100

    Remove all group buffs on entering instance and on caster leaving
    Atm only affects soulstones

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -355,6 +355,9 @@ bool Map::Add(Player* player)
     player->GetViewPoint().Event_AddedToWorld(&(*grid)(cell.CellX(), cell.CellY()));
     UpdateObjectVisibility(player, cell, p);
 
+    if (IsRaid())
+        player->RemoveAllGroupBuffsFromCaster(ObjectGuid());
+
     if (i_data)
         i_data->OnPlayerEnter(player);
 
@@ -718,6 +721,10 @@ void Map::Remove(Player* player, bool remove)
 {
     if (i_data)
         i_data->OnPlayerLeave(player);
+
+    if (IsRaid())
+        for (auto& playerRef : GetPlayers())
+            playerRef.getSource()->RemoveAllGroupBuffsFromCaster(player->GetObjectGuid());
 
     if (remove)
         player->CleanupsBeforeDelete();

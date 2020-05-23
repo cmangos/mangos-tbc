99ded42fdde370c6c9d67978f76bfd6673626674
Cyberium <cyberium@users.noreply.github.com>
2020-05-23 16:56:13 +0200

    [s2411] Fix errors and warnings in console due to inconsistancy with db data

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2398_01_realmd_banning"
  #define REVISION_DB_CHARACTERS "required_s2404_01_characters_new_ticket_system_sql_created"
- #define REVISION_DB_MANGOS "required_s2410_01_mangos_gameobject_template"
+ #define REVISION_DB_MANGOS "required_s2411_01_mangos_command"
 #endif // __REVISION_SQL_H__

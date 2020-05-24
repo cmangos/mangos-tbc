ea0cc17d5474396783516dadae2baee632761e4f
killerwife <killerwife@gmail.com>
2020-05-24 12:45:34 +0200

    WarEffort: Initial AQ War Effort commit

--- src/game/AI/ScriptDevAI/system/ScriptLoader.cpp
+++ src/game/AI/ScriptDevAI/system/ScriptLoader.cpp
@@ -28,6 +28,7 @@ extern void AddSC_quests_scripts();
 extern void AddSC_spell_scripts();
 extern void AddSC_world_map_scripts();
 extern void AddSC_boss_highlord_kruul();
+extern void AddSC_war_effort();
 
 // eastern kingdoms
 extern void AddSC_blackrock_depths();                       // blackrock_depths
@@ -380,6 +381,7 @@ void AddScripts()
     AddSC_spell_scripts();
     AddSC_world_map_scripts();
     AddSC_boss_highlord_kruul();
+    AddSC_war_effort();
 
     // eastern kingdoms
     AddSC_blackrock_depths();                               // blackrock_depths

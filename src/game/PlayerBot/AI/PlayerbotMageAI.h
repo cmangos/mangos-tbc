f5b9bbf7eee634a51bf2f80660171e9b77969833
cala <cala@users.noreply.github.com>
2020-07-17 16:53:09 +0200

    [PlayerBot] Make virtual declaration inside children class
    * Make virtual declaration inside children class, this should fix the Travis build that was failing.

--- src/game/PlayerBot/AI/PlayerbotMageAI.h
+++ src/game/PlayerBot/AI/PlayerbotMageAI.h
@@ -112,7 +112,7 @@ class PlayerbotMageAI : PlayerbotClassAI
         // all combat actions go here
         CombatManeuverReturns DoFirstCombatManeuver(Unit* pTarget);
         CombatManeuverReturns DoNextCombatManeuver(Unit* pTarget);
-        uint32 Neutralize(uint8 creatureType);
+        virtual uint32 Neutralize(uint8 creatureType) override;
 
         // all non combat actions go here, ex buffs, heals, rezzes
         void DoNonCombatActions();

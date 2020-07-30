b72f064935dc8adbd8334204181a3c5fc1bc4e63
killerwife <killerwife@gmail.com>
2020-07-31 01:34:14 +0200

    Fix PoM and similar effects proccing twice on one aoe

--- src/game/Spells/UnitAuraProcHandler.cpp
+++ src/game/Spells/UnitAuraProcHandler.cpp
@@ -256,7 +256,7 @@ pAuraProcHandler AuraProcHandler[TOTAL_AURAS] =
     &Unit::HandleNULLProc,                                  //222 unused
     &Unit::HandleRaidProcFromChargeAuraProc,                //223 Cold Stare
     &Unit::HandleNULLProc,                                  //224 unused
-    &Unit::HandleRaidProcFromChargeWithValueAuraProc,                           //225 SPELL_AURA_RAID_PROC_FROM_CHARGE_WITH_VALUE
+    &Unit::HandleRaidProcFromChargeWithValueAuraProc,       //225 SPELL_AURA_RAID_PROC_FROM_CHARGE_WITH_VALUE
     &Unit::HandleNULLProc,                                  //226 SPELL_AURA_PERIODIC_DUMMY
     &Unit::HandleNULLProc,                                  //227 SPELL_AURA_PERIODIC_TRIGGER_SPELL_WITH_VALUE
     &Unit::HandleNULLProc,                                  //228 SPELL_AURA_DETECT_STEALTH
@@ -2817,6 +2817,12 @@ SpellAuraProcResult Unit::HandleRaidProcFromChargeAuraProc(ProcExecutionData& da
     if (!damage)
         return SPELL_AURA_PROC_FAILED;
 
+    if (data.spell)
+    {
+        if (data.spell->IsAuraProcced(triggeredByAura))
+            return SPELL_AURA_PROC_FAILED;
+    }
+
     // aura can be deleted at casts
     SpellEntry const* spellProto = triggeredByAura->GetSpellProto();
     SpellEffectIndex effIdx = triggeredByAura->GetEffIndex();
@@ -2869,6 +2875,9 @@ SpellAuraProcResult Unit::HandleRaidProcFromChargeAuraProc(ProcExecutionData& da
                 else
                     new_holder->SetState(SPELLAURAHOLDER_STATE_READY);
 
+                if (data.spell)
+                    data.spell->RegisterAuraProc(new_holder->m_auras[triggeredByAura->GetEffIndex()]);
+
                 CastSpell(target, animationSpellId, TRIGGERED_IGNORE_GCD | TRIGGERED_IGNORE_CURRENT_CASTED_SPELL | TRIGGERED_HIDE_CAST_IN_COMBAT_LOG);
             }
         }
@@ -2884,6 +2893,12 @@ SpellAuraProcResult Unit::HandleRaidProcFromChargeWithValueAuraProc(ProcExecutio
     //if no damage then mending proc has failed, we dont need to do unecessary steps
     if (!damage)
         return SPELL_AURA_PROC_FAILED;
+
+    if (data.spell)
+    {
+        if (data.spell->IsAuraProcced(triggeredByAura))
+            return SPELL_AURA_PROC_FAILED;
+    }
 
     // aura can be deleted at casts
     SpellEntry const* spellProto = triggeredByAura->GetSpellProto();
@@ -2935,6 +2950,9 @@ SpellAuraProcResult Unit::HandleRaidProcFromChargeWithValueAuraProc(ProcExecutio
                     delete new_holder;
                 else
                     new_holder->SetState(SPELLAURAHOLDER_STATE_READY);
+
+                if (data.spell)
+                    data.spell->RegisterAuraProc(new_holder->m_auras[triggeredByAura->GetEffIndex()]);
 
                 CastSpell(target, 41637, TRIGGERED_IGNORE_GCD | TRIGGERED_IGNORE_CURRENT_CASTED_SPELL | TRIGGERED_HIDE_CAST_IN_COMBAT_LOG);
             }

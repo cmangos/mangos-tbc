6b53cdf89607e4825b5c21e15c5dd486b50486d3
killerwife <killerwife@gmail.com>
2020-08-09 10:30:09 +0200

    Enable setting movegen like MovementType in DB in SummonCreature

--- src/game/Entities/Object.cpp
+++ src/game/Entities/Object.cpp
@@ -1960,6 +1960,8 @@ Creature* WorldObject::SummonCreature(TempSpawnSettings settings, Map* map)
         creature->SetSpawnCounting(true);
 
     creature->GetMotionMaster()->SetDefaultPathId(settings.pathId);
+    if (settings.movegen != -1)
+        creature->SetDefaultMovementType(MovementGeneratorType(settings.movegen));
 
     if (settings.spawner)
     {

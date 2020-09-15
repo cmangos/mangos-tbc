84ec82aa41d7735dd4a5fc71ca5aa84407674a99
killerwife <killerwife@gmail.com>
2020-09-15 20:37:52 +0200

    Arena: Add missing mangos_string for new Arena error

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -4354,6 +4354,7 @@ INSERT INTO `mangos_string` VALUES
 (746,'Done flushing Arena points.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (747,'Awarding Arena Season rewards for Season %u.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (748,'Awarded Arena Season rewards and wiped old stats.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(749,'Someone in your party is still inside an arena.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (750,'Not enough players. This game will close in %u mins.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (751,'Not enough players. This game will close in %u seconds.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (752,'Only the Alliance can use that portal',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),

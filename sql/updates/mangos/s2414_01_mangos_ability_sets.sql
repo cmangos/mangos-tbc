0b3ebc585f58f955f6d31131eaa425601749b985
killerwife <killerwife@gmail.com>
2020-10-04 10:12:23 +0200

    Add mistakenly left out primary key alteration to s2414

--- sql/updates/mangos/s2414_01_mangos_ability_sets.sql
+++ sql/updates/mangos/s2414_01_mangos_ability_sets.sql
@@ -1,5 +1,5 @@
 ALTER TABLE db_version CHANGE COLUMN required_s2413_01_mangos_gm_mountup_command required_s2414_01_mangos_ability_sets bit;
 
 ALTER TABLE creature_template_spells ADD COLUMN `setId` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Id of set of spells' AFTER `entry`;
-
+ALTER TABLE creature_template_spells DROP PRIMARY KEY, ADD PRIMARY KEY(entry, setId);
 

517eaf6c118ee6adc4074b14a4b68cffddd6c545
MantisLord <sabinprosper@gmail.com>
2020-10-22 21:35:58 -0400

    Fix certain item mods not re-applying following resurrection
    Examples:
    13347 Crystal of Zin-Malor
    35749 Sorcerer's Alchemist Stone
    
    Unlike Argent Dawn Commission 12846 which also gives a passive aura, these don't persist on death, so we need something to check if they should be reapplied when the player comes back to life.

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -4605,6 +4605,10 @@ void Player::ResurrectPlayer(float restore_percent, bool applySickness)
         if (InstanceData* instanceData = GetMap()->GetInstanceData())
             instanceData->OnPlayerResurrect(this);
 
+    // reapplies certain item equip spells which didn't last on death
+    _RemoveAllItemMods();
+    _ApplyAllItemMods();
+
     if (!applySickness)
         return;
 

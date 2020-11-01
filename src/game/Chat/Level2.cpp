c23154f9cefbda732fa4eb4b9e1a3b5bea5d440e
henhouse <henry925@gmail.com>
2020-11-01 21:36:02 +0100

    Fixed Win build

--- src/game/Chat/Level2.cpp
+++ src/game/Chat/Level2.cpp
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "Battleground.h"
+#include "BattleGround.h"
 #include "Common.h"
 #include "Database/DatabaseEnv.h"
 #include "Server/DBCStores.h"

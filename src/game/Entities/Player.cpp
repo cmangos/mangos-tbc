0850e6ee595361516513e0526abd8b5f4d29f956
killerwife <killerwife@gmail.com>
2020-11-12 09:35:02 +0100

    Skills: Learn default mono and rank skills which have SKILL_FLAG_MAXIMIZED but no skillTierId Closes https://gitlab.com/lights-vengeance/issues/-/issues/917

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -6168,7 +6168,7 @@ void Player::LearnDefaultSkills()
                     step = (predefined ? tskill.Step : 1);
                     uint32 stepIndex = (step - 1);
 
-                    auto filterfunc = [&] (SkillRaceClassInfoEntry const& entry)
+                    auto filterfunc = [&] (SkillRaceClassInfoEntry const& entry) -> bool
                     {
                         if (entry.skillTierId)
                         {
@@ -6184,6 +6184,12 @@ void Player::LearnDefaultSkills()
                                 return true;
                             }
                         }
+                        else if (entry.flags & SKILL_FLAG_MAXIMIZED)
+                        {
+                            value = max = GetSkillMaxForLevel();
+                            return true;
+                        }
+
                         return false;
                     };
 

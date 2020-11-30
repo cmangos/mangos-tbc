553211071970bc02956ef6fac74c31d2f461b7e9
Warlockbugs <warlockbugs@outlook.com>
2020-11-30 20:46:51 +0300

    [s2423] Update default item duration for existing databases

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2421_01_realmd_account_locale_agnostic"
- #define REVISION_DB_CHARACTERS "required_s2420_01_characters_item_instance_data_drop"
+ #define REVISION_DB_CHARACTERS "required_s2423_01_characters_item_instance_duration_default"
  #define REVISION_DB_MANGOS "required_s2422_01_mangos_creature_template_spells_extension"
 #endif // __REVISION_SQL_H__

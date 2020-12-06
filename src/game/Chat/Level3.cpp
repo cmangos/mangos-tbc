854cd31baea9009bd1810b7f1c4be9668ae8423c
Henhouse <henry925@gmail.com>
2020-12-06 21:45:14 +0100

    Make GM revive command always resurrect with max health

--- src/game/Chat/Level3.cpp
+++ src/game/Chat/Level3.cpp
@@ -3126,7 +3126,7 @@ bool ChatHandler::HandleReviveCommand(char* args)
 
     if (target)
     {
-        target->ResurrectPlayer(0.5f);
+        target->ResurrectPlayer(1.0f);
         target->SpawnCorpseBones();
     }
     else

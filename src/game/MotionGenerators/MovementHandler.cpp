d0975b6762a9831da56cd6c637bb18632dbad9b5
killerwife <killerwife@gmail.com>
2020-12-10 21:41:00 +0100

    Movement: Fill mover properly and send packed guid in new packets

--- src/game/MotionGenerators/MovementHandler.cpp
+++ src/game/MotionGenerators/MovementHandler.cpp
@@ -342,14 +342,14 @@ void WorldSession::HandleForceSpeedChangeAckOpcodes(WorldPacket& recv_data)
             return;
     }
 
-    if (!ProcessMovementInfo(movementInfo, _player, _player, recv_data))
-        return;
-
     Unit* mover = _player->GetMover();
+
+    if (!ProcessMovementInfo(movementInfo, mover, _player, recv_data))
+        return;
 
     const SpeedOpcodePair& speedOpcodes = SetSpeed2Opc_table[move_type];
     WorldPacket data(speedOpcodes[2], 18);
-    data << guid;
+    data << guid.WriteAsPacked();
     data << movementInfo;
     data << newspeed;
     mover->SendMessageToSetExcept(data, _player);
@@ -449,7 +449,7 @@ void WorldSession::HandleMoveKnockBackAck(WorldPacket& recv_data)
     recv_data >> Unused<uint32>();                          // knockback packets counter
     recv_data >> movementInfo;
 
-    if (!ProcessMovementInfo(movementInfo, _player, _player, recv_data))
+    if (!ProcessMovementInfo(movementInfo, mover, _player, recv_data))
         return;
 
     if (mover->IsPlayer() && static_cast<Player*>(mover)->IsFreeFlying())
@@ -496,7 +496,9 @@ void WorldSession::HandleMoveFlagChangeOpcode(WorldPacket& recv_data)
     recv_data >> movementInfo;
     recv_data >> isApplied;
 
-    if (!ProcessMovementInfo(movementInfo, _player, _player, recv_data))
+    Unit* mover = _player->GetMover();
+
+    if (!ProcessMovementInfo(movementInfo, mover, _player, recv_data))
         return;
 
     Opcodes response = MSG_NULL_ACTION;
@@ -509,10 +511,8 @@ void WorldSession::HandleMoveFlagChangeOpcode(WorldPacket& recv_data)
         case CMSG_MOVE_SET_CAN_FLY_ACK: response = MSG_MOVE_UPDATE_CAN_FLY; break;
     }
 
-    Unit* mover = _player->GetMover();
-
     WorldPacket data(response, 8);
-    data << guid;
+    data << guid.WriteAsPacked();
     data << movementInfo;
     mover->SendMessageToSetExcept(data, _player);
 }
@@ -546,11 +546,11 @@ void WorldSession::HandleMoveRootAck(WorldPacket& recv_data)
             return;
     }
 
-    if (!ProcessMovementInfo(movementInfo, _player, _player, recv_data))
+    if (!ProcessMovementInfo(movementInfo, mover, _player, recv_data))
         return;
 
     WorldPacket data(recv_data.GetOpcode() == CMSG_FORCE_MOVE_UNROOT_ACK ? MSG_MOVE_UNROOT : MSG_MOVE_ROOT);
-    data << guid;
+    data << guid.WriteAsPacked();
     data << movementInfo;
     mover->SendMessageToSetExcept(data, _player);
 }

864e6a238210c0b9e2b0f8c39e761623704bf2c7
killerwife <killerwife@gmail.com>
2020-12-10 13:33:27 +0100

    GameObject/AI: Implement GO AI OnUse

--- src/game/Entities/GameObject.cpp
+++ src/game/Entities/GameObject.cpp
@@ -1280,6 +1280,9 @@ void GameObject::Use(Unit* user, SpellEntry const* spellInfo)
     if (!scriptReturnValue)
         GetMap()->ScriptsStart(sGameObjectTemplateScripts, GetEntry(), spellCaster, this);
 
+    if (AI())
+        AI()->OnUse(user, spellInfo);
+
     sWorldState.HandleGameObjectUse(this, user);
 
     switch (GetGoType())

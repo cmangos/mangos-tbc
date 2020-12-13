c28b9e906e3667c986686b60a0dbbc541a9840bc
MantisLord <sabinprosper@gmail.com>
2020-12-13 14:09:20 -0500

    Add another TBC broadcast text modification to tracking file

--- sql/base/dbc/cmangos_fixes/BroadcastText.sql
+++ sql/base/dbc/cmangos_fixes/BroadcastText.sql
@@ -7,10 +7,13 @@
 -- TBC section
 -- ============================================================
 
+-- Hallow's End - Shade of the Horseman
 -- these two texts were different before 3.0 changes were made to the event
 -- the npc_text and broadcast_text data in tbc-db incorrectly had the 3.0 version
 UPDATE `broadcast_text` SET `text1`='The Headless Horseman''s undead mind is obsessed!  During Hallow''s End, his attacks on this village are as rhythmic as his insane voice.  He will return in roughly $4676D.' WHERE `id`=22061; -- Alliance
 UPDATE `broadcast_text` SET `text1`='The Headless Horseman''s mind is plagued with dementia!  During Hallow''s End, his attacks on this village are as rhythmic as his insane voice.$B$BHe will return in roughly $4676D.' WHERE `id`=22528; -- Horde
+-- the first text of the jack-o-lantern was slightly different in TBC
+UPDATE broadcast_text SET `text`="This large jack-o'-lantern rests in the middle of the village.  It eyes those who look up it, betraying a dark menace within." WHERE id=23650;
 
 -- classic-db npc_text: On the northern dock, you can board a ship that will carry you to Rut'theran Village and Darnassus. From the southern dock, you can find passage across the Great Sea to Menethil Harbor on Khaz Modan. Safe journeys to you!
 -- broadcast text id 8106 (matches): On the northern dock, you can board a ship that will carry you to Rut'theran Village and Darnassus.  From the southern dock, you can find passage across the Great Sea to Menethil Harbor on Khaz Modan.  Safe journeys to you!

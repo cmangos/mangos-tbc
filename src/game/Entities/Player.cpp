3445a0d12705bc06cf7a1ab88b327c553181d0d9
killerwife <killerwife@gmail.com>
2020-12-21 10:01:36 +0100

    Spell: Safeguard PoM check with alive

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -20686,7 +20686,7 @@ Player* Player::GetNextRaidMemberWithLowestLifePercentage(float radius, AuraType
     {
         Player* target = itr->getSource();
 
-        if (target && target != this)
+        if (target && target != this && target->IsAlive())
         {
             float x, y, z;
             GetPosition(x, y, z);

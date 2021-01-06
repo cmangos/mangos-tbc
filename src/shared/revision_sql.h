b24880bf7555e00fcf6729e4ddeee35185c2793b
Xfurry <xfurry.cmangos@outlook.com>
2021-01-06 20:18:24 +0100

    DBScript: Change behavior in modify NPC / Unit flags commands
    New logic:
    0 = remove
    1 = add
    2 = toggle
    
    This will prevent potential issues with leaving 0 as the default value for toggle
    
    Signed-off-by: Xfurry <xfurry.cmangos@outlook.com>
    
    (based on cmangos/mangos-wotlk@f64bbe823)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_s2421_01_realmd_account_locale_agnostic"
  #define REVISION_DB_CHARACTERS "required_s2423_01_characters_item_instance_duration_default"
- #define REVISION_DB_MANGOS "required_s2425_01_mangos_creature_spawn_tideup"
+ #define REVISION_DB_MANGOS "s2426_01_mangos_dbscript_npc_flag_update"
 #endif // __REVISION_SQL_H__

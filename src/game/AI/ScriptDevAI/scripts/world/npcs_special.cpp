431cd493bdca2b7d5bf5307e958851d93dd583ec
killerwife <killerwife@gmail.com>
2021-01-27 20:42:17 +0100

    TK/MgT: Despawn egg on corpse removal of phoenix on wipe

--- src/game/AI/ScriptDevAI/scripts/world/npcs_special.cpp
+++ src/game/AI/ScriptDevAI/scripts/world/npcs_special.cpp
@@ -2181,6 +2181,12 @@ struct mob_phoenix_tkAI : public CombatAI
         summoned->AI()->SetReactState(REACT_PASSIVE);
     }
 
+    void CorpseRemoved(uint32& /*respawnDelay*/) override // safeguard against wipe
+    {
+        if (Creature* egg = m_creature->GetMap()->GetCreature(m_eggGuid))
+            egg->ForcedDespawn();
+    }
+
     void DoRebirth()
     {
         if (m_creature->IsAlive())

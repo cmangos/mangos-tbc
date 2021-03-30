1fa52b6be6ce48a93837f0f06682444f77ebc4af
insunaa <dev.insuna@gmail.com>
2021-03-30 20:28:25 +0200

    improve timing for arugal dialogue

--- src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/shadowfang_keep/instance_shadowfang_keep.cpp
+++ src/game/AI/ScriptDevAI/scripts/eastern_kingdoms/shadowfang_keep/instance_shadowfang_keep.cpp
@@ -21,6 +21,7 @@ SDComment:
 SDCategory: Shadowfang Keep
 EndScriptData */
 
+#include "AI/BaseAI/UnitAI.h"
 #include "AI/ScriptDevAI/include/sc_common.h"
 #include "shadowfang_keep.h"
 
@@ -28,16 +29,16 @@ static const DialogueEntry aArugalDialogue[] =
 {
     {NPC_VINCENT,                      0,   2000},
     {VINCENT_DEATH,          NPC_VINCENT,   8000},
-    {ARUGAL_VISIBLE,          NPC_ARUGAL,    500},
-    {ARUGAL_TELEPORT_IN,      NPC_ARUGAL,   2000},
-    {SAY_ARUGAL_INTRO_1,      NPC_ARUGAL,   1750},
-    {ARUGAL_EMOTE_POINT,      NPC_ARUGAL,   1750},
-    {SAY_ARUGAL_INTRO_2,      NPC_ARUGAL,   1750},
-    {ARUGAL_EMOTE_EXCLAMATION,NPC_ARUGAL,   1750},
-    {SAY_ARUGAL_INTRO_3,      NPC_ARUGAL,   1750},
-    {ARUGAL_EMOTE_LAUGH,      NPC_ARUGAL,   1750},
-    {SAY_ARUGAL_INTRO_4,      NPC_ARUGAL,   2000},
-    {ARUGAL_TELEPORT_OUT,     NPC_ARUGAL,    2500},
+    {ARUGAL_VISIBLE,          NPC_ARUGAL,   1800},
+    {ARUGAL_TELEPORT_IN,      NPC_ARUGAL,   3000},
+    {SAY_ARUGAL_INTRO_1,      NPC_ARUGAL,    100},
+    {ARUGAL_EMOTE_POINT,      NPC_ARUGAL,   2500},
+    {SAY_ARUGAL_INTRO_2,      NPC_ARUGAL,   2500},
+    {ARUGAL_EMOTE_EXCLAMATION,NPC_ARUGAL,   2500},
+    {SAY_ARUGAL_INTRO_3,      NPC_ARUGAL,   2500},
+    {ARUGAL_EMOTE_LAUGH,      NPC_ARUGAL,   2500},
+    {SAY_ARUGAL_INTRO_4,      NPC_ARUGAL,   2500},
+    {ARUGAL_TELEPORT_OUT,     NPC_ARUGAL,   2500},
     {ARUGAL_INTRO_DONE,       NPC_ARUGAL,      0},
     {NPC_ARCHMAGE_ARUGAL,              0,    100},
     {YELL_FENRUS,    NPC_ARCHMAGE_ARUGAL,   2000},
@@ -270,11 +271,16 @@ void instance_shadowfang_keep::JustDidDialogueStep(int32 entry)
             break;
         case ARUGAL_VISIBLE:
             if (Creature* creature = GetSingleCreatureFromStorage(NPC_ARUGAL))
-                creature->SetVisibility(VISIBILITY_ON);
+                creature->AI()->DoCastSpellIfCan(creature, SPELL_SPAWN, CAST_TRIGGERED);
             break;
         case ARUGAL_TELEPORT_IN:
             if (Creature* creature = GetSingleCreatureFromStorage(NPC_ARUGAL))
-                creature->AI()->DoCastSpellIfCan(creature, SPELL_SPAWN);
+                creature->SetVisibility(VISIBILITY_ON);
+            break;
+        case SAY_ARUGAL_INTRO_1:
+            if (Creature* creatureA = GetSingleCreatureFromStorage(NPC_ARUGAL))
+                if (Creature* creatureV = GetSingleCreatureFromStorage(NPC_VINCENT))
+                    creatureA->SetFacingToObject(creatureV);
             break;
         case ARUGAL_EMOTE_POINT:
             if (Creature* creature = GetSingleCreatureFromStorage(NPC_ARUGAL))
@@ -290,7 +296,7 @@ void instance_shadowfang_keep::JustDidDialogueStep(int32 entry)
             break;
         case ARUGAL_TELEPORT_OUT:
             if (Creature* creature = GetSingleCreatureFromStorage(NPC_ARUGAL))
-                creature->AI()->DoCastSpellIfCan(creature, SPELL_SPAWN);
+                creature->AI()->DoCastSpellIfCan(creature, SPELL_SPAWN, CAST_TRIGGERED);
             break;
         case ARUGAL_INTRO_DONE:
             SetData(TYPE_INTRO, DONE);

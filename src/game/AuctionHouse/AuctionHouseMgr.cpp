878f1595e6bd4754e35ff8933590af7fcfcef33a
Filip Bekic <filip1994sm@gmail.com>
2021-04-02 10:40:35 +0200

    Update

--- src/game/AuctionHouse/AuctionHouseMgr.cpp
+++ src/game/AuctionHouse/AuctionHouseMgr.cpp
@@ -820,10 +820,13 @@ void WorldSession::BuildListAuctionItems(std::vector<AuctionEntry*> const& aucti
             if (itemSubClass != 0xffffffff && proto->SubClass != itemSubClass)
                 continue;
 
-            if (inventoryType != 0xffffffff && proto->InventoryType != inventoryType) {
-                // if inventory type is chest, we want to return robes too
-                // i.e. cloth chests are in most cases robes by definition
-                if (inventoryType != 5 || proto->InventoryType != 20) {
+            if (inventoryType != 0xffffffff && proto->InventoryType != inventoryType) 
+            {
+                if (inventoryType != INVTYPE_CHEST || proto->InventoryType != INVTYPE_ROBE) 
+                {
+                    // if inventory type is chest, we want to return robes too
+                    // i.e. cloth chests are in most cases robes by definition
+                
                     continue;
                 }
             }

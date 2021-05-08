04f0b485d5d9381a5dee674f61db537432feced8
killerwife <killerwife@gmail.com>
2021-05-08 22:36:59 +0200

    Spell: Let swing go through if next swing spell fails

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -2986,7 +2986,7 @@ void Spell::cancel()
     m_caster->RemoveGameObject(m_spellInfo->Id, true);
 }
 
-void Spell::cast(bool skipCheck)
+SpellCastResult Spell::cast(bool skipCheck)
 {
     SetExecutedCurrently(true);
     SpellModRAII spellModController(this, m_caster->GetSpellModOwner());
@@ -3001,7 +3001,7 @@ void Spell::cast(bool skipCheck)
         SendCastResult(SPELL_FAILED_ERROR);
         finish(false);
         SetExecutedCurrently(false);
-        return;
+        return SPELL_FAILED_ERROR;
     }
 
     // update pointers base at GUIDs to prevent access to already nonexistent object
@@ -3013,7 +3013,7 @@ void Spell::cast(bool skipCheck)
         cancel();
         m_caster->DecreaseCastCounter();
         SetExecutedCurrently(false);
-        return;
+        return SPELL_FAILED_ERROR;
     }
 
     if (m_caster->GetTypeId() != TYPEID_PLAYER && m_targets.getUnitTarget() && m_targets.getUnitTarget() != m_caster && !m_spellInfo->HasAttribute(SPELL_ATTR_EX5_DONT_TURN_DURING_CAST))
@@ -3031,7 +3031,7 @@ void Spell::cast(bool skipCheck)
         if (castResult != SPELL_CAST_OK)
         {
             StopCast(castResult);
-            return;
+            return castResult;
         }
     }
 
@@ -3102,7 +3102,7 @@ void Spell::cast(bool skipCheck)
     {
         m_caster->DecreaseCastCounter();
         SetExecutedCurrently(false);
-        return;
+        return SPELL_FAILED_ERROR; // currently not propagating right error here but it should not be needed
     }
 
     spellModController.SetSuccess();
@@ -3148,6 +3148,7 @@ void Spell::cast(bool skipCheck)
 
     m_caster->DecreaseCastCounter();
     SetExecutedCurrently(false);
+    return SPELL_CAST_OK;
 }
 
 void Spell::handle_immediate()

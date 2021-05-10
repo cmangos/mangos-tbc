8f1143d93fc8341bb4f6659d5e761bf6dd0e8563
killerwife <killerwife@gmail.com>
2021-05-10 11:15:15 +0200

    [s2428] Change default value of column CurHealth to 0

--- sql/base/mangos.sql
+++ sql/base/mangos.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_s2426_01_mangos_command` bit(1) DEFAULT NULL
+  `required_s2428_01_mangos_column_fix` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Used DB version notes';
 
 --
@@ -980,7 +980,7 @@ CREATE TABLE `creature_spawn_data_template` (
   `Faction` int unsigned NOT NULL DEFAULT '0',
   `ModelId` mediumint unsigned NOT NULL DEFAULT '0',
   `EquipmentId` mediumint NOT NULL DEFAULT '0',
-  `CurHealth` int unsigned NOT NULL DEFAULT '1',
+  `CurHealth` int unsigned NOT NULL DEFAULT '0',
   `CurMana` int unsigned NOT NULL DEFAULT '0',
   `SpawnFlags` int unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`Entry`,`UnitFlags`,`ModelId`,`EquipmentId`,`CurHealth`,`CurMana`,`SpawnFlags`)

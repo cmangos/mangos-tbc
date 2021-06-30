175a9e76161f243164fef63fbc52e43bb67f02f2
cala <cala@users.noreply.github.com>
2021-06-30 23:06:31 +0200

    Move spells 29351 & 30114 to SpellScript
    * Move to SpellScript spells 29351 & 30114 (Plague Wave Controller) used in Heigan the Unclean encounter

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -1338,15 +1338,6 @@ void Aura::TriggerSpell()
 //                    case 28114: break;
 //                    // Communique Timer, camp
 //                    case 28346: break;
-                    case 29351:                             // Plague Wave Controller (Slow)
-                    case 30114:                             // Plague Wave Controller (Fast)
-                    {
-                        uint32 spellForTick[6] = { 30116, 30117, 30118, 30119, 30118, 30117 };  // Circling back and forth through the 4 plague areas
-                        uint32 tick = (GetAuraTicks() - 1) % 6;
-
-                        triggerTarget->CastSpell(triggerTarget, spellForTick[tick], TRIGGERED_OLD_TRIGGERED, nullptr, this, casterGUID);
-                        return;
-                    }
 //                    // Silithyst
 //                    case 29519: break;
                     case 29528:                             // Inoculate Nestlewood Owlkin

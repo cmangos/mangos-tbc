4b3dca15177c221bbf00b5fc1eb170786784233a
insunaa <dev.insuna@gmail.com>
2021-08-31 22:56:47 +0200

    fix a few clang compiler warnings

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -4806,7 +4806,7 @@ SpellCastResult Spell::CheckCast(bool strict)
             if ((m_targets.m_targetMask == TARGET_FLAG_SELF || m_trueCaster == target) && m_spellInfo->HasAttribute(SPELL_ATTR_EX_CANT_TARGET_SELF))
             {
                 if (IsOnlySelfTargeting(m_spellInfo))
-                    sLog.outCustomLog("Spell ID %u cast at self explicitly even though it has SPELL_ATTR_EX_CANT_TARGET_SELF");
+                    sLog.outCustomLog("Spell ID %u cast at self explicitly even though it has SPELL_ATTR_EX_CANT_TARGET_SELF", m_spellInfo->Id);
 
                 return SPELL_FAILED_BAD_TARGETS;
             }

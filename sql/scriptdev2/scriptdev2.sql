fdecf7759ece17af977344d195d7eb2a8df3c0d0
Miraco <daaniel743mail.com>
2021-12-02 19:25:49 +0100

    Remove npc_shenthul script, will use EventAI instead.

--- sql/scriptdev2/scriptdev2.sql
+++ sql/scriptdev2/scriptdev2.sql
@@ -996,9 +996,6 @@ UPDATE creature_template SET ScriptName='npc_commander_arcus' WHERE entry IN(234
 UPDATE instance_template SET ScriptName='instance_onyxias_lair' WHERE map=249;
 UPDATE creature_template SET ScriptName='boss_onyxia' WHERE entry=10184;
 
-/* ORGRIMMAR */
-UPDATE creature_template SET ScriptName='npc_shenthul' WHERE entry=3401;
-
 /* RAGEFIRE CHASM */
 
 /* RAZORFEN DOWNS */

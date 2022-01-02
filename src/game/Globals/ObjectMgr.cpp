de31c9644caf46843d51e570c4fcde7a98e6bf18
killerwife <killerwife@gmail.com>
2022-01-02 23:03:23 +0100

    Fix check for spawnmask for alterac valley

--- src/game/Globals/ObjectMgr.cpp
+++ src/game/Globals/ObjectMgr.cpp
@@ -1961,7 +1961,7 @@ void ObjectMgr::LoadCreatures()
             data.spawntimesecsmax = data.spawntimesecsmin;
         }
 
-        if (mapEntry->IsDungeon())
+        if (mapEntry->IsDungeon() || mapEntry->IsBattleGround())
         {
             if (data.spawnMask & ~SPAWNMASK_DUNGEON_ALL)
                 sLog.outErrorDb("Table `creature` have creature (GUID: %u) that have wrong spawn mask %u for non-raid dungeon map (Id: %u).", guid, data.spawnMask, data.mapid);
@@ -2196,7 +2196,7 @@ void ObjectMgr::LoadGameObjects()
             continue;
         }
 
-        if (mapEntry->IsDungeon())
+        if (mapEntry->IsDungeon() || mapEntry->IsBattleGround())
         {
             if (data.spawnMask & ~SPAWNMASK_DUNGEON_ALL)
                 sLog.outErrorDb("Table `gameobject` have gameobject (GUID: %u Entry: %u) that have wrong spawn mask %u for dungeon map (Id: %u), skip", guid, data.id, data.spawnMask, data.mapid);

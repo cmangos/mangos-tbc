e7601d3a5288eeed04aad0253355e4974139775c
cala <cala@users.noreply.github.com>
2022-01-17 23:06:52 +0100

    Add missing include in MoveSplineInit.h
    * Fix build for some *NIX

--- src/game/Movement/MoveSplineInit.h
+++ src/game/Movement/MoveSplineInit.h
@@ -19,6 +19,7 @@
 #ifndef MANGOSSERVER_MOVESPLINEINIT_H
 #define MANGOSSERVER_MOVESPLINEINIT_H
 
+#include "Entities/Object.h"
 #include "Movement/MoveSplineInitArgs.h"
 #include "MotionGenerators/PathFinder.h"
 

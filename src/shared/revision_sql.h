718e4537a1854baad60fec6a8a26399dece078ec
Cyberium <cyberium@users.noreply.github.com>
2022-01-20 18:42:52 +0100

    [s2441] [DB Uniformisation] Waypoint table column rename
    Step 1 before merging those table

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -3,5 +3,5 @@
  #define REVISION_DB_REALMD "required_s2433_01_realmd_anticheat"
  #define REVISION_DB_LOGS "required_s2433_01_logs_anticheat"
  #define REVISION_DB_CHARACTERS "required_s2429_01_characters_raf"
- #define REVISION_DB_MANGOS "required_s2440_01_mangos_artkits"
+ #define REVISION_DB_MANGOS "required_s2441_01_mangos_waypoint_path"
 #endif // __REVISION_SQL_H__

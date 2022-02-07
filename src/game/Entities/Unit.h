3b2992c70a57bf14da8bf7b6239095a0604dfdcd
killerwife <killerwife@gmail.com>
2022-02-07 20:12:43 +0100

    Implement UNIT_FLAG2_DO_NOT_FADE_IN

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -513,7 +513,7 @@ enum UnitFlags2
     UNIT_FLAG2_IGNORE_REPUTATION    = 0x00000004,
     UNIT_FLAG2_COMPREHEND_LANG      = 0x00000008,
     UNIT_FLAG2_CLONED               = 0x00000010,           // Used in SPELL_AURA_MIRROR_IMAGE
-    UNIT_FLAG2_UNK5                 = 0x00000020,
+    UNIT_FLAG2_DO_NOT_FADE_IN       = 0x00000020,
     UNIT_FLAG2_FORCE_MOVE           = 0x00000040,
     // UNIT_FLAG2_DISARM_OFFHAND       = 0x00000080,        // also shield case - added in 3.x, possible all later not used in pre-3.x
     // UNIT_FLAG2_UNK8                 = 0x00000100,

38a8b0760842d11d0b3f54c1bb418fc3ec0b3558
killerwife <killerwife@gmail.com>
2022-03-20 11:09:44 +0100

    Relations: Safeguard assist combat in instances against mobs which should not help ever
    Unselectable especially

--- src/game/Entities/Relations.cpp
+++ src/game/Entities/Relations.cpp
@@ -1391,7 +1391,7 @@ bool Unit::CanAssistInCombatAgainst(Unit const* who, Unit const* enemy) const
     MANGOS_ASSERT(enemy)
 
     if (GetMap()->Instanceable()) // in dungeons nothing else needs to be evaluated
-        return true;
+        return CanJoinInAttacking(enemy);
 
     if (IsInCombat()) // if fighting something else, do not assist
         return false;
@@ -1414,6 +1414,9 @@ bool Unit::CanAssistInCombatAgainst(Unit const* who, Unit const* enemy) const
 bool Unit::CanJoinInAttacking(Unit const* enemy) const
 {
     if (!CanEnterCombat())
+        return false;
+
+    if (!CanInitiateAttack())
         return false;
 
     if (IsFeigningDeathSuccessfully())

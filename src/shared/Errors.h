bdba08702c2af7d5a7f52adb22b6bc075eb91cfa
insunaa <dev.insuna@gmail.com>
2022-04-22 19:15:04 +0200

    Disable MANGOS_ASSERT when defining NDEBUG

--- src/shared/Errors.h
+++ src/shared/Errors.h
@@ -21,14 +21,18 @@
 
 #include "Common.h"
 
+#ifndef NDEBUG
 // Normal assert.
-#define WPError(CONDITION) \
+#  define WPError(CONDITION) \
 if (!(CONDITION)) \
 { \
     printf("%s:%i: Error: Assertion in %s failed: %s", \
         __FILE__, __LINE__, __FUNCTION__, STRINGIZE(CONDITION)); \
     assert(STRINGIZE(CONDITION) && 0); \
 }
+#else
+#  define WPError(CONDITION)
+#endif
 
 // Just warn.
 #define WPWarning(CONDITION) \

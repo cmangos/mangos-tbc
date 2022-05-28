4ecb0e6ce86082cec8c5234e2411f249ba44e1b8
killerwife <killerwife@gmail.com>
2022-05-28 19:30:54 +0200

    [s2451] Waypoint: Implement forced naming of waypoint_path paths

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -3,5 +3,5 @@
  #define REVISION_DB_REALMD "required_s2433_01_realmd_anticheat"
  #define REVISION_DB_LOGS "required_s2433_01_logs_anticheat"
  #define REVISION_DB_CHARACTERS "required_s2429_01_characters_raf"
- #define REVISION_DB_MANGOS "required_s2450_01_mangos_quest_maxlevel"
+ #define REVISION_DB_MANGOS "required_s2451_01_mangos_waypoint_path_name"
 #endif // __REVISION_SQL_H__

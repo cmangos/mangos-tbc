075bc88ac8ee7aa3eb8ca3e74be975057cfeb911
killerwife <killerwife@gmail.com>
2022-06-21 22:46:20 +0200

    Spell/Pet: Enable calling HandlePetCancelAuraOpcode for non mover

--- src/game/Spells/SpellHandler.cpp
+++ src/game/Spells/SpellHandler.cpp
@@ -516,10 +516,6 @@ void WorldSession::HandlePetCancelAuraOpcode(WorldPacket& recvPacket)
     recvPacket >> guid;
     recvPacket >> spellId;
 
-    // ignore for remote control state
-    if (!_player->IsSelfMover())
-        return;
-
     SpellEntry const* spellInfo = sSpellTemplate.LookupEntry<SpellEntry>(spellId);
     if (!spellInfo)
     {
@@ -548,9 +544,6 @@ void WorldSession::HandlePetCancelAuraOpcode(WorldPacket& recvPacket)
     }
 
     pet->RemoveAurasDueToSpell(spellId);
-
-    // TODO: check if its correctly handled in aura remove
-    //pet->AddCreatureSpellCooldown(spellId);
 }
 
 void WorldSession::HandleCancelGrowthAuraOpcode(WorldPacket& /*recvPacket*/)

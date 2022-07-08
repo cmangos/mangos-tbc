e0ac38975823eebd05747b87e2f67206d953aac3
_daanieL <daaniel743@gmail.com>
2022-07-08 10:50:08 +0200

    Update s451_01_mangos_instance_uldaman_prototype.sql

--- sql/updates/mangos/s451_01_mangos_instance_uldaman_prototype.sql
+++ sql/updates/mangos/s451_01_mangos_instance_uldaman_prototype.sql
@@ -44,7 +44,7 @@ DELETE FROM worldstate_name WHERE Id = 20000;
 INSERT INTO `worldstate_name` (`Id`, `Name`) VALUES 
 ('20000', 'Uldaman - Andora Spawn');
 
-DELETE FROM conditions WHERE PathId = 12002;
+DELETE FROM conditions WHERE condition_entry = 12002;
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value3`, `comments`) VALUES 
 ('12002', '42', '20000', '1', 'Uldaman - Andora Spawn');
 

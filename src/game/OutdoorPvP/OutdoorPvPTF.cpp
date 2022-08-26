df84cacacc3a89aecfd603632c6be88fada0b7fc
killerwife <killerwife@gmail.com>
2022-08-26 22:54:52 +0200

    Opvp/GameObject: Fix OutdoorPvP scripts return wrong val on not handling event and bricking dbscripts
    Closes https://github.com/cmangos/issues/issues/3116

--- src/game/OutdoorPvP/OutdoorPvPTF.cpp
+++ src/game/OutdoorPvP/OutdoorPvPTF.cpp
@@ -144,7 +144,7 @@ void OutdoorPvPTF::HandleObjectiveComplete(uint32 eventId, const std::list<Playe
 bool OutdoorPvPTF::HandleEvent(uint32 eventId, Object* source, Object* /*target*/)
 {
     if (!source->IsGameObject())
-        return true;
+        return false;
 
     GameObject* go = static_cast<GameObject*>(source);
     for (uint8 i = 0; i < MAX_TF_TOWERS; ++i)

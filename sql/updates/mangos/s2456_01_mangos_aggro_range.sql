16f56f35fc38481be0c6c08d06c781aa7477d0de
killerwife <killerwife@gmail.com>
2022-11-25 10:06:24 +0100

    [s2456] Unit: Change default aggro distance from 20yd to 18yd

---
+++ sql/updates/mangos/s2456_01_mangos_aggro_range.sql
@@ -0,0 +1,5 @@
+ALTER TABLE db_version CHANGE COLUMN required_s2454_01_mangos_battleground_reflooot required_s2456_01_mangos_aggro_range bit;
+
+ALTER TABLE creature_template MODIFY Detection INT UNSIGNED NOT NULL DEFAULT '18';
+
+

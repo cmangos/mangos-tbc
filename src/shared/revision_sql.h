4ce90bc0c0092b73a66f09fba6959b5fe0c908d8
killerwife <killerwife@gmail.com>
2023-02-13 19:41:16 +0100

    [s2460] GameObject: Separate state and animprogress to own table and enforce new defaults

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -3,5 +3,5 @@
  #define REVISION_DB_REALMD "required_s2455_01_realmd_platform"
  #define REVISION_DB_LOGS "required_s2433_01_logs_anticheat"
  #define REVISION_DB_CHARACTERS "required_s2452_01_characters_fishingSteps"
- #define REVISION_DB_MANGOS "required_s2459_01_mangos_addon_stand_state"
+ #define REVISION_DB_MANGOS "required_s2460_01_mangos_gameobject_data_split"
 #endif // __REVISION_SQL_H__

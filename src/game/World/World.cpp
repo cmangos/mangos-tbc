b303101f5f3806e452fa58c098ec7cc65ddf4f79
killerwife <killerwife@gmail.com>
2023-07-09 13:03:39 +0200

    GameObject: Add forgotten loading of gameobject_template_addon

--- src/game/World/World.cpp
+++ src/game/World/World.cpp
@@ -1076,6 +1076,9 @@ void World::SetInitialWorldSettings()
     sLog.outString(">>> Creature Addon Data loaded");
     sLog.outString();
 
+    sLog.outString("Loading Gameobject Template Addon Data...");
+    sObjectMgr.LoadGameObjectTemplateAddons();
+
     sLog.outString("Loading CreatureLinking Data...");      // must be after Creatures
     sCreatureLinkingMgr.LoadFromDB();
 

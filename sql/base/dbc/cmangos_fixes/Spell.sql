b65cb4614eb8d7b601bf8e246751f95d1aebd674
AnonXS <anonxsdev@gmail.com>
2023-09-10 20:57:25 +0200

    [Spell] Sand Storm 10092 should not be removed on Evade

--- sql/base/dbc/cmangos_fixes/Spell.sql
+++ sql/base/dbc/cmangos_fixes/Spell.sql
@@ -2203,6 +2203,7 @@ UPDATE `spell_template` SET `AttributesServerSide` = `AttributesServerSide`|4 WH
 5888, -- Darkshore Frenzy
 7131, -- Illusion Passive
 8203, -- Elemental Spirit Invisibility
+10092, -- Sand Storm
 11011, -- Stone Watcher of Norgannon Passive
 15978, -- Puncture
 21911, -- Puncture

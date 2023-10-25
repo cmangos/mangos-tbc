6b8843613c899b31dc47c21e0011d32c5bcda53c
killerwife <killerwife@gmail.com>
2023-10-25 20:53:34 +0200

    Spell 27978 should not apply UNIT_AURAFLAG_ALIVE_INVISIBLE

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -2958,7 +2958,6 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
                     }
                     return;
                 }
-                case 27978:
                 case 40131:
                     if (apply)
                         target->m_AuraFlags |= UNIT_AURAFLAG_ALIVE_INVISIBLE;

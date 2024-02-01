cmangos/mangos-wotlk

Support expansion-specific XP modifiers

src/game/Entities/Player.cpp
@@ -6881,7 +6881,7 @@ void Player::CheckAreaExploreAndOutdoor()
                 uint32 XP;
                 if (diff < -5)
                 {
-                    XP = uint32(sObjectMgr.GetBaseXP(GetLevel() + 5) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE));
+                    XP = uint32(sObjectMgr.GetBaseXP(GetLevel() + 5) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE) * GetMap()->GetExploreXPMod());
                 }
                 else if (diff > 5)
                 {
@@ -6891,11 +6891,11 @@ void Player::CheckAreaExploreAndOutdoor()
                     else if (exploration_percent < 0)
                         exploration_percent = 0;
 
-                    XP = uint32(sObjectMgr.GetBaseXP(p->area_level) * exploration_percent / 100 * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE));
+                    XP = uint32(sObjectMgr.GetBaseXP(p->area_level) * exploration_percent / 100 * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE) * GetMap()->GetExploreXPMod());
                 }
                 else
                 {
-                    XP = uint32(sObjectMgr.GetBaseXP(p->area_level) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE));
+                    XP = uint32(sObjectMgr.GetBaseXP(p->area_level) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE) * GetMap()->GetExploreXPMod());
                 }
 
                 GiveXP(XP, nullptr);
@@ -14489,7 +14489,7 @@ void Player::RewardQuest(Quest const* pQuest, uint32 reward, Object* questGiver,
     bool rewarded = q_status.m_rewarded && !pQuest->IsDungeonFinderQuest();
 
     // Used for client inform but rewarded only in case not max level
-    uint32 xp = uint32(pQuest->GetXPReward(this) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_QUEST));
+    uint32 xp = uint32(pQuest->GetXPReward(this) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_QUEST) * GetMap()->GetQuestXPMod());
 
     if (GetLevel() < GetMaxAttainableLevel())
     {

cmangos/mangos-tbc

src/game/Entities/Player.cpp
@@ -6442,7 +6442,7 @@ void Player::CheckAreaExploreAndOutdoor()
                 uint32 XP;
                 if (diff < -5)
                 {
-                    XP = uint32(sObjectMgr.GetBaseXP(GetLevel() + 5) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE));
+                    XP = uint32(sObjectMgr.GetBaseXP(GetLevel() + 5) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE) * GetMap()->GetExploreXPMod());
                 }
                 else if (diff > 5)
                 {
@@ -6452,11 +6452,11 @@ void Player::CheckAreaExploreAndOutdoor()
                     else if (exploration_percent < 0)
                         exploration_percent = 0;
 
-                    XP = uint32(sObjectMgr.GetBaseXP(p->area_level) * exploration_percent / 100 * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE));
+                    XP = uint32(sObjectMgr.GetBaseXP(p->area_level) * exploration_percent / 100 * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE) * GetMap()->GetExploreXPMod());
                 }
                 else
                 {
-                    XP = uint32(sObjectMgr.GetBaseXP(p->area_level) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE));
+                    XP = uint32(sObjectMgr.GetBaseXP(p->area_level) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_EXPLORE) * GetMap()->GetExploreXPMod());
                 }
 
                 GiveXP(XP, nullptr);
@@ -13316,7 +13316,7 @@ void Player::RewardQuest(Quest const* pQuest, uint32 reward, Object* questGiver,
     QuestStatusData& q_status = mQuestStatus[quest_id];
 
     // Used for client inform but rewarded only in case not max level
-    uint32 xp = uint32(pQuest->XPValue(this) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_QUEST));
+    uint32 xp = uint32(pQuest->XPValue(this) * sWorld.getConfig(CONFIG_FLOAT_RATE_XP_QUEST) * GetMap()->GetQuestXPMod());
 
     if (GetLevel() < GetMaxAttainableLevel())
         GiveXP(xp, nullptr);
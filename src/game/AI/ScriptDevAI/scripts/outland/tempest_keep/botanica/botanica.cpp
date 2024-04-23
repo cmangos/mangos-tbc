df939d354b5e0a625a5b4c19d06181c1240c31a1
Miraco <daaniel743@gmail.com>
2024-04-23 21:27:42 +0200

    more typo

--- src/game/AI/ScriptDevAI/scripts/outland/tempest_keep/botanica/botanica.cpp
+++ src/game/AI/ScriptDevAI/scripts/outland/tempest_keep/botanica/botanica.cpp
@@ -81,5 +81,5 @@ void AddSC_instance_botanica()
     pNewScript->GetInstanceData = &GetNewInstanceScript<instance_botanica>;
     pNewScript->RegisterSelf();
 
-    RegisterSpellScript<ArcaneDevastation>("spell_clear_all_debuffs");
+    RegisterSpellScript<ArcaneDevastation>("spell_arcane_devastation");
 }

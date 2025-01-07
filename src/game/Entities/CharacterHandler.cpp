9b03d101138566b5a0b20f5d255b27ed07a5fcb5
killerwife <killerwife@gmail.com>
2025-01-07 13:10:32 +0100

    Player: Cleanup reconnect protocol vs connect protocol

--- src/game/Entities/CharacterHandler.cpp
+++ src/game/Entities/CharacterHandler.cpp
@@ -882,7 +882,7 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder* holder)
 
     pCurrChar->GetSocial()->SendSocialList();
 
-    pCurrChar->SendInitialPacketsAfterAddToMap();
+    pCurrChar->SendInitialPacketsAfterAddToMap(false);
 
     static SqlStatementID updChars;
     static SqlStatementID updAccount;
@@ -1081,18 +1081,7 @@ void WorldSession::HandlePlayerReconnect()
     if (group)
         group->SendUpdateTo(_player);
 
-    _player->GetSocial()->SendSocialList();
-    uint32 areaId = 0;
-    uint32 zoneId = 0;
-    _player->GetZoneAndAreaId(zoneId, areaId);
-    _player->SendInitWorldStates(zoneId, areaId);
-    _player->GetSession()->ResetTimeSync();
-    _player->GetSession()->SendTimeSync();
-    _player->SendAllSpellMods(SPELLMOD_FLAT);
-    _player->SendAllSpellMods(SPELLMOD_PCT);
-    _player->CastSpell(_player, 836, TRIGGERED_OLD_TRIGGERED);       // LOGINEFFECT
-    _player->SendEnchantmentDurations();                             // must be after add to map
-    _player->SendItemDurations();                                    // must be after add to map
+    _player->SendInitialPacketsAfterAddToMap(true);
 
     // Send friend list online status for other players
     sSocialMgr.SendFriendStatus(_player, FRIEND_ONLINE, _player->GetObjectGuid(), true);
@@ -1120,9 +1109,6 @@ void WorldSession::HandlePlayerReconnect()
     std::string IP_str = GetRemoteAddress();
     sLog.outChar("Account: %d (IP: %s) Login Character:[%s] (guid: %u)",
         GetAccountId(), IP_str.c_str(), _player->GetName(), _player->GetGUIDLow());
-
-    // sync client auras timer
-    _player->UpdateClientAuras();
 
     // sync client control (if taxi flying the client is already sync)
     if (_player->IsTaxiFlying())

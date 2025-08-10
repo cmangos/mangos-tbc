3fee8fba674560a15c329ddb35de092aeeb7cd68
killerwife <killerwife@gmail.com>
2025-08-10 17:31:06 +0200

    Force costs for trade facilitated spell
    https://github.com/cmangos/issues/issues/3859

--- src/game/Trade/TradeHandler.cpp
+++ src/game/Trade/TradeHandler.cpp
@@ -340,7 +340,7 @@ void WorldSession::HandleAcceptTradeOpcode(WorldPacket& recvPacket)
                 return;
             }
 
-            my_spell = new Spell(_player, spellEntry, TRIGGERED_OLD_TRIGGERED);
+            my_spell = new Spell(_player, spellEntry, TRIGGERED_OLD_TRIGGERED | TRIGGERED_FORCE_COSTS);
             my_spell->SetCastItem(castItem);
             my_targets.setTradeItemTarget(_player);
             my_spell->m_targets = my_targets;

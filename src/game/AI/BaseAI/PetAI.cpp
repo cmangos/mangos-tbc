71615a47207e707ada4250c2769e61a48290353c
Velite12 <schamorro357@gmail.com>
2026-06-08 20:22:04 -0400

    Update PetAI.cpp

--- src/game/AI/BaseAI/PetAI.cpp
+++ src/game/AI/BaseAI/PetAI.cpp
@@ -339,7 +339,9 @@ std::vector<std::tuple<SpellEntry const*, Unit*, bool>> PetAI::PickSpellWithTarg
             if (!ShouldCast(spellInfo, victim) || !CanAutoCast(spellInfo, m_unit))
                 continue;
 
-            nonblockingSpells.emplace_back(spellInfo, m_unit, false);
+            // m_unit fails autocast if unit is currently unattackable (phase shift for example), so we leave nullptr
+            // which will later resolve to self targeting
+            nonblockingSpells.emplace_back(spellInfo, nullptr, false);
             continue;
         }
         // Try to cast a spell if the spell is AoE

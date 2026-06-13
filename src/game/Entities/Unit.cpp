994d364d7f5dc4172fd423697a053bb3f0b5018b
Velite12 <schamorro357@gmail.com>
2026-06-13 14:44:23 -0400

    Keep error log

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -1569,7 +1569,10 @@ SpellCastResult Unit::CastSpell(Unit* Victim, SpellEntry const* spellInfo, uint3
     {
         // This shouldn't happen, but we should return gracefully if it does...
         if (!Victim)
+        {
+            sLog.outError("CastSpell: victim was nullptr but tried to get position: caster %s, spellId %i", GetGuidStr().c_str(), spellInfo->Id);
             return SPELL_FAILED_BAD_TARGETS;
+        }    
         targets.setDestination(Victim->GetPositionX(), Victim->GetPositionY(), Victim->GetPositionZ()); 
     }
     if (spellInfo->Targets & TARGET_FLAG_SOURCE_LOCATION)
